8bbb14916d9598433859d6169ec27fce3e461967
ahollosi <ahollosi@96ab9672-09ca-45d6-a79d-3d69d39ca109>
2000-12-30 21:09:13 +0000

    some code cleanup -- mostly cosmetic changes

--- lib/display.php
+++ lib/display.php
@@ -1,9 +1,7 @@
-<!-- $Id: display.php,v 1.4 2000-11-01 11:31:41 ahollosi Exp $ -->
 <?php
-   /*
-      display.php: render a page. This has all the display 
-      logic in it, except for the search boxes.
-   */
+   // display.php: fetch page or get default content
+   // calls transform.php for actual transformation of wiki markup to HTML
+   rcs_id('$Id: display.php,v 1.5 2000-12-30 21:09:13 ahollosi Exp $');
  
    // if we got GET data, the first item is always a page name
    // if it wasn't this file would not have been included
@@ -23,9 +21,9 @@
    $enc_name = rawurlencode($pagename);
    $pagehash = RetrievePage($dbi, $pagename, $WikiPageStore);
 
+   // we render the page if it exists, else ask the user to write one.
    if (is_array($pagehash)) {
-      // we render the page if it's a hash, else ask the user to write one.
-      // This file returns a variable $html containing all the HTML markup
+      // transform.php returns $html containing all the HTML markup
       include("lib/transform.php");
    } else {
       $html .= sprintf(gettext("Describe %s here."),
