72c63da9c06449e9495eb9e1febb2cb40298596c
dairiki <dairiki@96ab9672-09ca-45d6-a79d-3d69d39ca109>
2001-04-06 21:12:59 +0000

    Updated translate.sh script to scan current source code for gettext strings to be translated.

--- trunk/locale/translate.sh
+++ trunk/locale/translate.sh
@@ -18,7 +18,7 @@ fi
 
 ALL_LINGUAS="nl es de sv"
 
-xgettext -L C++ -o locale/po/phpwiki.pot admin.php lib/*php
+xgettext -L C++ -o locale/po/phpwiki.pot {lib,admin}/*.php
 podir=locale/po
 for i in $ALL_LINGUAS; do
 	po=$podir/$i.po
