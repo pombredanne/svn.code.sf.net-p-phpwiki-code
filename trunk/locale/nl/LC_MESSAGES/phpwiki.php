e13476a32478e1b40c2084e2c294c34832af4214
dairiki <dairiki@96ab9672-09ca-45d6-a79d-3d69d39ca109>
2001-05-31 17:48:05 +0000

    Re-make *.{po,mo} files.

--- trunk/locale/nl/LC_MESSAGES/phpwiki.php
+++ trunk/locale/nl/LC_MESSAGES/phpwiki.php
@@ -1,5 +1,7 @@
 <?php
 
+$locale["!References"] =
+   "!Referenties";
 $locale["%d best incoming links:"] =
    "%d beste inkomende koppelingen:";
 $locale["%d best outgoing links:"] =
@@ -8,10 +10,20 @@ $locale["%d matches found in %d pages."] =
    "%d keer gevonden in %d pagina's.";
 $locale["%d most popular nearby:"] =
    "%d meest gevraagde in de buurt:";
+$locale["%d pages match your query."] =
+   "%d pagina's voldoen aan je vraag.";
 $locale["(first day for this Wiki)"] =
    "(eerste dag voor deze Wiki)";
+$locale["(new)"] =
+   "(nieuw)";
+$locale["- saved"] =
+   "- bewaard";
+$locale["- skipped"] =
+   "- overgeslagen";
 $locale["<b>Reload</b> the page. You should now see the most current version of the page. Your changes are no longer there."] =
    "<b>Reload</b> de pagina.  Je zou nu de meest recente versie van de pagina moeten zien.  Jouw veranderingen staan er nu niet meer.";
+$locale["Action $action is disabled in this wiki."] =
+   "Actie $action is niet beschikbaar in deze wiki.";
 $locale["Archived page:"] =
    "Gearchiveerde pagina:";
 $locale["Archived version"] =
@@ -22,10 +34,14 @@ $locale["Cannot establish connection to database, giving up."] =
    "Kan verbinding naar data base niet tot stand brengen, geef op.";
 $locale["Cannot open database %s, giving up."] =
    "Kan data base %s niet openen, geef op.";
+$locale["Click <a href=\"%s\">here</a> to remove the page now."] =
+   "Klik <a href=\"%s\">hier</a> om de pagina nu te verwijderen.";
+$locale["Complete."] =
+   "Klaar.";
 $locale["Copy of %s"] =
    "Kopie van %s";
 $locale["Copy your changes to the clipboard or to another temporary place (e.g. text editor)."] =
-   "Kopiëer je veranderingen naar het klipbord of een andere tijdelijke plaats (bijv. een tekst editor).";
+   "Kopier je veranderingen naar het klipbord of een andere tijdelijke plaats (bijv. een tekst editor).";
 $locale["Current page:"] =
    "Huidige pagina:";
 $locale["Current version"] =
@@ -43,15 +59,21 @@ $locale["Error writing page '%s'"] =
 $locale["Full Text Search Results"] =
    "Volledige tekst zoek resultaten";
 $locale["Function not yet implemented."] =
-   "Functie nog niet geïmplementeerd.";
+   "Functie nog niet gemplementeerd.";
 $locale["HomePage"] =
-   "VoorPagina";
+   "ThuisPagina";
 $locale["In order to recover from this situation follow these steps:"] =
    "Om uit deze benarde situatie te geraken, volg je deze stappen:";
 $locale["Insert into %s failed: %s"] =
    "Insert in %s gefaald: %s";
 $locale["Insert/update failed: %s"] =
    "Insert/update gefaald: %s";
+$locale["Lists nested too deep in SetHTMLOutputMode"] =
+   "Lijsten nesten te diep in SetHTMLOutputMode";
+$locale["Loading up virgin wiki"] =
+   "Laden van maagdelijke wiki";
+$locale["MIME file"] =
+   "MIME bestand";
 $locale["Make changes to the file again. Paste your additions from the clipboard (or text editor)."] =
    "Breng je veranderingen aan de pagina opnieuw aan.  Plak je toevoeginen van het klipbord (of tekst editor).";
 $locale["MySQL error: %s"] =
@@ -80,18 +102,28 @@ $locale["Remove page"] =
    "Verwijder pagina";
 $locale["Removed page '%s' succesfully."] =
    "Pagina '%s' verwijderd.";
+$locale["Return to "] =
+   "Terug naar ";
 $locale["Search"] =
    "Zoek";
 $locale["Searching for \"%s\" ....."] =
    "Zoeken naar \"%s\" ...";
+$locale["Serialized file"] =
+   "Geserialiseerd bestand";
+$locale["Show the page source"] =
+   "Toon de pagina bron tekst";
 $locale["Sorry for the inconvenience."] =
    "Excuses voor het ongemak.";
 $locale["Thank you for editing %s."] =
    "Bedankt voor het veranderen van %s.";
+$locale["The PhpWiki programming team"] =
+   "Het PhpWiki programmeerteam";
 $locale["The most recently changed pages are listed below."] =
    "De meest recent veranderde pagina's staan hieronder:";
 $locale["This page has been locked by the administrator and cannot be edited."] =
    "Deze pagina is afgesloten door de beheerder en kan niet veranderd worden.";
+$locale["Title Search Results"] =
+   "Titel zoek resultaten";
 $locale["Use your browser's <b>Back</b> button to go back to the edit page."] =
    "Gebruik de <b>Terug</b> knop van je bladeraar om terug te gaan naar de verander tekst pagina.";
 $locale["Versions are identical"] =
@@ -102,12 +134,28 @@ $locale["You are about to remove '%s' permanently!"] =
    "Je staat op het punt '%s' voorgoed te verwijderen!";
 $locale["You entered an invalid login or password."] =
    "Je hebt een ongeldige gebruikersnaam of wachtwoord ingevoerd.";
+$locale["You must set the administrator account and password before you can log in."] =
+   "Je moet de beheerder gebruiker en het wachtwoord zetten voordat je in kan loggen.";
+$locale["You must specify a directory to dump to"] =
+   "Je moet een directory specificeren om in te storten";
+$locale["You must specify a source to read from"] =
+   "Je moet een bron specificeren om van te lezen.";
 $locale["Your careful attention to detail is much appreciated."] =
    "Je zorgvuldige aandacht voor detail wordt erg gewaardeerd.";
+$locale["[%d] See [%s]"] =
+   "[%d] Zie [%s]";
+$locale["as version %d"] =
+   "als versie %d";
 $locale["by %s"] =
    "door %s";
+$locale["from %s"] =
+   "van %s";
+$locale["is identical to current version %d"] =
+   "is gelijk aaan huidige versie %d";
 $locale["last modified on %s"] =
-   "laatste verandering op %s";
+   "meest recente verandering op %s";
+$locale["plain file"] =
+   "plat bestand";
 $locale["version %s"] =
    "versie %s";
 
