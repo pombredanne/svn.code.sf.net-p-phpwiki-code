8ffeb9fb88a187ad319f21a0b6c37cb0c213bde5
wainstead <wainstead@96ab9672-09ca-45d6-a79d-3d69d39ca109>
2001-07-07 17:34:52 +0000

    Reformatted the code by hand to PEAR standards. Well, mostly by hand. php-mode doesn't like variable assignments that span several lines like:
         $html = "<P><B>"
               . sprintf(gettext ("Searching for \"%s\" ....."),
                 htmlspecialchars($searchterm))
               . "</B></P>\n";
    
    I have to indent this by hand.

--- lib/search.php
+++ lib/search.php
@@ -1,31 +1,34 @@
 <?php
-   // Title search: returns pages having a name matching the search term
-   rcs_id('$Id: search.php,v 1.5 2001-02-12 01:43:10 dairiki Exp $');
 
-   if (empty($searchterm))
-      $searchterm = '';		// FIXME: do something better here?
+// Title search: returns pages having a name matching the search term
 
-   fix_magic_quotes_gpc($searchterm);
+rcs_id('$Id: search.php,v 1.6 2001-07-07 17:34:52 wainstead Exp $');
 
-   $html = "<P><B>"
-	   . sprintf(gettext ("Searching for \"%s\" ....."),
-		     htmlspecialchars($searchterm))
-	   . "</B></P>\n";
+if (empty($searchterm))
+     $searchterm = '';		// FIXME: do something better here?
 
-   // quote regexp chars
-   $search = preg_quote($searchterm);
+     fix_magic_quotes_gpc($searchterm);
 
-   // search matching pages
-   $query = InitTitleSearch($dbi, $searchterm);
-   $found = 0;
-   while ($page = TitleSearchNextMatch($dbi, $query)) {
-      $found++;
-      $html .= LinkExistingWikiWord($page) . "<br>\n";
-   }
+     $html = "<P><B>"
+           . sprintf(gettext ("Searching for \"%s\" ....."),
+             htmlspecialchars($searchterm))
+           . "</B></P>\n";
 
-   $html .= "<hr noshade>\n"
-	    . sprintf(gettext ("%d pages match your query."), $found)
-	    . "\n";
+// quote regexp chars
+$search = preg_quote($searchterm);
 
-   echo GeneratePage('MESSAGE', $html, gettext ("Title Search Results"), 0);
+// search matching pages
+$query = InitTitleSearch($dbi, $searchterm);
+$found = 0;
+while ($page = TitleSearchNextMatch($dbi, $query)) {
+  $found++;
+  $html .= LinkExistingWikiWord($page) . "<br>\n";
+}
+
+$html .= "<hr noshade>\n"
+      . sprintf(gettext ("%d pages match your query."), $found)
+      . "\n";
+
+echo GeneratePage('MESSAGE', $html, gettext ("Title Search Results"), 0);
+
 ?>
