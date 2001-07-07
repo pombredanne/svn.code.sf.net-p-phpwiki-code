38e0924e5396bcc34976cf44671d03ddd82047fc
wainstead <wainstead@96ab9672-09ca-45d6-a79d-3d69d39ca109>
2001-07-07 17:44:25 +0000

    Got fooled by an IF with no braces.

--- lib/search.php
+++ lib/search.php
@@ -2,27 +2,30 @@
 
 // Title search: returns pages having a name matching the search term
 
-rcs_id('$Id: search.php,v 1.6 2001-07-07 17:34:52 wainstead Exp $');
+rcs_id('$Id: search.php,v 1.7 2001-07-07 17:44:25 wainstead Exp $');
 
-if (empty($searchterm))
-     $searchterm = '';		// FIXME: do something better here?
+if (empty($searchterm)) {
+    $searchterm = '';		// FIXME: do something better here?
+}
 
-     fix_magic_quotes_gpc($searchterm);
+fix_magic_quotes_gpc($searchterm);
 
-     $html = "<P><B>"
-           . sprintf(gettext ("Searching for \"%s\" ....."),
-             htmlspecialchars($searchterm))
-           . "</B></P>\n";
+$html = "<P><B>"
+      . sprintf(gettext ("Searching for \"%s\" ....."),
+        htmlspecialchars($searchterm))
+      . "</B></P>\n";
 
 // quote regexp chars
 $search = preg_quote($searchterm);
 
+
 // search matching pages
 $query = InitTitleSearch($dbi, $searchterm);
 $found = 0;
+
 while ($page = TitleSearchNextMatch($dbi, $query)) {
-  $found++;
-  $html .= LinkExistingWikiWord($page) . "<br>\n";
+    $found++;
+    $html .= LinkExistingWikiWord($page) . "<br>\n";
 }
 
 $html .= "<hr noshade>\n"
