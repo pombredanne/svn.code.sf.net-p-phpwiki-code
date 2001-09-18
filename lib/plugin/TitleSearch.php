6011bab7bd408ce7a4c7bbe2f2be1b6e1f8d0714
dairiki <dairiki@96ab9672-09ca-45d6-a79d-3d69d39ca109>
2001-09-18 19:19:05 +0000

    Jeff's hacks II, continued:
    More new files which I neglected to add during the previous commit.

---
+++ lib/plugin/TitleSearch.php
@@ -0,0 +1,66 @@
+<?php // -*-php-*-
+rcs_id('$Id: TitleSearch.php,v 1.1 2001-09-18 19:19:05 dairiki Exp $');
+
+require_once('lib/TextSearchQuery.php');
+
+/**
+ */
+class WikiPlugin_TitleSearch
+extends WikiPlugin
+{
+    var $name = 'TitleSearch';
+    
+    function getDefaultArguments() {
+        // FIXME: how to exclude multiple pages?
+        return array('s'		=> false,
+                     'auto_redirect'	=> false,
+                     'noheader'		=> false);
+    }
+
+    function getDefaultFormArguments() {
+        $defaults = parent::getDefaultFormArguments();
+        $defaults['description'] = 'Title Search';
+        return $defaults;
+    }
+    
+    function run($dbi, $argstr, $request) {
+
+        $args = $this->getArgs($argstr, $request);
+        if (empty($args['s']))
+            return '';
+
+        extract($args);
+        
+        $query = new TextSearchQuery($s);
+        $pages = $dbi->titleSearch($query);
+        $lines = array();
+        while ($page = $pages->next()) {
+            $name = $page->getName();
+            $lines[] = Element('li', LinkExistingWikiWord($name));
+            $last_name = $name;
+        }
+
+        if ($auto_redirect && count($lines) == 1)
+            $request->redirect(WikiURL($last_name));
+
+        $html = '';
+        if (!$noheader)
+            $html .= QElement('p',
+                              sprintf(gettext("Title search results for '%s'"), $s));
+        if ($lines)
+            $html .= Element('ul', join("\n", $lines));
+        else
+            $html .= Element('dl', QElement('dd', gettext("<no matches>")));
+        
+        return $html;
+    }
+};
+        
+// Local Variables:
+// mode: php
+// tab-width: 8
+// c-basic-offset: 4
+// c-hanging-comment-ender-p: nil
+// indent-tabs-mode: nil
+// End:   
+?>
