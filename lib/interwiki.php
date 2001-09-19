531145d41b46832bbba1cc77435edcb8406e0dbc
wainstead <wainstead@96ab9672-09ca-45d6-a79d-3d69d39ca109>
2001-09-19 02:36:24 +0000

    Added new local variables list; reformatted leading whitespace.

--- lib/interwiki.php
+++ lib/interwiki.php
@@ -1,66 +1,70 @@
-<?php rcs_id('$Id: interwiki.php,v 1.6 2001-09-18 19:16:23 dairiki Exp $');
+<?php rcs_id('$Id: interwiki.php,v 1.7 2001-09-19 02:36:24 wainstead Exp $');
 
 function generate_interwikimap_and_regexp()
 {
-   global $interwikimap_file, $InterWikiLinkRegexp, $interwikimap;
+    global $interwikimap_file, $InterWikiLinkRegexp, $interwikimap;
 
-   $intermap_data = file(INTERWIKI_MAP_FILE, 1);
-   $wikiname_regexp = "";
-   for ($i=0; $i<count($intermap_data); $i++)
-   {
-      list( $wiki, $inter_url ) = split(' ', chop($intermap_data[$i]));
-      $interwikimap[$wiki] = $inter_url;
-      if ($wikiname_regexp)
-	 $wikiname_regexp .= "|";
-      $wikiname_regexp .= $wiki;
-   }
+    $intermap_data = file(INTERWIKI_MAP_FILE, 1);
+    $wikiname_regexp = "";
+    for ($i=0; $i<count($intermap_data); $i++)
+        {
+            list( $wiki, $inter_url ) = split(' ', chop($intermap_data[$i]));
+            $interwikimap[$wiki] = $inter_url;
+            if ($wikiname_regexp)
+                $wikiname_regexp .= "|";
+            $wikiname_regexp .= $wiki;
+        }
 
-   $InterWikiLinkRegexp = "($wikiname_regexp)";
+    $InterWikiLinkRegexp = "($wikiname_regexp)";
 }
 
 generate_interwikimap_and_regexp();
 
 function LinkInterWikiLink($link, $linktext='')
 {
-   global $interwikimap;
+    global $interwikimap;
 
-   list( $wiki, $page ) = split( ":", $link, 2 );
+    list( $wiki, $page ) = split( ":", $link, 2 );
 
-   $url = $interwikimap[$wiki];
+    $url = $interwikimap[$wiki];
 
-   // Urlencode page only if it's a query arg.
-   if (strstr($url, '?'))
-      $url .= rawurlencode($page);
-   else
-      $url .= $page;
+    // Urlencode page only if it's a query arg.
+    if (strstr($url, '?'))
+        $url .= rawurlencode($page);
+    else
+        $url .= $page;
 
-   if ($linktext) {
-      $linktext = htmlspecialchars($linktext);
-      $class = 'named-interwiki';
-   }
-   else {
-      $linktext = ( htmlspecialchars("$wiki:")
-                    . QElement('span', array('class' => 'wikipage'), $page) );
-      $class = 'interwiki';
-   }
-   
-   return Element('a', array('href' => $url,
-			     'class' => $class),
-		  $linktext);
+    if ($linktext) {
+        $linktext = htmlspecialchars($linktext);
+        $class = 'named-interwiki';
+    }
+    else {
+        $linktext = ( htmlspecialchars("$wiki:")
+                      . QElement('span', array('class' => 'wikipage'), $page) );
+        $class = 'interwiki';
+    }
+
+    return Element('a', array('href' => $url,
+                              'class' => $class),
+                   $linktext);
 }
 
 // Link InterWiki links
 // These can be protected by a '!' like Wiki words.
 function wtt_interwikilinks($match, &$trfrm)
 {
-   if ($match[0] == "!")
-      return htmlspecialchars(substr($match,1));
-   return LinkInterWikiLink($match);
+    if ($match[0] == "!")
+        return htmlspecialchars(substr($match,1));
+    return LinkInterWikiLink($match);
 }
 
 // For emacs users
 // Local Variables:
 // mode: php
-// c-file-style: "ellemtel"
-// End:   
+// tab-width: 8
+// c-basic-offset: 4
+// c-hanging-comment-ender-p: nil
+// indent-tabs-mode: nil
+// End:
+
 ?>
