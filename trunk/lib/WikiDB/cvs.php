9af1d1ac2506314cb34ab4baccf7c255165e6399
riessen <riessen@96ab9672-09ca-45d6-a79d-3d69d39ca109>
2001-11-08 11:01:16 +0000

    extended the functionality of the cvs backend, extended the unit test for the backend. have yet to start on th

--- trunk/lib/WikiDB/cvs.php
+++ trunk/lib/WikiDB/cvs.php
@@ -1,6 +1,6 @@
 <?php
 
-rcs_id( '$Id: cvs.php,v 1.1 2001-09-28 14:28:21 riessen Exp $' );
+rcs_id( '$Id: cvs.php,v 1.2 2001-11-08 11:01:16 riessen Exp $' );
 
 require_once( 'lib/WikiDB.php' );
 require_once( 'lib/WikiDB/backend/cvs.php' );
@@ -10,11 +10,17 @@ require_once( 'lib/WikiDB/backend/cvs.php' );
  *
  * Author: Gerrit Riessen, gerrit.riessen@open-source-consultants.de
  */
-class WikiDB_cvs 
+class WikiDB_cvs
 extends WikiDB
-{
-    function WikiDB_cvs( $dbparams ) {
-	$backend = new WikiDB_backend_cvs( $dbparams );
+{  
+    var $_backend;
+
+    /**
+     * Constructor requires the DB parameters. 
+     */
+    function WikiDB_cvs( $dbparams ) 
+    {
+        $this->_backend = new WikiDB_backend_cvs( $dbparams );
     }
 }
 ?>
