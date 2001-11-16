2eaa39061416936d81d27021617ee1c3469c43b2
nobody <nobody@96ab9672-09ca-45d6-a79d-3d69d39ca109>
2001-11-16 02:53:22 +0000

    This commit was manufactured by cvs2svn to create tag 'release-1_2_2'.

---
+++ tags/release-1_2_2/lib/display.php
@@ -0,0 +1,40 @@
+<?php
+   // display.php: fetch page or get default content
+   // calls transform.php for actual transformation of wiki markup to HTML
+   rcs_id('$Id: display.php,v 1.5.2.2 2001-11-06 20:41:56 dairiki Exp $');
+ 
+   // if we got GET data, the first item is always a page name
+   // if it wasn't this file would not have been included
+
+   if (empty($QUERY_STRING) && isset($argv[0]))
+      $QUERY_STRING = $argv[0];
+  
+   if (isset($QUERY_STRING) && preg_match('/^[-_.+%\w]+$/', $QUERY_STRING)) {
+      $pagename = urldecode($QUERY_STRING);
+   } else { 
+      $pagename = gettext("FrontPage");
+
+      // if there is no FrontPage, create a basic set of Wiki pages
+      if (! IsWikiPage($dbi, $pagename)) {
+         include "lib/setupwiki.php";
+      }
+   }
+
+   $html = "";
+   $enc_name = rawurlencode($pagename);
+   $pagehash = RetrievePage($dbi, $pagename, $WikiPageStore);
+
+   // we render the page if it exists, else ask the user to write one.
+   if (is_array($pagehash)) {
+      // transform.php returns $html containing all the HTML markup
+      include("lib/transform.php");
+   } else {
+      $html .= sprintf(gettext("Describe %s here."),
+		       "$pagename<a href='$ScriptUrl?edit=$enc_name'>?</a>");
+   }
+
+   GeneratePage('BROWSE', $html, $pagename, $pagehash);
+   flush();
+
+   IncreaseHitCount($dbi, $pagename);
+?>
---
+++ tags/release-1_2_2/locale/translate.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+# translate.sh
+#
+# Usage:
+#
+#   ./locale/translate.sh
+#
+# This script should be run by
+#
+#    * PphWiki maintainers, before making a distribution
+#    * Translators, after making a translation update
+#
+
+if [ ! -f index.php ]; then
+	echo Usage: ./locale/translate.sh
+	exit 2
+fi
+
+ALL_LINGUAS="nl es de sv it"
+
+xgettext -L C++ -o locale/po/phpwiki.pot admin.php lib/*php
+podir=locale/po
+for i in $ALL_LINGUAS; do
+	po=$podir/$i.po
+	pot=$podir/phpwiki.pot
+	locale=locale/$i/LC_MESSAGES
+
+	msgmerge -o $po $po $pot
+	mkdir -p $locale
+	msgfmt -o $locale/phpwiki.mo $po
+
+	awk -- '
+BEGIN {
+  msgid=""; msgstr="";
+  print ("<?php\n");
+}
+/^msgid ""/ {
+  if (msgid && str)
+    print ("$locale[\"" msgid "\"] =\n   \"" str "\";");
+  str="";
+  next;
+}
+/^msgid "/ { #"{
+  if (msgid && str)
+    print ("$locale[\"" msgid "\"] =\n   \"" str "\";");
+  str = substr ($0, 8, length ($0) - 8);
+  msgstr="";
+}
+/^msgstr ""/ {
+  msgid=str;
+  str="";
+  next;
+}
+/^msgstr "/ { #"{
+  msgid=str;
+  str = substr ($0, 9, length ($0) - 9);
+  next;
+}
+/^"/ { #"{
+  str = (str substr ($0, 2, length ($0) - 2));
+  next;
+}
+END {
+  if (msgid && str)
+    print ("$locale[\"" msgid "\"] =\n   \"" str "\";");
+  print ("\n;?>");
+}
+
+' $po > $locale/phpwiki.php
+done
