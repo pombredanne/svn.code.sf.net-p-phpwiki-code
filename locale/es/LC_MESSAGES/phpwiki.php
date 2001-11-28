1bbca56887714eb6645cd88baa0caea953310e9b
dairiki <dairiki@96ab9672-09ca-45d6-a79d-3d69d39ca109>
2001-11-28 18:55:34 +0000

    Patches to Swedish translations from Jon Åslund <d98-jas@nada.kth.se>
    Rebuilt .po/.mo files (i.e. ran locale/translate.sh).

--- locale/es/LC_MESSAGES/phpwiki.php
+++ locale/es/LC_MESSAGES/phpwiki.php
@@ -74,18 +74,24 @@ $locale["Error message: %s"] =
    "Mensaje de error: %s";
 $locale["Cannot open database %s, giving up."] =
    "No se puede abrir la base de datos %s , me doy por vencido.";
-$locale["Insert/update failed: %s"] =
+$locale["Insert/update to table 'wiki' failed: %s"] =
    "Inserci&oacute;n/Actualizaci&oacute;n fall&oacute;: %s";
 $locale["Delete on %s failed: %s"] =
    "Eliminaci&oacute;n en %s fall&oacute;: %s";
 $locale["Insert into %s failed: %s"] =
    "Inserci&oacute;n en %s fall&oacute;: %s";
+$locale["Insert/update into table 'archive' failed: %s"] =
+   "Inserci&oacute;n/Actualizaci&oacute;n fall&oacute;: %s";
+$locale["MSSQL error: %s"] =
+   "Error de MySQL: %s";
+$locale["Insert/Update failed: %s <br>\n"] =
+   "Inserci&oacute;n/Actualizaci&oacute;n fall&oacute;: %s";
+$locale["Cannot delete '%s' from table '%s'"] =
+   "No se puede borrar '%s' de la tabla '%s'";
 $locale["MySQL error: %s"] =
    "Error de MySQL: %s";
 $locale["Error writing page '%s'"] =
    "Error al escribir la p&aacute;gina `%s'";
-$locale["Cannot delete '%s' from table '%s'"] =
-   "No se puede borrar '%s' de la tabla '%s'";
 $locale["Enter a page name"] =
    "Escriba un nombre de p&aacute;gina";
 $locale["Go"] =
@@ -130,8 +136,6 @@ $locale["WikiFatalError"] =
    "ErrorFatalDeWiki";
 $locale["Search"] =
    "Buscar";
-$locale["Nesting depth exceeded in SetHTMLOutputMode"] =
-   "L&iacute;mites de la pila excedidos en SetHTMLOutputMode";
 $locale["%d best incoming links:"] =
    "%d mejores enlaces de entrada:";
 $locale["%d best outgoing links:"] =
