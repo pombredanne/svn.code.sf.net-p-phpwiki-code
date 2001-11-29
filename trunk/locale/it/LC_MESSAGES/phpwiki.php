9f2d92b80e43baf805fd21d164cc0041bfff4a7b
dairiki <dairiki@96ab9672-09ca-45d6-a79d-3d69d39ca109>
2001-11-29 03:09:29 +0000

    Re-make in locale (to update .po and .mo files).

--- trunk/locale/it/LC_MESSAGES/phpwiki.php
+++ trunk/locale/it/LC_MESSAGES/phpwiki.php
@@ -4,64 +4,22 @@ $locale["%d best incoming links:"] =
    "%d migliori link in entrata:";
 $locale["%d best outgoing links:"] =
    "%s migliori link in uscita:";
-$locale["%d matches found in %d pages."] =
-   "Ho trovato %d occorrente in %d pagine.";
 $locale["%d most popular nearby:"] =
    "%d pi&ugrave; popolari:";
 $locale["<b>Reload</b> the page. You should now see the most current version of the page. Your changes are no longer there."] =
    "<b>Aggiorna</b> la pagina. Dovresti vedere l'ultima versione senzamodifiche";
-$locale["Archived page:"] =
-   "Pagina archiviata:";
-$locale["Archived version"] =
-   "Versione archiviata";
-$locale["Cannot delete '%s' from table '%s'"] =
-   "Non posso cancellare '%s' dalla tabella '%s'";
-$locale["Cannot establish connection to database, giving up."] =
-   "Non riesco a stabilire una connessione col database, abbandono.";
-$locale["Cannot open database %s, giving up."] =
-   "Non riesco ad aprire il database %s, abbandono.";
-$locale["Copy of %s"] =
-   "Copia di %s";
 $locale["Copy your changes to the clipboard or to another temporary place (e.g. text editor)."] =
    "Copia le tue modifiche nella clipboard o temporaneamente in unaltro posto (es: un editor di testo)";
-$locale["Current page:"] =
-   "Pagina corrente:";
-$locale["Current version"] =
-   "Versione corrente";
-$locale["Delete on %s failed: %s"] =
-   "Cancellazione su %s fallita: %s";
-$locale["Describe %s here."] =
-   "Descrivi %s qui";
-$locale["Diff of %s."] =
-   "Differenza di %s";
-$locale["Error message: %s"] =
-   "Messaggio d'errore: %s";
-$locale["Error writing page '%s'"] =
-   "Errore durante il salvataggio della pagina '%s'";
-$locale["Full Text Search Results"] =
-   "Risultati della ricerca su tutto il testo";
-$locale["Function not yet implemented."] =
-   "Funzione non implementata.";
 $locale["HomePage"] =
    "PaginaPrincipale";
 $locale["In order to recover from this situation follow these steps:"] =
    "Per risolvere questa situazione segui questi passi:";
-$locale["Insert into %s failed: %s"] =
-   "Inserimento in %s fallito: %s";
-$locale["Insert/update failed: %s"] =
-   "Inserimento/aggiornamento fallito: %s";
 $locale["Make changes to the file again. Paste your additions from the clipboard (or text editor)."] =
    "Cambia ancora il file ed installa le tue modifiche dalla clipboard (o dall'editor di testo)";
-$locale["MySQL error: %s"] =
-   "Errore di MySQL: %s";
 $locale["None"] =
    "Nessuno";
 $locale["Otherwise press the \"Back\" button of your browser."] =
    "Altrimenti premi il bottone \"Precedente\" sul tuo browser";
-$locale["Page name '%s' is not in the database"] =
-   "La pagina '%s' non e` presente nel database";
-$locale["PageInfo"] =
-   "InformazioniDellaPagina";
 $locale["PhpWiki is unable to save your changes, because another user edited and saved the page while you were editing the page too. If saving proceeded now changes from the previous author would be lost."] =
    "PhpWiki non &egrave; riuscito a salvare i tuoi cambiamenti perch&egrave; un altro utente ha cambiato la pagina mentre la stavi modificando. Se il salvataggio viene comunque fatto, i cambiamenti dell'altro utente verranno persi.";
 $locale["Press <b>Save</b> again."] =
@@ -70,16 +28,10 @@ $locale["Problem while editing %s"] =
    "Si sono verificati problemi durante la modifica %s";
 $locale["Problem while updating %s"] =
    "Problemi durante l'aggiornamento %s";
-$locale["RecentChanges"] =
-   "ModificheRecenti";
 $locale["Remove page"] =
    "Rimuovi la pagina";
 $locale["Removed page '%s' succesfully."] =
    "La pagina '%s' &egrave; stata rimossa correttamente.";
-$locale["Search"] =
-   "Ricerca";
-$locale["Searching for \"%s\" ....."] =
-   "Sto cercando \"%s\" .....";
 $locale["Sorry for the inconvenience."] =
    "Scusa per l'inconveniente.";
 $locale["Thank you for editing %s."] =
@@ -110,7 +62,5 @@ $locale["by %s"] =
    "di %s";
 $locale["last modified on %s"] =
    "Ultima modifica: %s";
-$locale["version %s"] =
-   "versione %s";
 
 ;?>
--- trunk/locale/sv/LC_MESSAGES/phpwiki.php
+++ trunk/locale/sv/LC_MESSAGES/phpwiki.php
@@ -4,68 +4,22 @@ $locale["%d best incoming links:"] =
    "De %d mest använda ingående länkarna:";
 $locale["%d best outgoing links:"] =
    "De %d mest använda utgående länkarna:";
-$locale["%d matches found in %d pages."] =
-   "%d träffar funna på %d sidor.";
 $locale["%d most popular nearby:"] =
    "De %d mest populära grannarna:";
-$locale["%d pages match your query."] =
-   "%d sidor matchar din fråga.";
-$locale["(first day for this Wiki)"] =
-   "(första dagen för den här Wikin)";
 $locale["<b>Reload</b> the page. You should now see the most current version of the page. Your changes are no longer there."] =
    "<b>Ladda om</b> sidan. Du kommer nu se den senaste versionen av sidan. Dina ändringar är inte där.";
-$locale["Archived page:"] =
-   "Arkiverad sida:";
-$locale["Archived version"] =
-   "Arkiverad version";
-$locale["Cannot delete '%s' from table '%s'"] =
-   "Kan ej ta bort \"%s\" från tabellen \"%s\"";
-$locale["Cannot establish connection to database, giving up."] =
-   "Ger upp, kan ej skapa anslutning till databasen.";
-$locale["Cannot open database %s, giving up."] =
-   "Ger upp, kan ej öppna databasen %s.";
-$locale["Copy of %s"] =
-   "Kopia av %s";
 $locale["Copy your changes to the clipboard or to another temporary place (e.g. text editor)."] =
    "Kopiera dina ändringar till urklippet eller till ett annat temporärt ställe (t.ex. en texteditor).";
-$locale["Current page:"] =
-   "Nuvarande sida:";
-$locale["Current version"] =
-   "Nuvarande version";
-$locale["Delete on %s failed: %s"] =
-   "Borttagning på %s misslyckades: %s";
-$locale["Describe %s here."] =
-   "Beskriv %s här.";
-$locale["Diff of %s."] =
-   "Diff av %s.";
-$locale["Error message: %s"] =
-   "Felmeddelande: %s";
-$locale["Error writing page '%s'"] =
-   "Kunde inte spara sidan \"%s\"";
-$locale["Full Text Search Results"] =
-   "Sökresultat för fulltextsökning";
-$locale["Function not yet implemented."] =
-   "Funktionen är ännu inte implementerad.";
 $locale["HomePage"] =
    "Framsida";
 $locale["In order to recover from this situation follow these steps:"] =
    "För att lösa situtationen, följ dessa steg:";
-$locale["Insert into %s failed: %s"] =
-   "Insättning till %s misslyckades: %s";
-$locale["Insert/update failed: %s"] =
-   "Insättning/uppdatering misslyckades: %s";
 $locale["Make changes to the file again. Paste your additions from the clipboard (or text editor)."] =
    "Ändra filen igen. Klistra in dina ändringar från urklippet (eller texteditorn).";
-$locale["MySQL error: %s"] =
-   "MySQL-fel: %s";
 $locale["None"] =
    "ingen";
 $locale["Otherwise press the \"Back\" button of your browser."] =
    "Annars klicka på backaknappen i din webbläsare.";
-$locale["Page name '%s' is not in the database"] =
-   "Sidnamnet \"%s\" finns inte i databasen";
-$locale["PageInfo"] =
-   "Sidinformation för";
 $locale["PhpWiki is unable to save your changes, because another user edited and saved the page while you were editing the page too. If saving proceeded now changes from the previous author would be lost."] =
    "PhpWiki kan inte spara dina ändringar eftersom en annan användare ändrade och sparade sidan under tiden som du ändrade på sidan. Sparar du nu försvinner ändringarna gjorda av den förra användaren.";
 $locale["Press <b>Save</b> again."] =
@@ -74,22 +28,12 @@ $locale["Problem while editing %s"] =
    "Har problem med att redigera %s";
 $locale["Problem while updating %s"] =
    "Har problem med att uppdatera %s";
-$locale["Quick title search:"] =
-   "Snabb titelsökning:";
-$locale["RecentChanges"] =
-   "SenasteÄndringar";
 $locale["Remove page"] =
    "Ta bort sida";
-$locale["Search"] =
-   "Sök";
-$locale["Searching for \"%s\" ....."] =
-   "Söker efter \"%s\" .....";
 $locale["Sorry for the inconvenience."] =
    "Ledsen för allt besvär.";
 $locale["Thank you for editing %s."] =
    "Tack för att du redigerade %s.";
-$locale["The most recently changed pages are listed below."] =
-   "De senaste ändrade sidorna listas nedan.";
 $locale["This page has been locked by the administrator and cannot be edited."] =
    "Den här sidan är låst av administratören och kan ej redigeras.";
 $locale["Use your browser's <b>Back</b> button to go back to the edit page."] =
@@ -116,7 +60,5 @@ $locale["by %s"] =
    "av %s";
 $locale["last modified on %s"] =
    "senast ändrad %s";
-$locale["version %s"] =
-   "version %s";
 
 ;?>
