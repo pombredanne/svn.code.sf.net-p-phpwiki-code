8b3c6e9366b4623dde1d8fe49ec1cd155a27ac2f
carstenklapp <carstenklapp@96ab9672-09ca-45d6-a79d-3d69d39ca109>
2001-12-04 23:49:33 +0000

    Converted tabs to spaces, mysql doesn't like tabs :-(

--- schemas/mysql.sql
+++ schemas/mysql.sql
@@ -1,46 +1,46 @@
--- $Id: mysql.sql,v 1.1 2001-12-03 04:05:38 wainstead Exp $
+-- $Id: mysql.sql,v 1.2 2001-12-04 23:49:33 carstenklapp Exp $
 
 drop table if exists page;
 CREATE TABLE page (
-	id		INT NOT NULL,
-        pagename	VARCHAR(100) BINARY NOT NULL,
-	hits		INT NOT NULL DEFAULT 0,
-        pagedata	MEDIUMTEXT NOT NULL DEFAULT '',
+	id              INT NOT NULL,
+        pagename        VARCHAR(100) BINARY NOT NULL,
+	hits            INT NOT NULL DEFAULT 0,
+        pagedata        MEDIUMTEXT NOT NULL DEFAULT '',
         PRIMARY KEY (id),
 	UNIQUE KEY (pagename)
 );
 
 drop table if exists version;
 CREATE TABLE version (
-	id		INT NOT NULL,
-        version		INT NOT NULL,
-	mtime		INT NOT NULL,
-	minor_edit	TINYINT DEFAULT 0,
-        content		MEDIUMTEXT NOT NULL DEFAULT '',
-        versiondata	MEDIUMTEXT NOT NULL DEFAULT '',
+	id              INT NOT NULL,
+        version         INT NOT NULL,
+	mtime           INT NOT NULL,
+	minor_edit      TINYINT DEFAULT 0,
+        content         MEDIUMTEXT NOT NULL DEFAULT '',
+        versiondata     MEDIUMTEXT NOT NULL DEFAULT '',
         PRIMARY KEY (id,version),
 	INDEX (mtime)
 );
 
 drop table if exists recent;
 CREATE TABLE recent (
-	id		INT NOT NULL,
-	latestversion	INT,
-	latestmajor	INT,
-	latestminor	INT,
+	id              INT NOT NULL,
+	latestversion   INT,
+	latestmajor     INT,
+	latestminor     INT,
         PRIMARY KEY (id)
 );
 
 drop table if exists nonempty;
 CREATE TABLE nonempty (
-	id		INT NOT NULL,
+	id              INT NOT NULL,
 	PRIMARY KEY (id)
 );
 
 drop table if exists link;
 CREATE TABLE link (
-        linkfrom	INT NOT NULL,
-        linkto		INT NOT NULL,
+        linkfrom        INT NOT NULL,
+        linkto          INT NOT NULL,
 	INDEX (linkfrom),
         INDEX (linkto)
 );
