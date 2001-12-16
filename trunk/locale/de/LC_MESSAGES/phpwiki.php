4201ac7f3843f74eda9ced4475ce20d5e714dd8d
carstenklapp <carstenklapp@96ab9672-09ca-45d6-a79d-3d69d39ca109>
2001-12-16 03:25:14 +0000

    Recommitting my previous German updates to the phpwiki.php locale file (with the correct ISO translation enabled in my CVS!)

--- trunk/locale/de/LC_MESSAGES/phpwiki.php
+++ trunk/locale/de/LC_MESSAGES/phpwiki.php
@@ -5,15 +5,15 @@ $locale["%d best incoming links:"] =
 $locale["%d best outgoing links:"] =
    "Die %d besten hinausgehenden Links:";
 $locale["%d most popular nearby:"] =
-   "Die %d meistbesuchten Seiten in der NŠhe:";
+   "Die %d meistbesuchten Seiten in der Nähe:";
 $locale[", saved on"] =
-   ", seite zuletzt verŠndert am";
+   ", seite zuletzt verändert am";
 $locale["<b>Reload</b> the page. You should now see the most current version of the page. Your changes are no longer there."] =
-   "<b>Aktualisiere</b> die Seite. Du solltest jetzt die neueste Versionder Seite sehen. Deine VerŠnderungen sind verschwunden.";
+   "<b>Aktualisiere</b> die Seite. Du solltest jetzt die neueste Versionder Seite sehen. Deine Veränderungen sind verschwunden.";
 $locale["Click"] =
    "Klicke";
 $locale["Copy your changes to the clipboard or to another temporary place (e.g. text editor)."] =
-   "Kopiere deine VerŠnderungen in die Zwischenablage oder an einen anderentemporŠren Platz (z.B. einen Texteditor)";
+   "Kopiere deine Veränderungen in die Zwischenablage oder an einen anderentemporären Platz (z.B. einen Texteditor)";
 $locale["Describe [%s] here."] =
    "Beschreibe [%s] hier.";
 $locale["Edit"] =
@@ -29,64 +29,64 @@ $locale["HomePage"] =
 $locale["In order to recover from this situation follow these steps:"] =
    "Um diese Situation zu korrigieren folge bitte den folgenden Schritten:";
 $locale["Last edited on"] =
-   "Zuletzt verŠndert am";
+   "Zuletzt verändert am";
 $locale["Lock page"] =
    "Seite sperren";
 $locale["major edits"] =
-   "HauptŠnderungen";
+   "Hauptänderungen";
 $locale["Make changes to the file again. Paste your additions from the clipboard (or text editor)."] =
-   "Editiere die Seite erneut. Kopiere deine VerŠnderungen aus derZwischenablage (oder aus dem Texteditor)";
+   "Editiere die Seite erneut. Kopiere deine Veränderungen aus derZwischenablage (oder aus dem Texteditor)";
 $locale["minor edits"] =
-   "geringe €nderungen";
+   "geringe Änderungen";
 $locale["None"] =
    "Keine";
 $locale["Otherwise press the \"Back\" button of your browser."] =
-   "Anderenfalls ben¸tze die \"Zur¸ck\" Funktion deines Browsers.";
+   "Anderenfalls benütze die \"Zurück\" Funktion deines Browsers.";
 $locale["PhpWiki is unable to save your changes, because another user edited and saved the page while you were editing the page too. If saving proceeded now changes from the previous author would be lost."] =
-   "PhpWiki kann deine VerŠnderungen nicht speichern, da ein andererBen¸tzer die Seite verŠndert und gespeichert hat, wŠhrend du diese Seiteeditiert hast. Wenn die Seite jetzt gespeichert werden w¸rde, dann w¸rdendie VerŠnderungen des anderen Ben¸tzers verloren gehen.";
+   "PhpWiki kann deine Veränderungen nicht speichern, da ein andererBenützer die Seite verändert und gespeichert hat, während du diese Seiteeditiert hast. Wenn die Seite jetzt gespeichert werden würde, dann würdendie Veränderungen des anderen Benützers verloren gehen.";
 $locale["Press <b>Save</b> again."] =
-   "Dr¸cke nochmal <b>Speichern</b>";
+   "Drücke nochmal <b>Speichern</b>";
 $locale["Problem while editing %s"] =
-   "Problem wŠhrend dem Editieren von %s";
+   "Problem während dem Editieren von %s";
 $locale["RecentChanges"] =
    "FrischeSeiten";
 $locale["Remove page"] =
-   "Seite lšschen";
+   "Seite löschen";
 $locale["Removed page '%s' succesfully."] =
-   "Seite '%s' erfolgreich gelšscht.";
+   "Seite '%s' erfolgreich gelöscht.";
 $locale["Thank you for editing %s."] =
-   "Danke f¸r das Editieren von %s.";
+   "Danke für das Editieren von %s.";
 $locale["The %d most recent %s are listed below."] =
    "Eine Liste der %d neuesten %s.";
 $locale["The %d most recent %s during the past %.1f days are listed below."] =
    "Eine Liste der %d neuesten %s innerhalb von %.1f Tagen.";
 $locale["The most recent %s during the past %.1f days are listed below."] =
-   "Eine Liste der J¸ngste %s innerhalb von %.1f Tagen.";
+   "Eine Liste der Jüngste %s innerhalb von %.1f Tagen.";
 $locale["This is a minor change."] =
-   "Geringf¸gige €nderung";
+   "Geringfügige Änderung";
 $locale["This page has been locked by the administrator and cannot be edited."] =
    "Diese Seite wurde vom Administrator gesperrt und kann nicht editiert werden.";
 $locale["Unlock page"] =
    "Seite freigeben";
 $locale["Use your browser's <b>Back</b> button to go back to the edit page."] =
-   "Ben¸tze die <b>Zur¸ck</b> Funktion deines Browsers um zur Editierseitezu gelangen.";
+   "Benütze die <b>Zurück</b> Funktion deines Browsers um zur Editierseitezu gelangen.";
 $locale["Versions are identical"] =
    "Versionen sind identisch";
 $locale["WikiFatalError"] =
    "FatalerWikiFehler";
 $locale["You are about to remove '%s' permanently!"] =
-   "Du bist im Begriff '%s' permanent zu lšschen!";
+   "Du bist im Begriff '%s' permanent zu löschen!";
 $locale["You entered an invalid login or password."] =
-   "Du hast ein ung¸ltiges Login oder Passwort eingegeben.";
+   "Du hast ein ungültiges Login oder Passwort eingegeben.";
 $locale["Your careful attention to detail is much appreciated."] =
-   "Deine besondere Aufmerksamkeit f¸r Details wird sehr geschŠtzt.";
+   "Deine besondere Aufmerksamkeit für Details wird sehr geschätzt.";
 $locale["by %s"] =
    "von %s";
 $locale["here"] =
    "hier";
 $locale["last modified on %s"] =
-   "zuletzt verŠndert am %s";
+   "zuletzt verändert am %s";
 $locale["tips for editing."] =
-   "tips f¸rs Editieren.";
+   "tips fürs Editieren.";
 
 ;?>
