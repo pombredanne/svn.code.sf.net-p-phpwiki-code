c8ca42095b4959dbd0cc46337b266c818f4a6d52
carstenklapp <carstenklapp@96ab9672-09ca-45d6-a79d-3d69d39ca109>
2001-12-26 11:38:27 +0000

    The first experimental (and very alpha) theme pack is here, have some fun and play with it, fix it up if you like. This one is (by design) completely css-based so unfortunately it doesn't render properly or even the same across different browsers. A preview screen snapshot is also included for comparison testing. Additional notes are in the included themeinfo.php file.

---
+++ trunk/themes/default/themeinfo.php
@@ -0,0 +1,50 @@
+<?php
+
+// FIXME: these files are moved to a subfolder in /templates
+// e.g. /templates/vanilla so relative paths to index.php
+// should be irrelevant.
+
+// The current .htaccess in /templates generates an error in apache 1.3.20.
+// It should be removed or changed to allow access for themes to work.
+
+// If you specify a relative URL for the CSS and images,
+// the are interpreted relative to DATA_PATH (see below).
+// (The default value of DATA_PATH is the directory in which
+// index.php (this file) resides.)
+
+// CSS location
+//
+// Note that if you use the stock phpwiki style sheet, 'phpwiki.css',
+// you should make sure that it's companion 'phpwiki-heavy.css'
+// is installed in the same directory that the base style file is.
+define('CSS_URL', "themes/$theme/phpwiki.css");
+
+// logo image
+$logo = "themes/$theme/wikibase.png";
+
+// Signature image which is shown after saving an edited page
+// If this is left blank (or unset), the signature will be omitted.
+$SignatureImg = "themes/$theme/signature.png";
+
+$templates = array('BROWSE' =>    "themes/$theme/templates/browse.html",
+		   'EDITPAGE' =>  "themes/$theme/templates/editpage.html",
+		   'MESSAGE' =>   "themes/$theme/templates/message.html");
+
+$URL_LINK_ICONS = array(
+                    'http'	=> "themes/$theme/icons/http.png",
+                    'https'	=> "themes/$theme/icons/https.png",
+                    'ftp'	=> "themes/$theme/icons/ftp.png",
+                    'mailto'	=> "themes/$theme/icons/mailto.png",
+                    'interwiki' => "themes/$theme/icons/interwiki.png",
+                    '*'		=> "themes/$theme/icons/zapg.png"
+                    );
+
+// (c-file-style: "gnu")
+// Local Variables:
+// mode: php
+// tab-width: 8
+// c-basic-offset: 4
+// c-hanging-comment-ender-p: nil
+// indent-tabs-mode: nil
+// End:   
+?>
