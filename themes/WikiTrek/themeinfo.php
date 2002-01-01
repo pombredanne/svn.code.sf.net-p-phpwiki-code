8d6ed490742151299975559e5fe14210fa5a540d
carstenklapp <carstenklapp@96ab9672-09ca-45d6-a79d-3d69d39ca109>
2002-01-01 07:18:42 +0000

    comments cleanup

--- themes/WikiTrek/themeinfo.php
+++ themes/WikiTrek/themeinfo.php
@@ -1,6 +1,6 @@
 <?php
 
-rcs_id('$Id: themeinfo.php,v 1.3 2001-12-27 18:11:55 carstenklapp Exp $');
+rcs_id('$Id: themeinfo.php,v 1.4 2002-01-01 07:18:42 carstenklapp Exp $');
 
 /**
  * This PhpWiki theme is experimental and will likely not appear as 
@@ -18,48 +18,43 @@ rcs_id('$Id: themeinfo.php,v 1.3 2001-12-27 18:11:55 carstenklapp Exp $');
  * any potential visual problems can be identified. The intention is
  * to elimate as many non-html elements from the html templates as
  * possible.
- */
+ *
+ * This theme does not render properly in all browsers. In particular,
+ * OmniWeb renders some text as black-on-black. Netscape 4 will probably
+ * choke on it too.
+ * * * * * * * * */
 
-//This theme does not render properly in all browsers. In particular,
-// OmniWeb renders some text as black-on-black. Netscape 4 will probably
-// choke on it too.
-
-//use this setting in index.php:
+// To activate this theme, specify this setting in index.php:
 //$theme="WikiTrek";
 
-// FIXME: these files are moved to a subfolder in /templates
-// e.g. /templates/vanilla so relative paths to index.php
-// should be irrelevant.
-
-// The current .htaccess in /templates generates an error in apache 1.3.20.
-// It should be removed or changed to allow access for themes to work.
-
-// If you specify a relative URL for the CSS and images,
-// the are interpreted relative to DATA_PATH (see below).
-// (The default value of DATA_PATH is the directory in which
-// index.php (this file) resides.)
-
-// CSS location
-//
-// Note that if you use the stock phpwiki style sheet, 'phpwiki.css',
-// you should make sure that it's companion 'phpwiki-heavy.css'
-// is installed in the same directory that the base style file is.
-
-
+// CSS file defines fonts, colors and background images for this style.
 $CSS_URL = "themes/$theme/WikiTrek.css";
 
-// logo image
+// Logo image appears on every page.
 $logo = "themes/$theme/Ufp-logo.png";
 
 // Signature image which is shown after saving an edited page
 // If this is left blank (or unset), the signature will be omitted.
 $SignatureImg = "themes/$theme/lights.gif";
 
-//gettext() does not work here yet
+// If this theme defines any custom link icons, it will completely override
+// any link icon settings defined in index.php.
+/*
+$URL_LINK_ICONS = array(
+                    'http'	=> "themes/$theme/http.png",
+                    'https'	=> "themes/$theme/https.png",
+                    'ftp'	=> "themes/$theme/ftp.png",
+                    'mailto'    => "themes/$theme/mailto.png",
+                    'interwiki' => "themes/$theme/interwiki.png",
+                    '*'	        => "themes/$theme/zapg.png"
+                    );
+*/
+
+//gettext() does not work here because we are included from index.php
 //normally themes shouldn't override date & time settings because this will mess up
 //any future user-specific preferences for locale
-$datetimeformat = sprintf(("Stardate %s"),'%B %e, %Y');	// may contain time of day
-$dateformat = sprintf(("Stardate %s"),'%B %e, %Y');	// must not contain time
+$datetimeformat = sprintf(("Stardate %s"),'%B.%e.%Y');	// may contain time of day
+$dateformat = sprintf(("Stardate %s"),'%B.%e.%Y');	// must not contain time
 
 // (c-file-style: "gnu")
 // Local Variables:
