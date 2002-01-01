c2f224d7ec8d5c47ed86ec12b3a853c852d1d03f
carstenklapp <carstenklapp@96ab9672-09ca-45d6-a79d-3d69d39ca109>
2002-01-01 09:39:21 +0000

    *** empty log message ***

--- trunk/locale/sv/LC_MESSAGES/phpwiki.php
+++ trunk/locale/sv/LC_MESSAGES/phpwiki.php
@@ -10,14 +10,34 @@ $locale["<b>Reload</b> the page. You should now see the most current version of 
    "<b>Ladda om</b> sidan. Du kommer nu se den senaste versionen av sidan. Dina ändringar är inte där.";
 $locale["Copy your changes to the clipboard or to another temporary place (e.g. text editor)."] =
    "Kopiera dina ändringar till urklippet eller till ett annat temporärt ställe (t.ex. en texteditor).";
+$locale["Diff"] =
+   "Diff";
+$locale["Edit"] =
+   "Redigera";
+$locale["Edit: %s"] =
+   "Redigera: %s.";
+$locale["FindPage"] =
+   "SökEfterSida";
+$locale["GoodStyle"] =
+   "BraStil";
+$locale["Hits"] =
+   "antal träffar";
 $locale["HomePage"] =
    "Framsida";
 $locale["In order to recover from this situation follow these steps:"] =
    "För att lösa situtationen, följ dessa steg:";
+$locale["Lock page"] =
+   "Lås sida";
 $locale["Make changes to the file again. Paste your additions from the clipboard (or text editor)."] =
    "Ändra filen igen. Klistra in dina ändringar från urklippet (eller texteditorn).";
+$locale["MostPopular"] =
+   "MestPopulär";
 $locale["None"] =
    "ingen";
+$locale["Page Name"] =
+   "namn på sidan";
+$locale["Page locked"] =
+   "Sidan är låst";
 $locale["PhpWiki is unable to save your changes, because another user edited and saved the page while you were editing the page too. If saving proceeded now changes from the previous author would be lost."] =
    "PhpWiki kan inte spara dina ändringar eftersom en annan användare ändrade och sparade sidan under tiden som du ändrade på sidan. Sparar du nu försvinner ändringarna gjorda av den förra användaren.";
 $locale["Press <b>Save</b> again."] =
@@ -30,16 +50,32 @@ $locale["RecentChanges"] =
    "SenasteÄndringar";
 $locale["Remove page"] =
    "Ta bort sida";
+$locale["Save"] =
+   "Spara";
 $locale["Search"] =
    "Sök";
+$locale["See %s tips for editing."] =
+   "%s: Redigeringstips.";
 $locale["Sorry for the inconvenience."] =
    "Ledsen för allt besvär.";
+$locale["TextFormattingRules"] =
+   "Textformateringsregler";
 $locale["Thank you for editing %s."] =
    "Tack för att du redigerade %s.";
+$locale["The %s most popular pages of this wiki:"] =
+   "De %s mest populära sidorna för den här wikin:";
 $locale["This page has been locked by the administrator and cannot be edited."] =
    "Den här sidan är låst av administratören och kan ej redigeras.";
+$locale["Title Search"] =
+   "Titelsökningen";
+$locale["TitleSearch"] =
+   "Titelsökningen";
+$locale["Unlock page"] =
+   "Lås upp sida";
 $locale["Use your browser's <b>Back</b> button to go back to the edit page."] =
    "Använd webbläsarens <b>backaknapp</b> för att gå tillbaka till redigeringssidan.";
+$locale["Version %d"] =
+   "Version %d";
 $locale["Versions are identical"] =
    "Versionerna är identiska";
 $locale["WikiFatalError"] =
@@ -54,5 +90,7 @@ $locale["by %s"] =
    "av %s";
 $locale["last modified on %s"] =
    "senast ändrad %s";
+$locale["version %d"] =
+   "version %d";
 
 ;?>
