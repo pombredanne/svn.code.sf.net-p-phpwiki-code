d95da2a1432796d75890eef28632f36104f29aaf
carstenklapp <carstenklapp@96ab9672-09ca-45d6-a79d-3d69d39ca109>
2002-01-01 21:41:32 +0000

    Added optional random $SignatureImg. Comments cleanup.

--- trunk/themes/Hawaiian/themeinfo.php
+++ trunk/themes/Hawaiian/themeinfo.php
@@ -1,37 +1,45 @@
 <?php
 
-rcs_id('$Id: themeinfo.php,v 1.1 2001-12-31 23:24:37 carstenklapp Exp $');
+rcs_id('$Id: themeinfo.php,v 1.2 2002-01-01 21:41:32 carstenklapp Exp $');
 
-// If you specify a relative URL for the CSS and images,
-// the are interpreted relative to DATA_PATH (see below).
-// (The default value of DATA_PATH is the directory in which
-// index.php resides.)
+/**
+ * WikiWiki Hawaiian theme for PhpWiki.
+ */
 
 // To activate this theme, specify this setting in index.php:
 //$theme=Hawaiian;
+// To deactivate themes, comment out all the $theme=lines in index.php.
 
+// CSS location
+//
 // CSS file defines fonts, colors and background images for this style.
+// The companion '*-heavy.css' file isn't defined, it's just expected to
+// be in the same directory that the base style is in.
 $CSS_URL = "themes/$theme/Hawaiian.css";
 
-// Logo image appears on every page.
+// Logo image appears on every page and links to the HomePage.
 $logo = "themes/$theme/PalmBeach.jpg";
 
-// Signature image which is shown after saving an edited page
-// If this is left blank (or unset), the signature will be omitted.
-//$SignatureImg = "themes/$theme/Submersible.jpg";
+// Signature image which is shown after saving an edited page.
+// If this is left blank, any signature defined in index.php will be
+// used. If it is not defined by index.php or in here then the
+// "Thank you for editing..." screen will be omitted.
+//$SignatureImg = "themes/$theme/pictures/SubmersiblePiscesV.jpg"; #for Steve
 $SignatureImg = "themes/$theme/WaterFall.jpg";
+// If you want to see more than just the waterfall
+// let a random picture be chosen for the signature image:
+//include("themes/$theme/pictures/random.php");
 
-// If any custom xhtml template files are defined by this theme they
-// will appear here.
+// If this theme defines any templates, they will completely override
+// whatever templates have been defined in index.php.
 /*
 $templates = array('BROWSE' =>    "themes/$theme/templates/browse.html",
 		   'EDITPAGE' =>  "themes/$theme/templates/editpage.html",
 		   'MESSAGE' =>   "themes/$theme/templates/message.html");
 */
 
-// If this theme defines any custom link icons, it will completely override
+// If this theme defines any custom link icons, they will completely override
 // any link icon settings defined in index.php.
-
 $URL_LINK_ICONS = array(
 /*                    'http'	  => "themes/$theme/http.png",     */
 /*                    'https'	  => "themes/$theme/https.png",    */
@@ -41,6 +49,13 @@ $URL_LINK_ICONS = array(
                       '*'	  => "themes/$theme/flower.png"
                     );
 
+// If this theme defines any templates, they will completely override
+// whatever templates have been defined in index.php.
+/*
+$templates = array('BROWSE' =>    "themes/$theme/templates/browse.html",
+		   'EDITPAGE' =>  "themes/$theme/templates/editpage.html",
+		   'MESSAGE' =>   "themes/$theme/templates/message.html");
+*/
 
 // (c-file-style: "gnu")
 // Local Variables:
