c8f6c73d07fd20e3394e549486cf25e8c37d063a
carstenklapp <carstenklapp@96ab9672-09ca-45d6-a79d-3d69d39ca109>
2002-01-03 23:07:43 +0000

    Sorry, this theme doesn't work yet.

--- trunk/themes/MacOSX/themeinfo.php
+++ trunk/themes/MacOSX/themeinfo.php
@@ -1,6 +1,6 @@
 <?php
 
-rcs_id('$Id: themeinfo.php,v 1.2 2002-01-03 00:09:18 carstenklapp Exp $');
+rcs_id('$Id: themeinfo.php,v 1.3 2002-01-03 23:07:43 carstenklapp Exp $');
 
 /**
  * A PhpWiki theme inspired by the Aqua appearance of Mac OS X.
@@ -9,39 +9,44 @@ rcs_id('$Id: themeinfo.php,v 1.2 2002-01-03 00:09:18 carstenklapp Exp $');
  * blend in with whatever background color or texture is on the page.
  * When viewed with an older browser, the images may be incorrectly
  * rendered with a thick solid black border. When viewed with a modern
- * browser, the images will display with nice edges and blended shadows.
+ * browser, the images will display with nice edges and blended
+ * shadows.
  * */
+
+// Sorry, this theme doesn't work yet. :-( There is some more template work
+// to do before more of the theme will be committed to the repository.
+// --CarstenKlapp
 
 // To activate this theme, specify this setting in index.php:
 //$theme="MacOSX";
 // To deactivate themes, comment out all the $theme=lines in index.php.
 
-// CSS file defines fonts, colors and background images for this style.
-// The companion '*-heavy.css' file isn't defined, it's just expected to
-// be in the same directory that the base style is in.
+// CSS file defines fonts, colors and background images for this
+// style.  The companion '*-heavy.css' file isn't defined, it's just
+// expected to be in the same directory that the base style is in.
 $CSS_DEFAULT = "MacOSX";
 
-$CSS_URLS = array_merge($CSS_URLS, array( "$CSS_DEFAULT" => "themes/$theme/${CSS_DEFAULT}.css" ) );
+$CSS_URLS = array_merge($CSS_URLS, array("$CSS_DEFAULT" => "themes/$theme/${CSS_DEFAULT}.css" ));
 
 // Logo image appears on every page and links to the HomePage.
 $logo = "themes/$theme/PhpWiki.png";
 
-// Signature image which is shown after saving an edited page.
-// If this is left blank, any signature defined in index.php will be
-// used. If it is not defined by index.php or in here then the
-// "Thank you for editing..." screen will be omitted.
+// Signature image which is shown after saving an edited page.  If
+// this is left blank, any signature defined in index.php will be
+// used. If it is not defined by index.php or in here then the "Thank
+// you for editing..." screen will be omitted.
 //$SignatureImg = "themes/$theme/signature.png";
 
 // If this theme defines any templates, they will completely override
-// whatever templates have been defined in index.php.
+// whatever templates have been defined in 'index.php'.
 /*
 $templates = array('BROWSE' =>    "themes/$theme/templates/browse.html",
 		   'EDITPAGE' =>  "themes/$theme/templates/editpage.html",
 		   'MESSAGE' =>   "themes/$theme/templates/message.html");
 */
 
-// If this theme defines any custom link icons, they will completely override
-// any link icon settings defined in index.php.
+// If this theme defines any custom link icons, they will completely
+// override any link icon settings defined in 'index.php'.
 /*
 $URL_LINK_ICONS = array(
                     'http'	=> "themes/$theme/icons/http.png",
