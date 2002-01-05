775698a411fd68533d825463fb25be7bfbea0226
carstenklapp <carstenklapp@96ab9672-09ca-45d6-a79d-3d69d39ca109>
2002-01-05 06:17:02 +0000

    just reflowing text

--- themes/MacOSX/themeinfo.php
+++ themes/MacOSX/themeinfo.php
@@ -1,6 +1,6 @@
 <?php
 
-rcs_id('$Id: themeinfo.php,v 1.6 2002-01-04 02:22:21 carstenklapp Exp $');
+rcs_id('$Id: themeinfo.php,v 1.7 2002-01-05 06:17:02 carstenklapp Exp $');
 
 /**
  * A PhpWiki theme inspired by the Aqua appearance of Mac OS X.
@@ -9,53 +9,58 @@ rcs_id('$Id: themeinfo.php,v 1.6 2002-01-04 02:22:21 carstenklapp Exp $');
  * blend in with whatever background color or texture is on the page.
  * When viewed with an older browser, the images may be incorrectly
  * rendered with a thick solid black border. When viewed with a modern
- * browser, the images will display with nice edges and blended shadows.
+ * browser, the images will display with nice edges and blended
+ * shadows.
  *
  * Known Problems:
  *
  * Most of the images you will see a white area around the outside.
- * Once the icons for the buttons have been finalized, the alpha channel
- * will be added to eliminate the white parts.
+ * Once the icons for the buttons have been finalized, the alpha
+ * channel will be added to eliminate the white parts.
  *
- * The button toolbars use tables for positioning. Yuck. (It will do for now).
+ * The button toolbars use tables for positioning. Yuck. (It will do
+ * for now).
  *
- * RSS has it's own Mac OS X style button but it's not actually used yet.
+ * RSS has it's own Mac OS X style button but it's not actually used
+ * yet.
  *
- * In Mozilla, there is a grey line under each of the images. I haven't 
- * been able to determine the cause of this yet.
+ * In Mozilla, there is a grey line under each of the images. I
+ * haven't been able to determine the cause of this yet.
  *
- * Only English buttons are available. Full localization is still a ways
- * off. Work is in progress to modularize & tokenize the normal templates
- * and (somehow) some cool toolbar functions are going to result from it.
- * This should make editing the MacOSX theme template files much easier
- * as well as providing a generic localization method for any other
- * toolbars (whether image or text).
+ * Only English buttons are available. Full localization is still a
+ * ways off. Work is in progress to modularize & tokenize the normal
+ * templates and (somehow) some cool toolbar functions are going to
+ * result from it.  This should make editing the MacOSX theme template
+ * files much easier as well as providing a generic localization
+ * method for any other toolbars (whether image or text).
  *
- * There is an image for a BackLinks button but it's not used yet either.
- * For now BackLinks are still accessed by clicking in the title.
+ * There is an image for a BackLinks button but it's not used yet
+ * either.  For now BackLinks are still accessed by clicking in the
+ * title.
  *
- * The CSS is still mostly the same as phpwiki.css. I'd like to change it a
- * bit but have no specific plans yet. Just a general feeling that it should
- * look and feel like a Mac interface: with subtle effects and a fine
- * color scheme. Since I'm sick of the stripes Mr. Jobs thoughtfully plastered
- * all over my screen, I've chosen a brushed paper (or stucco?) texture effect
- * very close to white. If your monitor isn't calibrated well you might not
- * even see it.
+ * The CSS is still mostly the same as phpwiki.css. I'd like to change
+ * it a bit but have no specific plans yet. Just a general feeling
+ * that it should look and feel like a Mac interface: with subtle
+ * effects and a fine color scheme. Since I'm sick of the stripes
+ * Mr. Jobs thoughtfully plastered all over my screen, I've chosen a
+ * brushed paper (or stucco?) texture effect very close to white. If
+ * your monitor isn't calibrated well you might not even see it.
  *
- * I probably won't be submitting anything else for this theme for a bit.
- * Not until the default toolbar stuff and templates are further along anyway.
+ * I probably won't be submitting anything else for this theme for a
+ * bit.  Not until the default toolbar stuff and templates are further
+ * along anyway.
  * 
- * Send me some feedback, do you like the icons used in the buttons? Got
- * any ideas for code to pick out the localized buttons from the right
- * directory? Automatic button generation for localized buttons isn't going
- * to happen for this theme--there is a gradient across the glass surface of
- * the button that only Mac OS X Aqua can generate. Chopping a button up and
- * stamping it with localized words means a lot of tweaking to the blank
- * button pieces to get the seams invisible. So it will be a nicer effect
- * to produce them by hand.
+ * Send me some feedback, do you like the icons used in the buttons?
+ * Got any ideas for code to pick out the localized buttons from the
+ * right directory? Automatic button generation for localized buttons
+ * isn't going to happen for this theme--there is a gradient across
+ * the glass surface of the button that only Mac OS X Aqua can
+ * generate. Chopping a button up and stamping it with localized words
+ * means a lot of tweaking to the blank button pieces to get the seams
+ * invisible. So it will be a nicer effect to produce them by hand.
  *
- * The current link icons I want to move into this theme, and come up with
- * some new linkicons for the default look. (Comments, feedback?)
+ * The current link icons I want to move into this theme, and come up
+ * with some new linkicons for the default look. (Comments, feedback?)
  *
  * */
 
@@ -63,46 +68,47 @@ rcs_id('$Id: themeinfo.php,v 1.6 2002-01-04 02:22:21 carstenklapp Exp $');
 //$theme="MacOSX";
 // To deactivate themes, comment out all the $theme=lines in index.php.
 
-// CSS location
-//
-// CSS file defines fonts, colors and background images for this style.
-// The companion '*-heavy.css' file isn't defined, it's just expected to
-// be in the same directory that the base style is in.
+// CSS file defines fonts, colors and background images for this
+// style.  The companion '*-heavy.css' file isn't defined, it's just
+// expected to be in the same directory that the base style is in.
 $CSS_DEFAULT = "MacOSX";
 
-$CSS_URLS = array_merge($CSS_URLS, array( "$CSS_DEFAULT" => "themes/$theme/${CSS_DEFAULT}.css" ) );
+$CSS_URLS = array_merge($CSS_URLS,
+                        array("$CSS_DEFAULT" => "themes/$theme/${CSS_DEFAULT}.css"));
 
 // Logo image appears on every page and links to the HomePage.
 $logo = "themes/$theme/PhpWiki.png";
 //$RSS = 
 
-// Signature image which is shown after saving an edited page.
-// If this is left blank, any signature defined in index.php will be
-// used. If it is not defined by index.php or in here then the
-// "Thank you for editing..." screen will be omitted.
+// Signature image which is shown after saving an edited page.  If
+// this is left blank, any signature defined in index.php will be
+// used. If it is not defined by index.php or in here then the "Thank
+// you for editing..." screen will be omitted.
 $SignatureImg = "themes/$theme/Signature.png"; // Papyrus 19pt
 
 // If this theme defines any templates, they will completely override
 // whatever templates have been defined in index.php.
 
-$templates = array('BROWSE' =>    "themes/$theme/templates/browse.html",
-		   'EDITPAGE' =>  "themes/$theme/templates/editpage.html",
-		   'MESSAGE' =>   "themes/$theme/templates/message.html");
+$templates = array(
+                   'BROWSE'   => "themes/$theme/templates/browse.html",
+                   'EDITPAGE' => "themes/$theme/templates/editpage.html",
+                   'MESSAGE'  => "themes/$theme/templates/message.html"
+                   );
 
-
-// If this theme defines any custom link icons, they will completely override
-// any link icon settings defined in index.php.
+// If this theme defines any custom link icons, they will completely
+// override any link icon settings defined in index.php.
 /*
 $URL_LINK_ICONS = array(
-                    'http'	=> "themes/$theme/icons/http.png",
-                    'https'	=> "themes/$theme/icons/https.png",
-                    'ftp'	=> "themes/$theme/icons/ftp.png",
-                    'mailto'	=> "themes/$theme/icons/mailto.png",
-                    'interwiki' => "themes/$theme/icons/interwiki.png",
-                    '*'		=> "themes/$theme/icons/zapg.png"
-                    );
+                        'http'      => "themes/$theme/icons/http.png",
+                        'https'     => "themes/$theme/icons/https.png",
+                        'ftp'       => "themes/$theme/icons/ftp.png",
+                        'mailto'    => "themes/$theme/icons/mailto.png",
+                        'interwiki' => "themes/$theme/icons/interwiki.png",
+                        '*'         => "themes/$theme/icons/zapg.png"
+                        );
 */
 
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 // (c-file-style: "gnu")
 // Local Variables:
 // mode: php
--- themes/WikiTrek/themeinfo.php
+++ themes/WikiTrek/themeinfo.php
@@ -1,76 +1,84 @@
-<?php
+<?php // -*-php-*-
 
-rcs_id('$Id: themeinfo.php,v 1.6 2002-01-03 00:09:17 carstenklapp Exp $');
+rcs_id('$Id: themeinfo.php,v 1.7 2002-01-05 06:17:01 carstenklapp Exp $');
 
 /**
- * This PhpWiki theme is experimental and will likely not appear as 
- * part of any release ("accessories not included"--download seperately.)
+ * This PhpWiki theme is experimental and will likely not appear as
+ * part of any release ("accessories not included"--download
+ * seperately.)
  *
- * The first experimental (and very alpha) theme pack is here, have some
- * fun and play with it, fix it up if you like.
+ * The first experimental (and very alpha) theme pack is here, have
+ * some fun and play with it, fix it up if you like.
  *
  * This one is (by design) completely css-based so unfortunately it
  * doesn't render properly or even the same across different browsers.
  * A preview screen snapshot is also included for comparison testing.
  *
- * The reverse coloring of this theme was chosen to provide an
- * extreme example of a heavily customized PhpWiki, through which
- * any potential visual problems can be identified. The intention is
- * to elimate as many non-html elements from the html templates as
+ * The reverse coloring of this theme was chosen to provide an extreme
+ * example of a heavily customized PhpWiki, through which any
+ * potential visual problems can be identified. The intention is to
+ * elimate as many non-html elements from the html templates as
  * possible.
  *
  * This theme does not render properly in all browsers. In particular,
- * OmniWeb renders some text as black-on-black. Netscape 4 will probably
- * choke on it too.
- * * * * * * * * */
+ * OmniWeb renders some text as black-on-black. Netscape 4 will
+ * probably choke on it too.
+ * * * * * * * * * * * * */
 
 // To activate this theme, specify this setting in index.php:
 //$theme="WikiTrek";
-// To deactivate themes, comment out all the $theme=lines in index.php.
+// To deactivate themes, comment out all the $theme=lines in 'index.php'.
 
-// CSS file defines fonts, colors and background images for this style.
-// The companion '*-heavy.css' file isn't defined, it's just expected to
-// be in the same directory that the base style is in.
+// CSS file defines fonts, colors and background images for this
+// style. The companion '*-heavy.css' file isn't defined, it's just
+// expected to be in the same directory that the base style is in.
 $CSS_DEFAULT = "WikiTrek";
 
-$CSS_URLS = array_merge($CSS_URLS, array( "$CSS_DEFAULT" => "themes/$theme/${CSS_DEFAULT}.css" ) );
+$CSS_URLS = array_merge($CSS_URLS,
+                        array("$CSS_DEFAULT" => "themes/$theme/${CSS_DEFAULT}.css"));
 
 // Logo image appears on every page and links to the HomePage.
 $logo = "themes/$theme/Ufp-logo.png";
 
-// Signature image which is shown after saving an edited page.
-// If this is left blank, any signature defined in index.php will be
-// used. If it is not defined by index.php or in here then the
-// "Thank you for editing..." screen will be omitted.
+// Signature image which is shown after saving an edited page.  If
+// this is left blank, any signature defined in index.php will be
+// used. If it is not defined by index.php or in here then the "Thank
+// you for editing..." screen will be omitted.
 $SignatureImg = "themes/$theme/lights.gif";
 
 // If this theme defines any templates, they will completely override
 // whatever templates have been defined in index.php.
 /*
-$templates = array('BROWSE' =>    "themes/$theme/templates/browse.html",
-		   'EDITPAGE' =>  "themes/$theme/templates/editpage.html",
-		   'MESSAGE' =>   "themes/$theme/templates/message.html");
+$templates = array(
+                   'BROWSE'   => "themes/$theme/templates/browse.html",
+                   'EDITPAGE' => "themes/$theme/templates/editpage.html",
+                   'MESSAGE'  => "themes/$theme/templates/message.html"
+                   );
 */
 
-// If this theme defines any custom link icons, it will completely override
-// any link icon settings defined in index.php.
+// If this theme defines any custom link icons, it will completely
+// override any link icon settings defined in index.php.
 /*
 $URL_LINK_ICONS = array(
-                    'http'	=> "themes/$theme/http.png",
-                    'https'	=> "themes/$theme/https.png",
-                    'ftp'	=> "themes/$theme/ftp.png",
-                    'mailto'    => "themes/$theme/mailto.png",
-                    'interwiki' => "themes/$theme/interwiki.png",
-                    '*'	        => "themes/$theme/zapg.png"
-                    );
+                        'http'      => "themes/$theme/http.png",
+                        'https'     => "themes/$theme/https.png",
+                        'ftp'       => "themes/$theme/ftp.png",
+                        'mailto'    => "themes/$theme/mailto.png",
+                        'interwiki' => "themes/$theme/interwiki.png",
+                        '*'         => "themes/$theme/zapg.png"
+                        );
 */
 
-//gettext() does not work here because we are included from index.php
-//normally themes shouldn't override date & time settings because this will mess up
-//any future user-specific preferences for locale
-$datetimeformat = sprintf(("Stardate %s"),'%B.%e.%Y');	// may contain time of day
-$dateformat = sprintf(("Stardate %s"),'%B.%e.%Y');	// must not contain time
+// The gettext() function does not work here because we are included
+// from 'index.php'. Normally themes shouldn't override date & time
+// settings because this will mess up any future user-specific
+// preferences for locale.
+$datetimeformat = sprintf(("Stardate %s"),'%B.%e.%Y'); // may contain
+                                                       // time of day
+$dateformat = sprintf(("Stardate %s"),'%B.%e.%Y'); // must not contain
+                                                   // time
 
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 // (c-file-style: "gnu")
 // Local Variables:
 // mode: php
