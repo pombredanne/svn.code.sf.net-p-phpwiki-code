12002ff2e72fe94504476101f904602627ccfc7b
carstenklapp <carstenklapp@96ab9672-09ca-45d6-a79d-3d69d39ca109>
2002-01-07 06:47:45 +0000

    testing kkv

---
+++ trunk/cvstestfile-deleteme.php
@@ -0,0 +1,6 @@
+cvs admin -kkv themes/MacOSX/MacOSX-heavy.css
+cvs admin -kkv themes/MacOSX/MacOSX.css
+cvs admin -kkv themes/MacOSX/templates/browse.html
+cvs admin -kkv themes/MacOSX/templates/editpage.html
+cvs admin -kkv themes/MacOSX/templates/message.html
+
