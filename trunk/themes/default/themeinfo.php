d433cc66a9617ebd9ef130bb60a880b09ef3980c
carstenklapp <carstenklapp@96ab9672-09ca-45d6-a79d-3d69d39ca109>
2002-01-11 22:43:57 +0000

    Added rssicon. Commented out all variables to prevent problems in case someone unwittingly sets $theme="default"; instead of theme=""; in index.php.

--- trunk/themes/default/themeinfo.php
+++ trunk/themes/default/themeinfo.php
@@ -1,12 +1,12 @@
 <?php
 
-rcs_id('$Id: themeinfo.php,v 1.8 2002-01-05 06:17:02 carstenklapp Exp $');
+rcs_id('$Id: themeinfo.php,v 1.9 2002-01-11 22:43:57 carstenklapp Exp $');
 
 /**
  * This is really just a template to copy for designing new
  * themes. The real "default" theme is built into index.php, none of
  * the variables here point to any actual files.
- * */
+ */
 
 // To activate this theme, specify this setting in index.php:
 //$theme="default";
@@ -15,30 +15,40 @@ rcs_id('$Id: themeinfo.php,v 1.8 2002-01-05 06:17:02 carstenklapp Exp $');
 // CSS file defines fonts, colors and background images for this
 // style.  The companion '*-heavy.css' file isn't defined, it's just
 // expected to be in the same directory that the base style is in.
-$CSS_DEFAULT = "default";
+/*
+$CSS_DEFAULT = "PhpWiki";
 
 $CSS_URLS = array_merge($CSS_URLS,
                         array("$CSS_DEFAULT" => "themes/$theme/${CSS_DEFAULT}.css"));
+*/
 
 // Logo image appears on every page and links to the HomePage.
-$logo = "themes/$theme/wikibase.png";
+//$logo = "themes/$theme/wikibase.png";
+
+// RSS logo icon (path relative to index.php)
+// If this is left blank (or unset), the default "images/rss.png"
+// will be used.
+//$rssicon = "themes/$theme/rss.png";
 
 // Signature image which is shown after saving an edited page.  If
 // this is left blank, any signature defined in index.php will be
 // used. If it is not defined by index.php or in here then the "Thank
 // you for editing..." screen will be omitted.
-$SignatureImg = "themes/$theme/signature.png";
+//$SignatureImg = "themes/$theme/signature.png";
 
 // If this theme defines any templates, they will completely override
 // whatever templates have been defined in index.php.
+/*
 $templates = array(
                    'BROWSE'   => "themes/$theme/templates/browse.html",
                    'EDITPAGE' => "themes/$theme/templates/editpage.html",
                    'MESSAGE'  => "themes/$theme/templates/message.html"
                    );
+*/
 
 // If this theme defines any custom link icons, they will completely
 // override any link icon settings defined in index.php.
+/*
 $URL_LINK_ICONS = array(
                         'http'      => "themes/$theme/icons/http.png",
                         'https'     => "themes/$theme/icons/https.png",
@@ -47,6 +57,7 @@ $URL_LINK_ICONS = array(
                         'interwiki' => "themes/$theme/icons/interwiki.png",
                         '*'         => "themes/$theme/icons/zapg.png"
                         );
+*/
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 // (c-file-style: "gnu")
