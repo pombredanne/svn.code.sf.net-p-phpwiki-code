a8fca732f5ab5aea0a0425992368d75f106bbb3b
carstenklapp <carstenklapp@96ab9672-09ca-45d6-a79d-3d69d39ca109>
2002-01-12 02:58:34 +0000

    internal rename to ViewSource

--- trunk/lib/plugin/ViewSource.php
+++ trunk/lib/plugin/ViewSource.php
@@ -1,46 +1,42 @@
 <?php // -*-php-*-
-rcs_id('$Id: ViewSource.php,v 1.1 2002-01-12 02:47:55 carstenklapp Exp $');
+rcs_id('$Id: ViewSource.php,v 1.2 2002-01-12 02:58:34 carstenklapp Exp $');
 require_once('lib/Template.php');
 /**
- * A handy plugin for viewing the WikiMarkup of locked (and unlocked) pages.
+ * A handy plugin for viewing the Wiki markup code of locked (and
+ * unlocked) pages.
  *
  * Comments/Discussion:
  *
- *  I don't like the name ViewMarkup.  I'd prefer ViewSource.
- *  No big deal though. --JeffDairiki
- *
  *  In the long run, it may be cleaner to include this functionality
- *  in with the EditPage code.  (I.e. if you try to edit locked pages,
+ *  in with the EditPage code.  (i.e. if you try to edit locked pages,
  *  you get a slightly modified version of the normal page editing
  *  form.  Mostly: no save button.)  --JeffDairiki
+ *  Agreed. --CarstenKlapp
  */
-class WikiPlugin_ViewMarkup
+class WikiPlugin_ViewSource
 extends WikiPlugin
 {
     function getName () {
-        return _("ViewMarkup");
+        return _("ViewSource");
     }
 
     function getDescription () {
-        return sprintf(_("View WikiMarkup for page '%s'."),'[pagename]');
+        return sprintf(_("View Wiki code for page '%s'."), '[pagename]');
     }
     
-        function WikiPlugin_ViewMarkup() {
+    function WikiPlugin_ViewSource() {
     }
 
     function getDefaultArguments() {
         return array('page' => false,
                      'rev'  => false);
-        
     }
-    
+
     function run($dbi, $argstr, $request) {
         $args = $this->getArgs($argstr, $request);
         if (empty($args['page']))
             return '';
-        
-        //fetch the latest version of the page. Should this be made to
-        //work when viewing an old revision of a page too?
+
         $page = $dbi->getPage($args['page']);
         if (empty($args['rev'])) {
             $rev = $page->getCurrentRevision();
@@ -50,7 +46,7 @@ extends WikiPlugin
         }
         else {
             $rev = $page->getRevision($args['rev']);
-            
+
             if (!$rev) {
                 return QElement('p', array('class' => 'error'),
                                 __sprintf("I'm sorry.  Version %d of %s is not in my database.",
@@ -66,11 +62,13 @@ extends WikiPlugin
 
         $html = Element('h2', __sprintf("Page source for %s", $link));
 
-        /* <tt> seems to be a good compromise in IE and OmniWeb
-           it doesn't combine newlines and <br />, and renders monospaced */
-        //$html .= Element('tt', nl2br(htmlspecialchars($rev->getPackedContent())));
+        // <tt> seems to be a good compromise in IE and OmniWeb it
+        // doesn't combine newlines and <br />, and renders monospaced
+        $html .= Element('tt',
+                         nl2br(htmlspecialchars($rev->getPackedContent())));
 
-        /* Display page source in a <textarea>:
+        /**
+         * Display page source in a <textarea>:
          *  o Same appearance as when editing page.
          *  o Easiest to cut and paste from.
          */
@@ -78,17 +76,17 @@ extends WikiPlugin
         $prefs = $user->getPreferences();
         $html .= Element('p',
                          QElement('textarea',
-                                  array('class' => 'wikiedit',
-                                        'rows' => $prefs['edit_area.height'],
-                                        'cols' => $prefs['edit_area.width'],
-                                        'wrap' => 'virtual',
+                                  array('class'  => 'wikiedit',
+                                        'rows'   => $prefs['edit_area.height'],
+                                        'cols'   => $prefs['edit_area.width'],
+                                        'wrap'   => 'virtual',
                                         'readonly' => true),
                                   $rev->getPackedContent()));
 
         return $html;
     }
 };
-        
+
 // (c-file-style: "gnu")
 // Local Variables:
 // mode: php
