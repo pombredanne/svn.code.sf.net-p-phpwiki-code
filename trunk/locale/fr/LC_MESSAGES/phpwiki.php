eb6ffc67f73b9a4b89054a44f75daee0f78b02a7
carstenklapp <carstenklapp@96ab9672-09ca-45d6-a79d-3d69d39ca109>
2002-01-13 22:58:45 +0000

    Initial import from http://www.koota.net/projects/phpwiki/, translation by Pascal.

---
+++ trunk/locale/fr/LC_MESSAGES/phpwiki.php
@@ -0,0 +1,70 @@
+<?php
+
+$locale["%d best incoming links:"] =
+   "%d meilleurs liens entrants :";
+$locale["%d best outgoing links:"] =
+   "%d meilleurs liens sortants :";
+$locale["%d most popular nearby:"] =
+   "%d pages les plus visitées aux alentours :";
+$locale["%s: %s"] =
+   "%s : %s";
+$locale["<b>Reload</b> the page. You should now see the most current version of the page. Your changes are no longer there."] =
+   "<b>Rechargez</b> la page. Vous devez maintenant voir la version la plus récente de la page mais sans vos modifications.";
+$locale["Complete."] =
+   "Complet.";
+$locale["Copy your changes to the clipboard or to another temporary place (e.g. text editor)."] =
+   "Copier et coller vos modifications temporairement dans un autre endroit(par exemple dans un éditeur de texte).";
+$locale["Describe %s here."] =
+   "Décrivez %s ici.";
+$locale["In order to recover from this situation follow these steps:"] =
+   "Afin de se sortir de cette situation, suivez ces étapes :";
+$locale["Lock page"] =
+   "Verrouiller la page";
+$locale["Make changes to the file again. Paste your additions from the clipboard (or text editor)."] =
+   "Faites à nouveau vos modifications. Collez vos changements à partir de l'endroit où vous les avez conservés.";
+$locale["None"] =
+   "Aucune";
+$locale["Page locked"] =
+   "Page verrouillée";
+$locale["PhpWiki is unable to save your changes, because another user edited and saved the page while you were editing the page too. If saving proceeded now changes from the previous author would be lost."] =
+   "PhpWiki n'est pas en mesure d'enregistrer vos modifications parce qu'unautre utilisateur a changé puis enregistré la page entre temps. Ses modifications seraient perdues si la page était enregistrée maintenant.";
+$locale["Press <b>Save</b> again."] =
+   "<b>Enregistrer</b> à nouveau la page.";
+$locale["Problem while editing %s"] =
+   "Un problème est survenu pendant la modification de la page %s";
+$locale["Problem while updating %s"] =
+   "un problème est survenu pendant la mise à jour de %s";
+$locale["RecentChanges"] =
+   "ChangementsRecents";
+$locale["Removed page '%s' succesfully."] =
+   "Page supprimée '%s' avec succès.";
+$locale["Save"] =
+   "Enregistrer";
+$locale["Search"] =
+   "Chercher";
+$locale["Sorry for the inconvenience."] =
+   "Désolé pour la gène occasionnée.";
+$locale["Thank you for editing %s."] =
+   "Merci d'avoir contribué à la page %s";
+$locale["Unlock page"] =
+   "Déverrouiller la page";
+$locale["Use your browser's <b>Back</b> button to go back to the edit page."] =
+   "Revenez en arrière avec le bouton <b>Page précédente</b> de votre navigateur pour retrouver la page modifiée.";
+$locale["Versions are identical"] =
+   "Les versions sont identiques";
+$locale["WikiFatalError"] =
+   "ErreurFataleDeWiki";
+$locale["You are about to remove '%s' permanently!"] =
+   "Vous allez supprimer définitivement la page '%s' !";
+$locale["You entered an invalid login or password."] =
+   "Vous avez entré un nom d'utilisateur ou un mot de passe invalide";
+$locale["Your careful attention to detail is much appreciated."] =
+   "Le soin particulier apporté à la rédaction de cette page sera apprécié.";
+$locale["by %s"] =
+   "par %s";
+$locale["last modified on %s"] =
+   "dernière modification le %s";
+$locale["version %s"] =
+   "version %s";
+
+;?>
