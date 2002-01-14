58c06549559fcd8279fd7379276814982ad011cb
carstenklapp <carstenklapp@96ab9672-09ca-45d6-a79d-3d69d39ca109>
2002-01-14 20:36:09 +0000

    Merged in SteÌphane's 1.2 translations.

--- trunk/locale/fr/LC_MESSAGES/phpwiki.php
+++ trunk/locale/fr/LC_MESSAGES/phpwiki.php
@@ -8,36 +8,54 @@ $locale["%d most popular nearby:"] =
    "%d pages les plus visitées aux alentours :";
 $locale["%s: %s"] =
    "%s : %s";
+$locale["(diff)"] =
+   "(diff)";
 $locale["<b>Reload</b> the page. You should now see the most current version of the page. Your changes are no longer there."] =
-   "<b>Rechargez</b> la page. Vous devez maintenant voir la version la plus récente de la page mais sans vos modifications.";
+   "<b>Recharge</b> la page. Tu devrais alors voir la version actuelle de la page. Tes modifications n'y sont pas.";
+$locale["<none>"] =
+   "<acune>";
+$locale["Bad %s"] =
+   "Mauvais %s";
 $locale["Complete."] =
    "Complet.";
 $locale["Copy your changes to the clipboard or to another temporary place (e.g. text editor)."] =
-   "Copier et coller vos modifications temporairement dans un autre endroit(par exemple dans un éditeur de texte).";
+   "Copie tes modifications dans un endroit temporaire (un éditeur de texte par exemple).";
 $locale["Describe %s here."] =
    "Décrivez %s ici.";
+$locale["Diff"] =
+   "Diff";
+$locale["Diff: %s"] =
+   "Diff : %s";
+$locale["Edit"] =
+   "Editer";
+$locale["HomePage"] =
+   "Accueil";
 $locale["In order to recover from this situation follow these steps:"] =
-   "Afin de se sortir de cette situation, suivez ces étapes :";
+   "Pour remettre de l'ordre dans cette situation, suis ces étapes :";
 $locale["Lock page"] =
    "Verrouiller la page";
 $locale["Make changes to the file again. Paste your additions from the clipboard (or text editor)."] =
-   "Faites à nouveau vos modifications. Collez vos changements à partir de l'endroit où vous les avez conservés.";
+   "Ré-applique tes modifications, colle les ajouts depuis la fenêtre où tu les as placés.";
 $locale["None"] =
    "Aucune";
+$locale["Not Implemented"] =
+   "Non encore implémentée";
 $locale["Page locked"] =
    "Page verrouillée";
 $locale["PhpWiki is unable to save your changes, because another user edited and saved the page while you were editing the page too. If saving proceeded now changes from the previous author would be lost."] =
-   "PhpWiki n'est pas en mesure d'enregistrer vos modifications parce qu'unautre utilisateur a changé puis enregistré la page entre temps. Ses modifications seraient perdues si la page était enregistrée maintenant.";
+   "PhpWiki n'a pu enregistrer tes modifications, parce qu'un autre utilisateur a édité et sauvegardé la page pendant que tu faisais de même. Si j'avais continué, j'aurais perdu les modifications dues à l'auteur précédent.";
 $locale["Press <b>Save</b> again."] =
-   "<b>Enregistrer</b> à nouveau la page.";
+   "<b>Enregistrer</b> à nouveau.";
 $locale["Problem while editing %s"] =
-   "Un problème est survenu pendant la modification de la page %s";
+   "Problème pendant l'édition de %s";
 $locale["Problem while updating %s"] =
-   "un problème est survenu pendant la mise à jour de %s";
+   "Problème pendant la mise à jour de %s";
 $locale["RecentChanges"] =
-   "ChangementsRecents";
+   "ChangementsRécents";
+$locale["Remove page"] =
+   "Supprimer la page";
 $locale["Removed page '%s' succesfully."] =
-   "Page supprimée '%s' avec succès.";
+   "Page '%s' supprimée avec succès.";
 $locale["Save"] =
    "Enregistrer";
 $locale["Search"] =
@@ -45,26 +63,30 @@ $locale["Search"] =
 $locale["Sorry for the inconvenience."] =
    "Désolé pour la gène occasionnée.";
 $locale["Thank you for editing %s."] =
-   "Merci d'avoir contribué à la page %s";
+   "Merci d'avoir édité %s.";
 $locale["Unlock page"] =
    "Déverrouiller la page";
 $locale["Use your browser's <b>Back</b> button to go back to the edit page."] =
-   "Revenez en arrière avec le bouton <b>Page précédente</b> de votre navigateur pour retrouver la page modifiée.";
+   "Utilise la fonction <b>Retour</b> de ton navigateur pour revenir à la page d'édition.";
+$locale["Version %d"] =
+   "version %d";
 $locale["Versions are identical"] =
    "Les versions sont identiques";
 $locale["WikiFatalError"] =
    "ErreurFataleDeWiki";
 $locale["You are about to remove '%s' permanently!"] =
-   "Vous allez supprimer définitivement la page '%s' !";
+   "Tu es sur le point de supprimer '%s' de façon permanente!";
 $locale["You entered an invalid login or password."] =
-   "Vous avez entré un nom d'utilisateur ou un mot de passe invalide";
-$locale["Your careful attention to detail is much appreciated."] =
-   "Le soin particulier apporté à la rédaction de cette page sera apprécié.";
+   "Tu as entré un login ou mot de passe invalide";
 $locale["by %s"] =
    "par %s";
+$locale["current version"] =
+   "Version actuelle";
 $locale["last modified on %s"] =
    "dernière modification le %s";
+$locale["version %d"] =
+   "version %d";
 $locale["version %s"] =
    "version %s";
 
-;?>
+?>
