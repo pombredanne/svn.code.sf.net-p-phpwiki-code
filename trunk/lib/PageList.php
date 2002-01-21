5ffc29ad125e9b6fa0e89ee5232c9be1bd792d3c
carstenklapp <carstenklapp@96ab9672-09ca-45d6-a79d-3d69d39ca109>
2002-01-21 21:00:21 +0000

    Added new report columns, alternate column titles and added some comments.

--- trunk/lib/PageList.php
+++ trunk/lib/PageList.php
@@ -1,13 +1,19 @@
-<?php rcs_id('$Id: PageList.php,v 1.9 2002-01-21 19:18:16 carstenklapp Exp $');
+<?php rcs_id('$Id: PageList.php,v 1.10 2002-01-21 21:00:21 carstenklapp Exp $');
 
 // This relieves some work for these plugins:
 //
 // BackLinks, LikePages, Mostpopular, TitleSearch
 //
-// It also allows dynamic expansion of those plugins to
-// include more columns in their output.
+// It also allows dynamic expansion of those plugins to include more
+// columns in their output.
 //
 // There are still a few rough edges.
+//
+// FIXME: Make caption work properly with new HtmlElement
+//
+// FIXME: Add error recovery! If the column title isn't found due to a
+//        typo, the wiki craps out, you will not even be able to edit the
+//        page again to fix the typo in the column name!
 
 class PageList {
     function PageList ($pagelist_name = '') {
@@ -52,7 +58,7 @@ class PageList {
         }
         return $this->_total;
     }
-    
+
     function getContent() {
         $caption = HTML::div(array('align' => 'left'), $this->getCaption());
 
@@ -62,21 +68,35 @@ class PageList {
             return $this->_generateTable($caption);
         }
     }
-    
+
     ////////////////////
     // private
     ////////////////////
-    
+
+    // Some of these column_name aliases could probably be eliminated
+    // once standard titles have been agreed upon
+
     // lookup alignment from column name
     function _column_align($column_name) {
         $map = array(
-                     _("Page Name")      => 'left',
-                     _("Last Modified")  => 'left',
-                     _("Hits")           => 'right',
-                     _("Date Created")   => 'left',
-                     _("# Of Revisions") => 'right',
-                     _("Last Summary")   => 'left',
-                     _("Last Edited By") => 'left'
+                     _("Page Name")       => 'left',
+                     _("Page")            => 'left',
+                     _("Name")            => 'left',
+                     _("Last Modified")   => 'left',
+                     _("Hits")            => 'right',
+                     _("Visitors")        => 'right',
+                     _("Date Created")    => 'left',
+                     _("Creation Date")   => 'left',
+                     _("# Of Revisions")  => 'right',        //FIXME: count revisions in db
+                     _("Last Summary")    => 'left',
+                     _("Last Author")     => 'left',
+                     _("Last Edited By")  => 'left',
+                     _("Author")          => 'left',
+                     _("Original Author") => 'left',
+                     _("Created By")      => 'left',
+                     _("Locked")          => 'left',
+                     _("Minor Edit")      => 'left',
+                     _("Minor")           => 'left',
                      );
 
         $key = $map[$column_name];
@@ -93,13 +113,24 @@ class PageList {
     // lookup database field from column name
     function _colname_to_dbfield($column_name) {
         $map = array(
-                     _("Page Name")      => 'pagename',
-                     _("Last Modified")  => 'mtime',
-                     _("Hits")           => 'hits',
-                     _("Date Created")   => '',
-                     _("# of revisions") => '',
-                     _("Last Summary")   => 'summary',
-                     _("Last Edited By") => ''
+                     _("Page Name")       => 'pagename',
+                     _("Page")            => 'pagename',
+                     _("Name")            => 'pagename',
+                     _("Last Modified")   => 'mtime',
+                     _("Hits")            => 'hits',
+                     _("Visitors")        => 'hits',
+                     _("Date Created")    => '_ctime',
+                     _("Creation Date")   => '_ctime',
+                     _("# of revisions")  => '',        //FIXME: count revisions in db
+                     _("Last Summary")    => 'summary',
+                     _("Last Author")     => 'author',
+                     _("Last Edited By")  => 'author',
+                     _("Author")          => '_cauthor',
+                     _("Original Author") => '_cauthor',
+                     _("Created By")      => '_cauthor',
+                     _("Locked")          => 'locked',
+                     _("Minor Edit")      => 'is_minor_edit',
+                     _("Minor")           => 'is_minor_edit',
                     );
         $key = $map[$column_name];
         if (! $key) {
@@ -110,17 +141,28 @@ class PageList {
         return $key;
     }
 
-    
+
     // lookup whether fieldname requires page revision
     function _does_require_rev($column_name) {
         $map = array(
-                     _("Page Name")      => false,
-                     _("Last Modified")  => true,
-                     _("Hits")           => false,
-                     _("Date Created")   => '',
-                     _("# Of Revisions") => '',
-                     _("Last Summary")   => true,
-                     _("Last Edited By") => true
+                     _("Page Name")       => false,
+                     _("Page")            => false,
+                     _("Name")            => false,
+                     _("Last Modified")   => true,
+                     _("Hits")            => false,
+                     _("Visitors")        => false,
+                     _("Date Created")    => false,
+                     _("Creation Date")   => false,
+                     _("# Of Revisions")  => '',        //FIXME: count revisions in db
+                     _("Last Summary")    => true,
+                     _("Last Author")     => true,
+                     _("Last Edited By")  => true,
+                     _("Author")          => true,
+                     _("Original Author") => false,
+                     _("Created By")      => false,
+                     _("Locked")          => false,
+                     _("Minor Edit")      => true,
+                     _("Minor")           => true,
                      );
         $key = $map[$column_name];
         return $key;
@@ -129,6 +171,7 @@ class PageList {
     // make a table given the caption
     function _generateTable($caption) {
         $pad = new RawXml('&nbsp;&nbsp;');
+        $emdash = new RawXml('&#8212;');
 
         $thead = HTML::thead();
         foreach ($this->_columns as $column_name) {
@@ -142,22 +185,45 @@ class PageList {
         $tbody = HTML::tbody();
         foreach ($this->_pages as $page_handle) {
             $row = HTML::tr();
+            global $Theme;
+
             foreach ($this->_columns as $column_name) {
                 $col = HTML::td();
                 $field = $this->_colname_to_dbfield($column_name);
+
                 if ($this->_does_require_rev($column_name)) {
                     $current = $page_handle->getCurrentRevision();
                     $value = $current->get($field);
-                    if ($field=='mtime') {
-                        global $Theme;
-                        $td = ($Theme->formatDateTime($value));
-                    } else {
+                }
+                //TODO: make method to determine formatting
+                switch ($field) {
+                    // needs current rev
+                case 'mtime' :
+                    $td = ($Theme->formatDateTime($value));
+                    break;
+                case 'is_minor_edit' :
+                    $td = ($value) ? _("minor") : $emdash;
+                    break;
+
+                    // does not need current rev
+                case 'pagename' :
+                    $td = new RawXml (LinkExistingWikiWord($page_handle->getName()));
+                    break;
+                case 'locked' :
+                    $td = ($page_handle->get($field)) ? _("locked") : $emdash;
+                    break;
+                case '_ctime' :
+                    $revision = $page_handle->getRevision(1);
+                    $td = $Theme->formatDateTime($revision->get('mtime'));
+                    break;
+                case '_cauthor' :
+                    $revision = $page_handle->getRevision(1);
+                    $td = new RawXml (LinkExistingWikiWord($revision->get('author')));
+                    break;
+
+                default :
+                    if ($this->_does_require_rev($column_name)) {
                         $td = ($value);
-                    }
-                } else {
-                    //TODO: make method to determine formatting
-                    if ($field=='pagename') {
-                        $td = (new RawXml (LinkExistingWikiWord($page_handle->getName())));
                     } else {
                         $td = ($page_handle->get($field));
                     }
@@ -177,10 +243,9 @@ class PageList {
         // Final table assembly
         $table = HTML::table(array('cellpadding' => 0,
                                    'cellspacing' => 1,
-                                   'border' => 0,
-                                   'summary' => $summary,
-                                   'width' => '100%')
-                            );
+                                   'border'      => 0,
+                                   'summary'     => $summary,
+                                   'width'       => '100%'));
 
         $table->pushContent(HTML::caption(array('align'=>'top'), $caption));
         $table->pushContent($thead);
