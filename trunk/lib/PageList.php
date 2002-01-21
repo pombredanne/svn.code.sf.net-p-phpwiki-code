02531145ff8fadf55f8f84211ec875569181a8e5
carstenklapp <carstenklapp@96ab9672-09ca-45d6-a79d-3d69d39ca109>
2002-01-21 07:04:33 +0000

    This will relieve some of the work of plugins like LikePages, MostPopular, and allows expansion of those plugins to dynamically include more columns in their output if desired. This doesn't quite work yet, the database access stuff here isn't finished.

---
+++ trunk/lib/PageList.php
@@ -0,0 +1,156 @@
+<?php rcs_id('$Id: PageList.php,v 1.1 2002-01-21 07:04:33 carstenklapp Exp $');
+
+// this will relieve some of the work of plugins like LikePages, MostPopular
+// and allows dynamic expansion of those plugins do include more columns in their output
+
+// This doesn't quite work yet, the database access here isn't finished.
+
+class PageList {
+    function PageList ($pagelist_name = '') {
+        $this->name = $pagelist_name;
+        $this->_caption = "";
+        $this->_columns = array(_("Page Name"));
+        $this->_pages = array();
+        $this->_total = 0;
+        $this->_html = "";
+    }
+
+    function setCaption ($caption_string) {
+        $this->_caption = $caption_string;
+    }
+
+    function getCaption () {
+        // put the total into the caption if needed
+        if (strstr($this->_caption, '%d')) {
+            $this->setCaption(sprintf($this->_caption, getTotal()));
+        }
+        return $this->_caption;
+    }
+
+    function addColumn ($new_columnname) {
+        array_push($this->_columns, $new_columnname);
+    }
+
+    function addPage ($page_handle) {
+        array_push($this->_pages, &$page_handle);
+        $this->_total = $this->_total + 1;
+    }
+
+    function getTotal () {
+        if (! $this->_total) {
+            //this might not be necessary
+            $this->_total = count($this->_pages);
+        }
+        return $this->_total;
+    }
+    
+    function getHTML() {
+    //function getHTML($dbi) {
+        // TODO: Generate a list instead of a table when only one
+        // column (pagenames only).
+        // TODO: use the new html functions
+        if ($this->_html == "") {
+            $html = "";
+            $pad = "&nbsp;&nbsp;";
+            $html .= "<p>" . $this->getCaption() . "</p>";
+            $summary = "FIXME: add brief summary and column names";
+            $html .= "<table summary=\"$summary\" border=\"0\" padding=\"0\" cellspacing=\"0\">";
+            foreach ($this->_columns as $column_name) {
+                $html .= $this->_column_align($column_name) == 'right' ? "<tr align=\"right\">" : "<tr>";
+                //FIXME: add column headers
+                foreach ($this->_pages as $page_handle) {
+                    $html .= "<td>";
+                    echo $page_handle->getName(); //debugging;
+                    $field = $this->_colname_to_dbfield($column_name);
+                    if ($this->_does_require_rev($field)) {
+                        //this doesn't quite work yet
+                        $current = $page_handle->getCurrentRevision();
+                        $html .= $current->get($field);
+                    } else {
+                        //TODO: formatting such as
+                        //$Theme->formatDateTime($current->get('mtime'))
+                        $html .= $page_handle->get($field);
+                    }
+                    //FIXME: omit padding for last column;
+                    $html .= $pad . "</td>";
+                }
+                $html .= "</tr>\n";
+            }
+            $this->_html = $html;
+            return $html;
+        } else {
+            return $this->_html;
+        }
+    }
+
+    ////////////////////
+    // private
+    ////////////////////
+
+    // lookup alignment from column name
+    function _column_align($column_name) {
+        $map = array(
+                     _("Page Name")     => 'left',
+                     _("Last Modified") => 'left',
+                     _("Hits")          => 'right'
+                     );
+
+        $key = $map[$column_name];
+//            trigger_error("_column_align: key for '$column_name' is '$key'",
+//                          E_USER_ERROR); //tempdebug
+
+        if (! $key) {
+            //FIXME: localise after wording has been finalized.
+            trigger_error("_column_align: key for '$column_name' not found",
+                          E_USER_ERROR);
+        }
+        return $key;
+    }
+
+
+    // lookup database field from column name
+    function _colname_to_dbfield($column_name) {
+        $map = array(
+                     _("Page Name")     => 'name',
+                     _("Last Modified") => 'mtime',
+                     _("Hits")          => 'hits'
+                    );
+        $key = $map[$column_name];
+        if (! $key) {
+            //FIXME: localise after wording has been finalized.
+            trigger_error("_colname_to_dbfield: key for '$column_name' not found",
+                          E_USER_ERROR);
+        }
+        return $key;
+    }
+
+    
+    // lookup whether fieldname requires page revision
+    function _does_require_rev($column_name) {
+        $map = array(
+                     _("Page Name")     => false,
+                     _("Last Modified") => false,
+                     _("Hits")          => true
+                     );
+        $key = $map[$column_name];
+        //not required
+        //if (! $key) {
+            //FIXME: localise after wording has been finalized.
+            //trigger_error("_does_require_rev: key for '$column_name' not found",
+        //                  E_USER_ERROR);
+        //}
+        return $key;
+    }
+    
+};
+
+
+// (c-file-style: "gnu")
+// Local Variables:
+// mode: php
+// tab-width: 8
+// c-basic-offset: 4
+// c-hanging-comment-ender-p: nil
+// indent-tabs-mode: nil
+// End:   
+?>
