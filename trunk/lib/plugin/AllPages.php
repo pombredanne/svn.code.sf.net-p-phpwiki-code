c953f63bf6e91d4d0762ab9a82a307b97ed29cf0
dairiki <dairiki@96ab9672-09ca-45d6-a79d-3d69d39ca109>
2002-01-22 06:35:13 +0000

    A rudimentary AllPages plugin.

---
+++ trunk/lib/plugin/AllPages.php
@@ -0,0 +1,46 @@
+<?php // -*-php-*-
+rcs_id('$Id');
+
+require_once('lib/PageList.php');
+
+/**
+ */
+class WikiPlugin_AllPages
+extends WikiPlugin
+{
+    function getName () {
+        return _("AllPages");
+    }
+
+    function getDescription () {
+        return _("All Pages");
+    }
+    
+    function getDefaultArguments() {
+        return array('noheader'	=> false,
+		     'include_empty' => false);
+    }
+
+    function run($dbi, $argstr, $request) {
+        extract($this->getArgs($argstr, $request));
+        
+        $pages = $dbi->getAllPages($include_empty);
+        $pagelist = new PageList();
+        if (!$noheader)
+            $pagelist->setCaption(_("Pages in this wiki (%d total):"));
+
+        while ($page = $pages->next())
+            $pagelist->addPage($page);
+
+        return $pagelist;
+    }
+};
+        
+// Local Variables:
+// mode: php
+// tab-width: 8
+// c-basic-offset: 4
+// c-hanging-comment-ender-p: nil
+// indent-tabs-mode: nil
+// End:   
+?>
