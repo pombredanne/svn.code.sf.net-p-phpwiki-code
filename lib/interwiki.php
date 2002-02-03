8531f1314f90e31fb41dc04a4908e84e01494a94
carstenklapp <carstenklapp@96ab9672-09ca-45d6-a79d-3d69d39ca109>
2002-02-03 20:04:41 +0000

    Enhancements to render links inside InterWikiMap urls and wikinames.

--- lib/interwiki.php
+++ lib/interwiki.php
@@ -1,4 +1,4 @@
-<?php rcs_id('$Id: interwiki.php,v 1.18 2002-02-03 19:21:58 carstenklapp Exp $');
+<?php rcs_id('$Id: interwiki.php,v 1.19 2002-02-03 20:04:41 carstenklapp Exp $');
 
 class InterWikiMap {
     function InterWikiMap (&$request) {
@@ -68,19 +68,22 @@ class InterWikiMap {
         if (!preg_match_all("/^\s*(\S+)\s+((?:$AllowedProtocols):[^\s<>\"']+)/m",
                             $text, $matches, PREG_SET_ORDER))
             return false;
-        foreach ($matches as $m)
+        foreach ($matches as $m) {
+            if (substr($m[1], 0, 1) == "~")
+                $m[1] = substr($m[1], 1);
             $map[$m[1]] = $m[2];
+        }
         $map['Category'] = 'Category';
         return $map;
     }
-        
+
     function _getMapFromWikiPage ($page) {
         if (! $page->get('locked'))
             return false;
         
         $current = $page->getCurrentRevision();
         
-        if (preg_match('|^<verbatim>\n(.*)^</verbatim>|ms',
+        if (preg_match('|^<pre>\n(.*)^</pre>|ms',
                        $current->getPackedContent(), $m)) {
             return $m[1];
         }
