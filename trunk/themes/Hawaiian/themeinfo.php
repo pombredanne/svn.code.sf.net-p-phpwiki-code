b726427a7fb7f516edb94f16442a9478ebb027fa
carstenklapp <carstenklapp@96ab9672-09ca-45d6-a79d-3d69d39ca109>
2002-02-03 21:28:30 +0000

    Added note about timezone conversion with $Theme->setDateTimeFormat() and setDateFormat()

--- trunk/themes/Hawaiian/themeinfo.php
+++ trunk/themes/Hawaiian/themeinfo.php
@@ -1,6 +1,6 @@
 <?php
 
-rcs_id('$Id: themeinfo.php,v 1.19 2002-01-23 14:26:22 carstenklapp Exp $');
+rcs_id('$Id: themeinfo.php,v 1.20 2002-02-03 21:28:30 carstenklapp Exp $');
 
 /**
  * WikiWiki Hawaiian theme for PhpWiki.
@@ -62,6 +62,8 @@ $Theme->setAutosplitWikiWords(true);
  * below.  (These examples give the default formats.)
  * Formats are given as format strings to PHP strftime() function See
  * http://www.php.net/manual/en/function.strftime.php for details.
+ * Do not include the server's zone (%Z), times are converted to the
+ * user's time zone.
  */
 //$Theme->setDateTimeFormat("%B %e, %Y");   // may contain time of day
 //$Theme->setDateFormat("%B %e, %Y");	    // must not contain time
