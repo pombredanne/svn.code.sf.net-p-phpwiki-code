a79e364bf1a601568b0bee397d92eca2cd3647e8
carstenklapp <carstenklapp@96ab9672-09ca-45d6-a79d-3d69d39ca109>
2002-02-04 20:40:25 +0000

    Bugfix. New: allow admin to lock / unlock page even if no text changes were made. Indicate lockchanged status on savepage.tmpl and nochanges.tmpl.

--- lib/editpage.php
+++ lib/editpage.php
@@ -1,5 +1,5 @@
 <?php
-rcs_id('$Id: editpage.php,v 1.34 2002-02-04 17:21:34 carstenklapp Exp $');
+rcs_id('$Id: editpage.php,v 1.35 2002-02-04 20:40:25 carstenklapp Exp $');
 
 require_once('lib/Template.php');
 
@@ -7,7 +7,7 @@ class PageEditor
 {
     function PageEditor (&$request) {
         $this->request = &$request;
-        
+
         $this->user = $request->getUser();
         $this->page = $request->getPage();
 
@@ -16,7 +16,7 @@ class PageEditor
         $this->meta = array('author' => $this->user->getId(),
                             'locked' => $this->page->get('locked'),
                             'author_id' => $this->user->getAuthenticatedId());
-        
+
         $version = $request->getArg('version');
         if ($version !== false) {
             $this->selected = $this->page->getRevision($version);
@@ -26,7 +26,7 @@ class PageEditor
             $this->selected = $this->current;
             $this->version = $this->current->getVersion();
         }
-        
+
         if ($this->_restoreState()) {
             $this->_initialEdit = false;
         }
@@ -39,7 +39,7 @@ class PageEditor
     function editPage () {
         $saveFailed = false;
         $tokens = array();
-        
+
         if ($this->canEdit()) {
             if ($this->isInitialEdit())
                 return $this->viewSource();
@@ -58,7 +58,7 @@ class PageEditor
             $tokens['PREVIEW_CONTENT'] = $this->getPreview(); // FIXME: convert to _MESSAGE?
 
         // FIXME: NOT_CURRENT_MESSAGE?
-        
+
         $tokens = array_merge($tokens, $this->getFormElements());
 
         return $this->output('editpage', _("Edit: %s"), $tokens);
@@ -67,7 +67,7 @@ class PageEditor
     function output ($template, $title_fs, $tokens) {
         $selected = &$this->selected;
         $current = &$this->current;
-        
+
         if ($selected && $selected->getVersion() != $current->getVersion()) {
             $rev = $selected;
             $pagelink = WikiLink($selected);
@@ -76,29 +76,46 @@ class PageEditor
             $rev = $current;
             $pagelink = WikiLink($this->page);
         }
-        
-        
+
+
         $title = new FormattedText ($title_fs, $pagelink);
         $template = Template($template, $tokens);
 
         GeneratePage($template, $title, $rev);
         return true;
     }
-    
-    
+
+
     function viewSource ($tokens = false) {
         assert($this->isInitialEdit());
         assert($this->selected);
-        
+
         $tokens['PAGE_SOURCE'] = $this->_content;
-        
+
         return $this->output('viewsource', _("View Source: %s"), $tokens);
+    }
+
+    function setPageLockChanged($isadmin, $lock, &$page) {
+        if ($isadmin) {
+            if (! $page->get('locked') == $lock) {
+                $request = &$this->request;
+                $request->setArg('lockchanged', true); //is it safe to add new args to $request like this?
+            }
+            $page->set('locked', $lock);
+        }
     }
 
     function savePage () {
         $request = &$this->request;
-        
+
         if ($this->isUnchanged()) {
+            // Allow admin lock/unlock even if
+            // no text changes were made.
+            if ($isadmin = $this->user->isadmin()) {
+                $page = &$this->page;
+                $lock = $this->meta['locked'];
+                $this->setPageLockChanged($isadmin, $lock, &$page);
+            }
             // Save failed. No changes made.
             include_once('lib/display.php');
             // force browse of current version:
@@ -110,6 +127,7 @@ class PageEditor
         $page = &$this->page;
         $lock = $this->meta['locked'];
         $this->meta['locked'] = ''; // hackish
+
         // Save new revision
         $newrevision = $page->createRevision($this->_currentVersion + 1,
                                              $this->_content,
@@ -120,7 +138,8 @@ class PageEditor
             return false;
         }
         // New contents successfully saved...
-        if ($lock && $this->user->isadmin()) $page->set('locked', true);
+        if ($isadmin = $this->user->isadmin())
+            $this->setPageLockChanged($isadmin, $lock, &$page);
 
         // Clean out archived versions of this page.
         include_once('lib/ArchiveCleaner.php');
@@ -148,11 +167,11 @@ class PageEditor
             $template->replace('WARNINGS', $warnings);
 
         $pagelink = WikiLink($page);
-        
+
         GeneratePage($template, fmt("Saved: %s", $pagelink), $newrevision);
         return true;
     }
-    
+
     function isConcurrentUpdate () {
         assert($this->current->getVersion() >= $this->_currentVersion);
         return $this->current->getVersion() != $this->_currentVersion;
@@ -168,7 +187,7 @@ class PageEditor
 
     function isUnchanged () {
         $current = &$this->current;
-        
+
         if ($this->meta['markup'] !=  $current->get('markup'))
             return false;
 
@@ -189,18 +208,18 @@ class PageEditor
 
     function getLockedMessage () {
         return
-            HTML(HTML::h2(_("Page Locked")),
-                 HTML::p(_("This page has been locked by the administrator so your changes can not be saved.")),
-                 HTML::p(_("(Copy your changes to the clipboard. You can try editing a different page or save your text in a text editor.)")),
-                 HTML::p(_("Sorry for the inconvenience.")));
+        HTML(HTML::h2(_("Page Locked")),
+             HTML::p(_("This page has been locked by the administrator so your changes can not be saved.")),
+             HTML::p(_("(Copy your changes to the clipboard. You can try editing a different page or save your text in a text editor.)")),
+             HTML::p(_("Sorry for the inconvenience.")));
     }
-    
+
     function getConflictMessage () {
         /*
-          xgettext only knows about c/c++ line-continuation strings
-          it does not know about php's dot operator.
-          We want to translate this entire paragraph as one string, of course.
-        */
+         xgettext only knows about c/c++ line-continuation strings
+         it does not know about php's dot operator.
+         We want to translate this entire paragraph as one string, of course.
+         */
 
         $re_edit_link = Button('edit', _("Edit the new version"), $this->page);
 
@@ -209,7 +228,7 @@ class PageEditor
                                        $re_edit_link)),
                           HTML::li(_("Make changes to the file again. Paste your additions from the clipboard (or text editor).")),
                           HTML::li(_("Save your updated changes.")));
-         
+
         return HTML(HTML::h2(_("Conflicting Edits!")),
                     HTML::p(_("In the time since you started editing this page, another user has saved a new version of it.  Your changes can not be saved, since doing so would overwrite the other author's changes.")),
                     HTML::p(_("In order to recover from this situation, follow these steps:")),
@@ -223,41 +242,41 @@ class PageEditor
 
         // wrap=virtual is not HTML4, but without it NS4 doesn't wrap long lines
         $readonly = $this->canEdit() || $this->isConcurrentUpdate();
-        
-        return HTML::textarea(array('class' => 'wikiedit',
-                                    'name' => 'edit[content]',
-                                    'rows' => $request->getPref('editHeight'),
-                                    'cols' => $request->getPref('editWidth'),
+
+        return HTML::textarea(array('class'    => 'wikiedit',
+                                    'name'     => 'edit[content]',
+                                    'rows'     => $request->getPref('editHeight'),
+                                    'cols'     => $request->getPref('editWidth'),
                                     'readonly' => (bool) $readonly,
-                                    'wrap' => 'virtual'),
+                                    'wrap'     => 'virtual'),
                               $this->_content);
     }
-    
+
     function getFormElements () {
         $request = &$this->request;
         $page = &$this->page;
 
 
-        $h = array('action' 		   => 'edit',
-                   'pagename'		   => $page->getName(),
-                   'version'		   => $this->version,
+        $h = array('action'   => 'edit',
+                   'pagename' => $page->getName(),
+                   'version'  => $this->version,
                    'edit[current_version]' => $this->_currentVersion);
 
         $el['HIDDEN_INPUTS'] = HiddenInputs($h);
-        
+
 
         $el['EDIT_TEXTAREA'] = $this->getTextArea();
 
-        $el['SUMMARY_INPUT'] 
-            = HTML::input(array('type' => 'text',
+        $el['SUMMARY_INPUT']
+            = HTML::input(array('type'  => 'text',
                                 'class' => 'wikitext',
-                                'name' => 'edit[summary]',
-                                'size' => 50,
+                                'name'  => 'edit[summary]',
+                                'size'  => 50,
                                 'value' => $this->meta['summary']));
         $el['MINOR_EDIT_CB']
             = HTML::input(array('type' => 'checkbox',
-                                'name' => 'edit[minor_edit]',
-                                 'checked' => (bool) $this->meta['is_minor_edit']));
+                                'name'  => 'edit[minor_edit]',
+                                'checked' => (bool) $this->meta['is_minor_edit']));
         $el['NEW_MARKUP_CB']
             = HTML::input(array('type' => 'checkbox',
                                 'name' => 'edit[markup]',
@@ -268,7 +287,7 @@ class PageEditor
             = HTML::input(array('type' => 'checkbox',
                                 'name' => 'edit[locked]',
                                 'disabled' => (bool) !$this->user->isadmin(),
-                                'checked' => (bool) $this->meta['locked']));
+                                'checked'  => (bool) $this->meta['locked']));
 
         $el['PREVIEW_B'] = Button('submit:edit[preview]', _("Preview"));
 
@@ -283,10 +302,10 @@ class PageEditor
 
     function _restoreState () {
         $request = &$this->request;
-        
+
         $posted = $request->getArg('edit');
         $request->setArg('edit', false);
-        
+
         if (!$posted || !$request->isPost() || $request->getArg('action') != 'edit')
             return false;
 
@@ -294,7 +313,7 @@ class PageEditor
             return false;
         $this->_content = preg_replace('/[ \t\r]+\n/', "\n",
                                        rtrim($posted['content']));
-        
+
         $this->_currentVersion = (int) $posted['current_version'];
 
         if ($this->_currentVersion < 0)
@@ -309,7 +328,7 @@ class PageEditor
         $meta['is_minor_edit'] = !empty($posted['minor_edit']);
 
         $this->meta = array_merge($this->meta, $meta);
-        
+
         if (!empty($posted['preview']))
             $this->editaction = 'preview';
         elseif (!empty($posted['save']))
@@ -319,7 +338,7 @@ class PageEditor
 
         return true;
     }
-    
+
     function _initializeState () {
         $request = &$this->request;
         $current = &$this->current;
@@ -331,7 +350,7 @@ class PageEditor
 
         $this->_currentVersion = $current->getVersion();
         $this->_content = $selected->getPackedContent();
-        
+
         $this->meta['summary'] = '';
         $this->meta['locked'] = $this->page->get('locked');
 
@@ -357,5 +376,5 @@ class PageEditor
 // c-basic-offset: 4
 // c-hanging-comment-ender-p: nil
 // indent-tabs-mode: nil
-// End:   
+// End:
 ?>
