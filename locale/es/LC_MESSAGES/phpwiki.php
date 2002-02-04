730ab81bc5400f7dd7ee8e892c892624646075ff
carstenklapp <carstenklapp@96ab9672-09ca-45d6-a79d-3d69d39ca109>
2002-02-04 06:07:11 +0000

    Rebuilt po file, a few new strings. Translated some of the easy (short) ones

--- locale/es/LC_MESSAGES/phpwiki.php
+++ locale/es/LC_MESSAGES/phpwiki.php
@@ -1,9 +1,27 @@
 <?php
 
+$locale["%s days"] =
+   "%s dias";
+$locale["%s: file not found"] =
+   "%s: fichero no encontrado";
 $locale["(last edited %s)"] =
    "(editada por última vez %s)";
+$locale["1 day"] =
+   "1 día";
+$locale["All Pages"] =
+   "Todas las Páginas";
+$locale["AllPages"] =
+   "TodasLasPáginas";
+$locale["An unnamed PhpWiki"] =
+   "Un PhpWiki sin un nombre";
+$locale["Calendar"] =
+   "Calendario";
+$locale["Complete."] =
+   "Acabado.";
 $locale["Copy your changes to the clipboard or to another temporary place (e.g. text editor)."] =
    "Copie sus cambios al portapapeles o a otro sitio temporal (p.ej. editor de textos).";
+$locale["Deleted."] =
+   "Borrada.";
 $locale["Describe %s here."] =
    "Describe %s aquí.";
 $locale["Edit"] =
@@ -26,12 +44,20 @@ $locale["FullTextSearch"] =
    "BúsquedaDelTexto";
 $locale["GoodStyle"] =
    "BuenEstilo";
+$locale["H"] =
+   "Al";
+$locale["Height"] =
+   "Altura";
 $locale["Hits"] =
    "Número de hits";
 $locale["HomePage"] =
    "PáginaPrincipal";
+$locale["In order to recover from this situation, follow these steps:"] =
+   "Para restaurar esta situación siga los siguientes pasos:";
 $locale["Include of '%s' failed"] =
    "Inserción de '%s' falló";
+$locale["InterWikiMap"] =
+   "InterWikiMap";
 $locale["Last"] =
    "Final";
 $locale["Last edited on %s."] =
@@ -44,16 +70,26 @@ $locale["Locked"] =
    "Bloqueada";
 $locale["Make changes to the file again. Paste your additions from the clipboard (or text editor)."] =
    "Haga cambios a la página otra vez. Pegue sus modificaciones del portapapeles (o del editor de textos).";
+$locale["Modern"] =
+   "Moderno";
+$locale["MostPopular"] =
+   "MasPopulares";
+$locale["New page."] =
+   "Página nueva.";
 $locale["Next"] =
    "Siguiente";
 $locale["None"] =
-   "Ninguno";
+   "Ninguna";
 $locale["Page Locked"] =
    "Página Bloqueada";
 $locale["Page Name"] =
    "Nombre de la Página";
+$locale["Password"] =
+   "Contraseña";
 $locale["Previous"] =
    "Anterior";
+$locale["Printer"] =
+   "Impresora";
 $locale["RecentChanges"] =
    "CambiosRecientes";
 $locale["References"] =
@@ -62,10 +98,14 @@ $locale["Remove Page"] =
    "Borrar Página";
 $locale["Remove page"] =
    "Borrar página";
+$locale["Remove the page now"] =
+   "Borrar la página ahora";
 $locale["Removed page '%s' succesfully."] =
    "Página '%s' borrada satisfactoriamente.";
 $locale["Search"] =
    "Buscar";
+$locale["Someone has edited the page!"] =
+   "¡Alguien ha modificado la página!";
 $locale["Sorry for the inconvenience."] =
    "Disculpe la inconveniencia.";
 $locale["Synopsis"] =
@@ -80,10 +120,14 @@ $locale["This is a minor change."] =
    "Esto es un cambio pequeño.";
 $locale["This page has been locked by the administrator and cannot be edited."] =
    "Esta página ha sido bloqueada por el administrador y no puede ser editada.";
+$locale["Time Zone"] =
+   "Zona Horaria";
 $locale["Title Search"] =
    "Una búsqueda de títulos";
 $locale["TitleSearch"] =
    "BúsquedaDeTítulos";
+$locale["Today"] =
+   "Hoy";
 $locale["Unlock Page"] =
    "Desbloquear Página";
 $locale["Version"] =
@@ -92,16 +136,26 @@ $locale["Version %d"] =
    "Versión %d";
 $locale["Versions are identical"] =
    "Las versiones son idénticas";
+$locale["W"] =
+   "An";
+$locale["Width"] =
+   "Anchura";
+$locale["Yesterday"] =
+   "Ayer";
 $locale["You are about to remove '%s' permanently!"] =
    "¡Va a borrar '%s' permanentemente!";
 $locale["Your careful attention to detail is much appreciated."] =
    "Se agradece una cuidadosa atención a los detalles.";
 $locale["by %s"] =
    "por %s";
+$locale["day"] =
+   "un dia";
 $locale["last modified on %s"] =
    "modificado por última vez el %s";
 $locale["locked"] =
    "bloqueada";
+$locale["new page"] =
+   "página nueva";
 $locale["version %d"] =
    "versión %d";
 $locale["version %s"] =
