63d6c9c592469cfa8737fa5e02986975f748df88
dairiki <dairiki@96ab9672-09ca-45d6-a79d-3d69d39ca109>
2002-02-21 03:24:19 +0000

    Fixed template invocation.

--- lib/plugin/Info.php
+++ lib/plugin/Info.php
@@ -1,13 +1,9 @@
 <?php // -*-php-*-
-rcs_id('$Id: Info.php,v 1.1 2002-02-21 03:12:37 carstenklapp Exp $');
+rcs_id('$Id: Info.php,v 1.2 2002-02-21 03:24:19 dairiki Exp $');
 /**
  *
  * Sorry, this isn't finished yet.
  *
- * Fatal error: Call to a member function on a non-object in
- * /Library/WebServer/Documents/phpwiki/lib/Template.php(116) : eval()'d code on line 5
- *
- * This looks like a situation similar to the Template problem of DumpHtmlToDir() in loadsave.php.
  *
  */
 class WikiPlugin_Info
@@ -34,8 +30,7 @@ extends WikiPlugin
         $page = $request->getPage();
     
         if (!empty($version)) {
-            $revision = $page->getRevision($version);
-            if (!($new = $page->getRevision($version)))
+            if (!($revision = $page->getRevision($version)))
                 NoSuchRevision($request, $page, $version);
         }
         else {
@@ -47,29 +42,9 @@ extends WikiPlugin
                               $Theme->linkExistingWikiWord($pagename, false, $revision)));
 
         //$pagetitle->addTooltip(sprintf(_("Return to %s"), $pagename));
-    
-        $t = new Template('info', $request);
+        $t = new Template('info', $request,
+                          array('revision' => $revision));
         return $t;
-
-//The rest here is just feeble experimentation, to try to give Template what it needs to allow it to be called from here.
-
-//        return $t->asXML();
-//        return $t->printXML();
-
-//        $html = Template('browse', array('CONTENT' => $t));
-//        GeneratePage($t, $pagetitle, $revision);
-
-
-//        $html = Template('browse', array('CONTENT' => $t));
-
-//          return $html;
-//        return $html->asXML();
-//        return $html->printXML();
-
-//        GeneratePage('info', $pagetitle, $version);
-//        GeneratePage($html, $pagetitle, $version);
-//        GeneratePage($html, $pagetitle, $revision);
-//        flush();
     }
 };
 
