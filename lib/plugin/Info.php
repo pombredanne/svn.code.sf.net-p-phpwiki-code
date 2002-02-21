a391e661b94b31b9f396022c1a39fcc55b4843ff
carstenklapp <carstenklapp@96ab9672-09ca-45d6-a79d-3d69d39ca109>
2002-02-21 06:09:11 +0000

    Info action is now an actionpage-plugin. (Thanks Jeff, for fixing this!!)

--- lib/plugin/Info.php
+++ lib/plugin/Info.php
@@ -1,9 +1,6 @@
 <?php // -*-php-*-
-rcs_id('$Id: Info.php,v 1.2 2002-02-21 03:24:19 dairiki Exp $');
+rcs_id('$Id: Info.php,v 1.3 2002-02-21 06:09:11 carstenklapp Exp $');
 /**
- *
- * Sorry, this isn't finished yet.
- *
  *
  */
 class WikiPlugin_Info
@@ -37,11 +34,6 @@ extends WikiPlugin
             $revision = $page->getCurrentRevision();
         }
 
-        global $Theme;
-        $pagetitle = HTML(fmt("%s: %s", _("Info"),
-                              $Theme->linkExistingWikiWord($pagename, false, $revision)));
-
-        //$pagetitle->addTooltip(sprintf(_("Return to %s"), $pagename));
         $t = new Template('info', $request,
                           array('revision' => $revision));
         return $t;
