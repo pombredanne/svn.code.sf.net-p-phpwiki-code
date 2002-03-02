a111a7952dd3f3dd7b96fc0db8a3b36e9ee284be
carstenklapp <carstenklapp@96ab9672-09ca-45d6-a79d-3d69d39ca109>
2002-03-02 02:59:56 +0000

    This is a complete rewrite of the FuzzyPages plugin. The page-title comparson criteria have been greatly simplified and the new search results are now much more satisfying.

--- lib/plugin/FuzzyPages.php
+++ lib/plugin/FuzzyPages.php
@@ -1,15 +1,38 @@
 <?php // -*-php-*-
-rcs_id('$Id: FuzzyPages.php,v 1.5 2002-02-28 00:46:51 carstenklapp Exp $');
+rcs_id('$Id: FuzzyPages.php,v 1.6 2002-03-02 02:59:56 carstenklapp Exp $');
+/*
+ Copyright 1999, 2000, 2001, 2002 $ThePhpWikiProgrammingTeam
 
-//require_once('lib/TextSearchQuery.php');
+ This file is part of PhpWiki.
+
+ PhpWiki is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ PhpWiki is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with PhpWiki; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+
 //require_once('lib/PageList.php');
 
 /**
- * FuzzyChanges is an experimental plugin which looks for similar page titles.
- * 
- * Pages are considered similar if they sound similar - metaphone() (english only)
- * or if the name is written similar - levenshtein()
+ * FuzzyPages is plugin which searches for similar page titles.
  *
+ * Pages are considered similar by averaging the similarity scores of
+ * the spelling comparison and the metaphone comparison for each page
+ * title in the database (php's metaphone() is an improved soundex
+ * function).
+ *
+ * http://www.php.net/manual/en/function.similar-text.php
+ * http://www.php.net/manual/en/function.metaphone.php
  */
 class WikiPlugin_FuzzyPages
 extends WikiPlugin
@@ -23,62 +46,146 @@ extends WikiPlugin
     }
 
     function getDefaultArguments() {
-        return array('page'	=> '[pagename]',
-                     's'	=> false);
+        return array('page'  => '[pagename]',
+                     's'     => false,
+                     'debug' => false);
     }
+
+
+
+    function spelling_similarity($subject) {
+        $spelling_similarity_score = 0;
+        similar_text($subject, $this->_searchterm,
+                     &$spelling_similarity_score);
+        return $spelling_similarity_score;
+    }
+
+    function sound_similarity($subject) {
+        $sound_similarity_score = 0;
+        // similar_text() automatically calculates a percentage.
+        // http://www.php.net/manual/en/function.similar-text.php
+        similar_text(metaphone($subject), $this->_searchterm_metaphone,
+                     &$sound_similarity_score);
+        return $sound_similarity_score;
+    }
+
+    function averageSimilarities($subject) {
+        return ($this->spelling_similarity($subject)
+                + $this->sound_similarity($subject)) / 2;
+    }
+
+    function collectSimilarPages(&$list, &$dbi) {
+        if (! defined('MIN_SCORE_CUTOFF'))
+            define('MIN_SCORE_CUTOFF', 33);
+
+        $this->_searchterm_metaphone = metaphone($this->_searchterm);
+
+        $allPages = $dbi->getAllPages();
+
+        while ($pagehandle = $allPages->next()) {
+            $pagename = $pagehandle->getName();
+            $similarity_score = $this->averageSimilarities($pagename);
+            if ($similarity_score > MIN_SCORE_CUTOFF)
+                $list = array_merge($list,
+                                    array($pagename => $similarity_score));
+        }
+    }
+
+    function sortCollectedPages(&$list) {
+        array_multisort(&$list, SORT_NUMERIC, SORT_DESC);
+    }
+
+
+
+    function addTableCaption(&$table, &$dbi) {
+        if ($dbi->isWikiPage($this->_searchterm))
+            $link = WikiLink($this->_searchterm, 'auto');
+        else
+            $link = $this->_searchterm;
+        $caption = fmt("These page titles match fuzzy with '%s'", $link);
+        $table->pushContent(HTML::caption(array('align'=>'top'), $caption));
+    }
+
+    function addTableHead(&$table) {
+        $row = HTML::tr(HTML::td(_("Name")),
+                        HTML::td(array('align' => 'right'), _("Score")));
+        if ($this->debug)
+            $this->_pushDebugHeadingTDinto($row);
+
+        $table->pushContent(HTML::thead($row));
+    }
+
+    function addTableBody(&$list, &$table) {
+        if (! defined('HIGHLIGHT_ROWS_CUTOFF_SCORE'))
+            define('HIGHLIGHT_ROWS_CUTOFF_SCORE', 60);
+
+        $tbody = HTML::tbody();
+        foreach ($list as $found_pagename => $score) {
+            $row = HTML::tr(array('class' =>
+                                  $score > HIGHLIGHT_ROWS_CUTOFF_SCORE
+                                  ? 'oddrow' : 'evenrow'),
+                            HTML::td(WikiLink($found_pagename)),
+                            HTML::td(array('align' => 'right'),
+                                     round($score)));
+
+            if ($this->debug)
+                $this->_pushDebugTDinto($row, $found_pagename);
+
+            $tbody->pushContent($row);
+        }
+        $table->pushContent($tbody);
+    }
+
+    function formatTable(&$list, &$dbi) {
+
+        $table = HTML::table(array('cellpadding' => 2,
+                                   'cellspacing' => 1,
+                                   'border'      => 0,
+                                   'class' => 'pagelist'));
+        $this->addTableCaption($table, &$dbi);
+        $this->addTableHead($table);
+        $this->addTableBody($list, $table);
+        return $table;
+    }
+
+
 
     function run($dbi, $argstr, $request) {
         $args = $this->getArgs($argstr, $request);
         extract($args);
         if (empty($page))
             return '';
+        $this->debug = $debug;
 
-        $thispage = $page;
-if ($s) $thispage = $s;
-        $list = array();
-        $pages = $dbi->getAllPages();
+        $this->_searchterm = $s ? $s : $page;
+        $this->_list = array();
 
-        while ($page = $pages->next()) {
-            $name = $page->getName();
+        $this->collectSimilarPages($this->_list, &$dbi);
 
-            // name length comparison
-            $lengthdiff = abs(strlen($name) - strlen($thispage));
+        $this->sortCollectedPages($this->_list);
 
-            // english soundex comparison, higher = similar
-            $metaphone_similar = similar_text(metaphone($thispage), metaphone($name));
+        return $this->formatTable($this->_list, &$dbi);
+    }
 
-            // spelling similarity comparison, 0 = identical
-            $levenshtein = levenshtein($thispage, $name);
 
-            // fudge factors
-            $metamin = 2;
-            $lengthdiffweight = 0.5;
-            $metaphone_priority_factor = 2.5;
-            $levenmax = 5 + $lengthdiffweight * $lengthdiff;
 
-            $similarity_score = $metaphone_priority_factor * $metaphone_similar - $levenshtein;
+    function _pushDebugHeadingTDinto(&$row) {
+            $row->pushContent(HTML::td(_("Spelling Score")),
+                              HTML::td(_("Sound Score")),
+                              HTML::td('Metaphones'));
+    }
 
-            if ($similarity_score < -20 || $levenshtein > $levenmax || $metaphone_similar < $metamin)
-                continue;
-            $offset = +8; // more fudge
-            $list = array_merge($list, array($name => $similarity_score + $offset));
-        }
+    function _pushDebugTDinto(&$row, $pagename) {
+        // This actually calculates everything a second time for each page
+        // but the individual scores are retained separately.
+        $debug_spelling = round($this->spelling_similarity($pagename), 1);
+        $debug_sound = round($this->sound_similarity($pagename), 1);
+        $debug_metaphone = sprintf("(%s, %s)", metaphone($pagename),
+                                   $this->_searchterm_metaphone);
 
-        array_multisort($list, SORT_NUMERIC, SORT_DESC);
-
-        $table = HTML::table(array('cellpadding' => 2,
-                                   'cellspacing' => 1,
-                                   'border'      => 0,
-                                   'class'	 => 'pagelist'));
-        $descrip = fmt("These page titles match fuzzy with '%s'",
-                        WikiLink($thispage, 'auto'));
-        $table->pushContent(HTML::caption(array('align'=>'top'), $descrip));
-        foreach ($list as $key => $val) {
-            $row = HTML::tr(HTML::td(WikiLink($key)),
-                            HTML::td(array('align' => 'right'), $val));
-            $table->pushContent($row);
-        }
-        return $table;
+        $row->pushcontent(HTML::td(array('align' => 'center'), $debug_spelling),
+                          HTML::td(array('align' => 'center'), $debug_sound),
+                          HTML::td($debug_metaphone));
     }
 };
 
@@ -88,5 +195,5 @@ if ($s) $thispage = $s;
 // c-basic-offset: 4
 // c-hanging-comment-ender-p: nil
 // indent-tabs-mode: nil
-// End:   
+// End:
 ?>
