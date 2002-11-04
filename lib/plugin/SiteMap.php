ad974b4011dc9d17a184b6c1d2c695c524e13add
carstenklapp <carstenklapp@96ab9672-09ca-45d6-a79d-3d69d39ca109>
2002-11-04 06:48:46 +0000

    reformat source

--- lib/plugin/SiteMap.php
+++ lib/plugin/SiteMap.php
@@ -1,16 +1,16 @@
 <?php // -*-php-*-
-rcs_id('$Id: SiteMap.php,v 1.3 2002-11-04 06:45:48 carstenklapp Exp $');
+rcs_id('$Id: SiteMap.php,v 1.4 2002-11-04 06:48:46 carstenklapp Exp $');
 /**
 http://sourceforge.net/tracker/?func=detail&aid=537380&group_id=6121&atid=306121
 
 Submitted By: Cuthbert Cat (cuthbertcat)
 
-This is a quick mod of BackLinks to do the job
-recursively. If your site is categorized correctly, and
-all the categories are listed in CategoryCategory, then
-a RecBackLinks there will produce a contents page for
-the entire site.
-The list is as per the recursion level.
+This is a quick mod of BackLinks to do the job recursively. If your
+site is categorized correctly, and all the categories are listed in
+CategoryCategory, then a RecBackLinks there will produce a contents
+page for the entire site.
+
+The list is as deep as the recursion level.
 */
 
 require_once('lib/PageList.php');
@@ -23,9 +23,10 @@ extends WikiPlugin
     }
 
     function getDescription () {
-        return sprintf(_("SiteMap: Recursively get BackLinks for %s"),'[pagename]');
+        return sprintf(_("SiteMap: Recursively get BackLinks for %s"),
+                       '[pagename]');
     }
-  
+
     function getDefaultArguments() {
         return array('exclude'		=> '',
                      'include_self'	=> 0,
@@ -36,48 +37,60 @@ extends WikiPlugin
                      'info'		=> false
                      );
     }
-    // info arg allows multiple columns info=mtime,hits,summary,version,author,locked,minor
-    // exclude arg allows multiple pagenames exclude=HomePage,RecentChanges
+    // info arg allows multiple columns
+    // info=mtime,hits,summary,version,author,locked,minor
+    //
+    // exclude arg allows multiple pagenames
+    // exclude=HomePage,RecentChanges
 
-    function recursivelyGetLinks($startpage, $pagearr, 
-                                 $level = '*', $reclimit = '***') {
+    function recursivelyGetLinks($startpage, $pagearr, $level = '*', 
+                                 $reclimit = '***') {
         static $VisitedPages = array();
 
         $startpagename = $startpage->getName();
-// echo "<br> recursivelyGetLinks( " . $startpagename . " , " . $level . " )\n";
-        if ($level == $reclimit) return $pagearr;
-        if (in_array($startpagename,$VisitedPages)) return $pagearr;
-        array_push($VisitedPages,$startpagename);
+        // echo "<br> recursivelyGetLinks( " . $startpagename . " , "
+        // . $level . " )\n";
+        if ($level == $reclimit)
+            return $pagearr;
+        if (in_array($startpagename, $VisitedPages))
+            return $pagearr;
+        array_push($VisitedPages, $startpagename);
         $pagelinks = $startpage->getLinks();
         while ($link = $pagelinks->next()) {
             $linkpagename = $link->getName();
             if (($linkpagename != $startpagename)
                 && !in_array($linkpagename, $this->ExcludedPages)) {
                 $pagearr[$level . " [$linkpagename]"] = $link;
-                $pagearr = $this->recursivelyGetLinks($link, $pagearr, $level . '*', $reclimit);
+                $pagearr = $this->recursivelyGetLinks($link, $pagearr, 
+                                                      $level . '*', $reclimit);
             }
         }
         return $pagearr;
     }
- 
+
     function run($dbi, $argstr, $request) {
         $args = $this->getArgs($argstr, $request, false);
         extract($args);
-        if (!$page) return '';
+        if (!$page)
+            return '';
         $out = '';
         $exclude = $exclude ? explode(",", $exclude) : array();
-        if (!$include_self) $exclude[] = $page;
+        if (!$include_self)
+            $exclude[] = $page;
         $this->ExcludedPages = $exclude;
-        $this->_default_limit = str_pad('',3,'*');
+        $this->_default_limit = str_pad('', 3, '*');
         if (is_numeric($reclimit)) {
-            if ($reclimit < 0) $reclimit = 0;
-            if ($reclimit > 10) $reclimit = 10;
-            $limit = str_pad('',$reclimit+2,'*');
+            if ($reclimit < 0)
+                $reclimit = 0;
+            if ($reclimit > 10)
+                $reclimit = 10;
+            $limit = str_pad('', $reclimit + 2, '*');
         } else {
             $limit = '***';
         }
         if (! $noheader) 
-            $out .= $description . " " . sprintf(_("(max. recursion level: %d)"), $reclimit) . ":\n\n";
+            $out .= $description ." ". sprintf(_("(max. recursion level: %d)"),
+                                               $reclimit) . ":\n\n";
         $pagelist = new PageList($info, $exclude);
         $p = $dbi->getPage($page);
 
@@ -99,6 +112,6 @@ extends WikiPlugin
 // c-basic-offset: 4
 // c-hanging-comment-ender-p: nil
 // indent-tabs-mode: nil
-// End:
+// End:      
         
 ?>
