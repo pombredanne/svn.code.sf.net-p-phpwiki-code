4f828e9d4c75a0cae6746ebc0e9e1ed206f5d8c4
carstenklapp <carstenklapp@96ab9672-09ca-45d6-a79d-3d69d39ca109>
2003-01-06 00:08:08 +0000

    Added a basic WikiBlog page type, takes advantage of editpage summary field.

--- lib/PageType.php
+++ lib/PageType.php
@@ -1,4 +1,5 @@
-<?php rcs_id('$Id: PageType.php,v 1.13 2002-09-04 20:39:47 dairiki Exp $');
+<?php // -*-php-*-
+rcs_id('$Id: PageType.php,v 1.14 2003-01-06 00:08:08 carstenklapp Exp $');
 /*
  Copyright 1999, 2000, 2001, 2002 $ThePhpWikiProgrammingTeam
 
@@ -19,9 +20,7 @@
  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-
 include_once('lib/BlockParser.php');
-
 
 /**
  * Get a PageType
@@ -31,9 +30,9 @@ include_once('lib/BlockParser.php');
  * require_once('lib/PageType.php');
  * $transformedContent = PageType($pagerevisionhandle, $pagename, $markup);
  *
- * The pagename and markup args are only required when displaying the content
- * for an edit preview, otherwise they will be extracted from the page $revision
- * instance.
+ * The pagename and markup args are only required when displaying the
+ * content for an edit preview, otherwise they will be extracted from
+ * the page $revision instance.
  *
  * See http://phpwiki.sourceforge.net/phpwiki/PageType
  */
@@ -41,40 +40,69 @@ function PageType(&$rev, $pagename = false, $markup = false) {
 
     if (isa($rev, 'WikiDB_PageRevision')) {
         $text = $rev->getPackedContent();
-        $pagename = $rev->getPageName(); 
+        $pagename = $rev->getPageName();
         $markup = $rev->get('markup');
 
-    } else {
-        // Hopefully only an edit preview gets us here, else we might be screwed.
+    }
+    else {
+        // Hopefully only an edit preview gets us here, else we might
+        // be screwed.
         if ($pagename == false) {
-            $error_text = "DEBUG: \$rev was not a 'WikiDB_PageRevision'. (Are you not previewing a page edit?)"; //debugging message only
+            //debugging message only
+            $error_text = "DEBUG: \$rev was not a 'WikiDB_PageRevision'. (Are you not previewing a page edit?)";
             trigger_error($error_text, E_USER_NOTICE);
         }
         $text = $rev;
     }
 
-    // PageType currently only works with InterWikiMap.
-    // Once a contentType field has been implemented in the
-    // database then that can be used instead of this pagename check.
-    $i = _("InterWikiMap");
+    // PageType currently only works with InterWikiMap and WikiBlog.
+    // Once a contentType field has been implemented in the database
+    // then that can be used instead of this pagename check.
+
+    /**
+     * Check whether pagename is InterWikiMap.
+     */
+    $isInterWikiMap = _("InterWikiMap");
+
+    /**
+     * Check whether pagename indicates a blog. Adapted from the
+     * WikiBlog plugin.
+     */
+    // get subpage basename
+    $escpage = array_shift(explode(SUBPAGE_SEPARATOR, $pagename));
+    if (preg_match("/^$escpage\/Blog-([[:digit:]]{14})$/", $pagename, $matches))
+        $isWikiBlog = $pagename;
+    else
+        $isWikiBlog = "";
+
     switch($pagename) {
-        case $i:
+        case $isInterWikiMap:
             $ContentTemplateName = 'interwikimap';
             $content_template = new interWikiMapPageType($text, $markup);
+            //trigger_error("DEBUG: PageType is an InterWikiMap");
+            break;
+        case $isWikiBlog:
+            $ContentTemplateName = 'wikiblog';
+            $content_template = new wikiBlogPageType($text, $markup);
+            $content_template->_summary = $rev->get('summary');
+            //trigger_error("DEBUG: PageType is a WikiBlog");
             break;
         default:
             $ContentTemplateName = 'wikitext';
             $content_template = new PageType($text, $markup);
+            //trigger_error("DEBUG: PageType is default");
     }
 
     return $content_template->getContent();
 }
 
-
 /**
+ * The basic PageType formats a standard WikiPage with one section:
  *
+ * - wikitext
  */
-class PageType {
+class PageType
+{
     /**
      * This is a simple WikiPage
      */
@@ -87,22 +115,22 @@ class PageType {
         $this->_markup = $markup;
         $this->_html = HTML();
 
-        $this->_defineSections();
-        $this->_populateSections();
     }
 
     function _defineSections() {
         /**
          * ... section_id => ('css_class', $this->_section_function)
          */
-        $this->_divs = array('wikitext' => array('wikitext', $this->_extractText()));
+        $this->_divs = array('wikitext' => array('wikitext',
+                                                 $this->_extractText()));
     }
 
     function _populateSections() {
         foreach ($this->_divs as $section => $data) {
             list($class, $function) = $data;
             if (!empty($function))
-                $this->_html->pushContent(HTML::div(array('class' => $class), $function));
+                $this->_html->pushContent(HTML::div(array('class' => $class),
+                                                    $function));
         }
     }
 
@@ -111,6 +139,7 @@ class PageType {
          * Custom text extractions might want to check if the section
          * contains any text using trim() before returning any
          * transformed text, to avoid displaying blank boxes.
+         *
          * See interWikiMapPageType->_extractStartText()
          * and interWikiMapPageType->_extractEndText() for examples.
          */
@@ -118,13 +147,54 @@ class PageType {
     }
 
     function getContent() {
+        // _defineSections & _populateSections execution moved to here
+        // from constructor, to allow custom vars (used for WikiBlog
+        // page summary field)
+        $this->_defineSections();
+        $this->_populateSections();
         return $this->_html;
     }
 };
 
+/**
+ * wikiBlogPageType formats a Wiki page as a blog, with two sections:
+ *
+ * - wikiblog-summary
+ * - wikitext
+ */
+class wikiBlogPageType
+extends PageType
+{
+    var $_summary = "";
 
-class interWikiMapPageType extends PageType {
+    function _defineSections() {
+        /**
+         * section_id => ('css_class', $this->_section_function)
+         */
+        // FIXME: Create new css styles
+        $this->_divs = array('wikiblog-summary' => array('wikitext', $this->_extractSummary()),
+                             'wikitext' => array('wikitext', $this->_extractText())
+                             );
+    }
 
+    function _extractSummary() {
+        return HTML(HTML::strong(array('class' => 'wikiblog-label'),
+                                 _("Summary:")),
+                    " ", $this->_summary);
+    }
+};
+
+/**
+ * interWikiMapPageType formats a Wiki page as an InterWikiMap, with
+ * up to three sections:
+ *
+ * - interwikimap-header
+ * - interwikimap
+ * - interwikimap-footer
+ */
+class interWikiMapPageType
+extends PageType
+{
     function _defineSections() {
         /**
          * section_id => ('css_class', $this->_section_function)
@@ -156,9 +226,11 @@ class interWikiMapPageType extends PageType {
                 if ($dbi->isWikiPage($moniker)) {
                     $moniker = WikiLink($moniker);
                 }
-                $moniker = HTML::td(array('class' => 'interwiki-moniker'), $moniker);
-                $interurl = HTML::td(array('class' =>'interwiki-url'), HTML::tt($interurl));
-                
+                $moniker = HTML::td(array('class' => 'interwiki-moniker'),
+                                    $moniker);
+                $interurl = HTML::td(array('class' =>'interwiki-url'),
+                                     HTML::tt($interurl));
+
                 $tbody->pushContent(HTML::tr($moniker, $interurl));
             }
         }
@@ -194,9 +266,9 @@ class interWikiMapPageType extends PageType {
         }
         return "";
     }
-
 };
 
+// $Log: not supported by cvs2svn $
 
 // Local Variables:
 // mode: php
