d9b6df98c5c6e5ca5ffae3e4ef3eca5e2d52fe71
carstenklapp <carstenklapp@96ab9672-09ca-45d6-a79d-3d69d39ca109>
2003-01-18 21:19:25 +0000

    Code cleanup: Reformatting; added copyleft, getVersion, getDescription

--- trunk/lib/plugin/BackLinks.php
+++ trunk/lib/plugin/BackLinks.php
@@ -1,38 +1,63 @@
 <?php // -*-php-*-
-rcs_id('$Id: BackLinks.php,v 1.18 2002-01-31 01:14:14 dairiki Exp $');
+rcs_id('$Id: BackLinks.php,v 1.19 2003-01-18 21:19:25 carstenklapp Exp $');
 /**
+ Copyright 1999, 2000, 2001, 2002 $ThePhpWikiProgrammingTeam
+
+ This file is part of PhpWiki.
+
+ PhpWiki is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ PhpWiki is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with PhpWiki; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
+/**
+ */
 require_once('lib/PageList.php');
 
 class WikiPlugin_BackLinks
 extends WikiPlugin
 {
-    function getName () {
+    function getName() {
         return _("BackLinks");
     }
 
-    function getDescription () {
-        return sprintf(_("Get BackLinks for %s"),'[pagename]');
+    function getDescription() {
+        return sprintf(_("List all pages which link to %s."), '[pagename]');
     }
-  
+
+    function getVersion() {
+        return preg_replace("/[Revision: $]/", '',
+                            "\$Revision: 1.19 $");
+    }
+
     function getDefaultArguments() {
-        return array('exclude'		=> '',
-                     'include_self'	=> 0,
-                     'noheader'		=> 0,
-                     'page'		=> '[pagename]',
-                     'info'		=> false
+        return array('exclude'      => '',
+                     'include_self' => 0,
+                     'noheader'     => 0,
+                     'page'         => '[pagename]',
+                     'info'         => false
                      );
     }
-    // info arg allows multiple columns info=mtime,hits,summary,version,author,locked,minor
+    // info arg allows multiple columns
+    // info=mtime,hits,summary,version,author,locked,minor
     // exclude arg allows multiple pagenames exclude=HomePage,RecentChanges
- 
+
     function run($dbi, $argstr, $request) {
         $this->_args = $this->getArgs($argstr, $request);
         extract($this->_args);
         if (!$page)
             return '';
-        
+
         $exclude = $exclude ? explode(",", $exclude) : array();
         if (!$include_self)
             $exclude[] = $page;
@@ -61,6 +86,8 @@ extends WikiPlugin
 
 };
 
+// $Log: not supported by cvs2svn $
+
 // For emacs users
 // Local Variables:
 // mode: php
@@ -69,5 +96,4 @@ extends WikiPlugin
 // c-hanging-comment-ender-p: nil
 // indent-tabs-mode: nil
 // End:
-        
 ?>
--- trunk/lib/plugin/_BackendInfo.php
+++ trunk/lib/plugin/_BackendInfo.php
@@ -1,5 +1,25 @@
 <?php // -*-php-*-
-rcs_id('$Id: _BackendInfo.php,v 1.19 2002-10-31 03:28:31 carstenklapp Exp $');
+rcs_id('$Id: _BackendInfo.php,v 1.20 2003-01-18 21:19:24 carstenklapp Exp $');
+/**
+ Copyright 1999, 2000, 2001, 2002 $ThePhpWikiProgrammingTeam
+
+ This file is part of PhpWiki.
+
+ PhpWiki is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ PhpWiki is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with PhpWiki; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
 require_once('lib/Template.php');
 /**
  */
@@ -14,6 +34,11 @@ extends WikiPlugin
         return sprintf(_("Get debugging information for %s."), '[pagename]');
     }
 
+    function getVersion() {
+        return preg_replace("/[Revision: $]/", '',
+                            "\$Revision: 1.20 $");
+    }
+
     function getDefaultArguments() {
         return array('page' => '[pagename]');
     }
@@ -26,7 +51,8 @@ extends WikiPlugin
 
         $backend = &$dbi->_backend;
 
-        $html = HTML(HTML::h3(fmt("Querying backend directly for '%s'", $page)));
+        $html = HTML(HTML::h3(fmt("Querying backend directly for '%s'",
+                                  $page)));
 
 
         $table = HTML::table(array('border' => 1,
@@ -37,59 +63,63 @@ extends WikiPlugin
             $html->pushContent(HTML::p(fmt("No pagedata for %s", $page)));
         else {
             $table->pushContent($this->_showhash("get_pagedata('$page')",
-                                                 $pagedata,$page));
+                                                 $pagedata, $page));
         }
 
         for ($version = $backend->get_latest_version($page);
              $version;
              $version = $backend->get_previous_version($page, $version))
-        {
-            $vdata = $backend->get_versiondata($page, $version, true);
+            {
+                $vdata = $backend->get_versiondata($page, $version, true);
 
-            $content = &$vdata['%content'];
-            if ($content === true)
-                $content = '<true>';
-            elseif (strlen($content) > 40)
-                $content = substr($content,0,40) . " ...";
-            unset($vdata['%pagedata']); // problem in backend
-            $table->pushContent(HTML::tr(HTML::td(array('colspan' => 2))));
-            $table->pushContent($this->_showhash("get_versiondata('$page',$version)",
-                                                 $vdata));
-        }
+                $content = &$vdata['%content'];
+                if ($content === true)
+                    $content = '<true>';
+                elseif (strlen($content) > 40)
+                    $content = substr($content,0,40) . " ...";
+                unset($vdata['%pagedata']); // problem in backend
+                $table->pushContent(HTML::tr(HTML::td(array('colspan' => 2))));
+                $table->pushContent($this->_showhash("get_versiondata('$page',$version)",
+                                                     $vdata));
+            }
 
         $html->pushContent($table);
         return $html;
     }
 
-    function _showhash ($heading, $hash, $pagename='') {
+    function _showhash ($heading, $hash, $pagename = '') {
         $rows[] = HTML::tr(array('bgcolor' => '#ffcccc',
                                  'style' => 'color:#000000'),
                            HTML::td(array('colspan' => 2,
-                                          'style' => 'color:#000000'), $heading));
+                                          'style' => 'color:#000000'),
+                                    $heading));
         ksort($hash);
         foreach ($hash as $key => $val) {
-            if (is_string($val) and (substr($val,0,2) == 'a:')) {
+            if (is_string($val) && (substr($val, 0, 2) == 'a:')) {
                 // how to indent this table?
                 $val = unserialize($val);
                 $rows[] = HTML(HTML::raw('&nbsp;'), HTML::raw('&nbsp;'),
-                               $this->_showhash ("get_pagedata('$pagename')['$key']",
+                               $this->_showhash("get_pagedata('$pagename')['$key']",
                                                  $val));
             } else {
-                if ($key == 'passwd' and ! $request->_user->isAdmin())
+                if ($key == 'passwd' && ! $request->_user->isAdmin())
                     $val = $val ? _("<not displayed>") : _("<empty>");
                 $rows[] = HTML::tr(HTML::td(array('align' => 'right',
                                                   'bgcolor' => '#cccccc',
                                                   'style' => 'color:#000000'),
-                                            HTML(HTML::raw('&nbsp;'), $key, HTML::raw('&nbsp;'))),
-                               HTML::td(array('bgcolor' => '#ffffff',
-                                              'style' => 'color:#000000'),
-                                        $val ? $val : HTML::raw('&nbsp;')));
+                                            HTML(HTML::raw('&nbsp;'), $key,
+                                                 HTML::raw('&nbsp;'))),
+                                   HTML::td(array('bgcolor' => '#ffffff',
+                                                  'style' => 'color:#000000'),
+                                            $val ? $val : HTML::raw('&nbsp;'))
+                                   );
             }
         }
         return $rows;
     }
 };
 
+// $Log: not supported by cvs2svn $
 
 // (c-file-style: "gnu")
 // Local Variables:
