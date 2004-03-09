1cd470ede65690a941736a0fcd0b955e82c676b3
rurban <rurban@96ab9672-09ca-45d6-a79d-3d69d39ca109>
2004-03-09 13:08:40 +0000

    fix undefined TransformText error: load BlockParser, get rid of warnings

--- trunk/lib/plugin/RichTable.php
+++ trunk/lib/plugin/RichTable.php
@@ -1,11 +1,11 @@
-<?php 
-
-/* 
-
+<?php // -*-php-*-
+rcs_id('$Id: RichTable.php,v 1.4 2004-03-09 13:08:40 rurban Exp $');
+/**
   RichTablePlugin
   A PhpWiki plugin that allows insertion of tables using a richer syntax
   http://www.it.iitb.ac.in/~sameerds/phpwiki/index.php/RichTablePlugin
- 
+*/
+/* 
   Copyright (C) 2003 Sameer D. Sahasrabuddhe
   
   This program is free software; you can redistribute it and/or
@@ -21,10 +21,7 @@
   You should have received a copy of the GNU General Public License
   along with this program; if not, write to the Free Software
   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
-
 */
-
-rcs_id('$Id: RichTable.php,v 1.3 2004-02-28 21:14:08 rurban Exp $');
 
 // error_reporting (E_ALL & ~E_NOTICE);
 
@@ -45,101 +42,112 @@ extends WikiPlugin
 
     function getVersion() {
         return preg_replace("/[Revision: $]/", '',
-                            "\$Revision: 1.3 $");
+                            "\$Revision: 1.4 $");
     }
 
     function run($dbi, $argstr, &$request, $basepage) {
-
     	global $Theme;
+        include_once("lib/BlockParser.php");
 
         $lines = preg_split('/\n/', $argstr);
         $table = HTML::table();
  
-		if ($lines[0][0] == '*') {
-			$line = substr(array_shift($lines),1);
-			$attrs = $this->_parse_attr($line);
-			foreach ($attrs as $key => $value) {
-				if (in_array ($key, array("id", "class", "title", "style",
-								"bgcolor", "frame", "rules", "border",
-								"cellspacing", "cellpadding",
-								"summary", "align", "width"))) {
-					$table->setAttr($key, $value);
-				}
-			}
-		}
+        if ($lines[0][0] == '*') {
+            $line = substr(array_shift($lines),1);
+            $attrs = $this->_parse_attr($line);
+            foreach ($attrs as $key => $value) {
+                if (in_array ($key, array("id", "class", "title", "style",
+                                          "bgcolor", "frame", "rules", "border",
+                                          "cellspacing", "cellpadding",
+                                          "summary", "align", "width"))) {
+                    $table->setAttr($key, $value);
+                }
+            }
+        }
+        
+        foreach ($lines as $line){
+            if (substr($line,0,1) == "-") {
+                if (isset($row)) {
+                    if (isset($cell)) {
+                        if (isset($content)) {
+                            $cell->pushContent(TransformText($content));
+                            unset($content);
+                        }
+                        $row->pushContent($cell);
+                        unset($cell);
+                    }
+                    $table->pushContent($row);
+                }	
+                $row = HTML::tr();
+                $attrs = $this->_parse_attr(substr($line,1));
+                foreach ($attrs as $key => $value) {
+                    if (in_array ($key, array("id", "class", "title", "style",
+                                              "bgcolor", "align", "valign"))) {
+                        $row->setAttr($key, $value);
+                    }
+                }
+                continue;
+            }
+            if (substr($line,0,1) == "|" and isset($row)) {
+                if (isset($cell)) {
+                    if (isset ($content)) {
+                        $cell->pushContent(TransformText($content));
+                        unset($content);
+                    }
+                    $row->pushContent($cell);
+                }
+                $cell = HTML::td();
+                $line = substr($line, 1);
+                if ($line[0] == "*" ) {
+                    $attrs = $this->_parse_attr(substr($line,1));
+                    foreach ($attrs as $key => $value) {
+                        if (in_array ($key, array("id", "class", "title", "style",
+                                                  "colspan", "rowspan", "width", "height",
+                                                  "bgcolor", "align", "valign"))) {
+                            $cell->setAttr($key, $value);
+                        }
+                    }
+                    continue;
+                } 
+            } 
+            if (isset($row) and isset($cell)) {
+                $line = str_replace("?\>", "?>", $line);
+                $line = str_replace("\~", "~", $line);
+                if (empty($content)) $content = '';
+                $content .= $line . "\n";
+            }
+        }
+        if (isset($row)) {
+            if (isset($cell)) {
+                if (isset($content))
+                    $cell->pushContent(TransformText($content));
+                $row->pushContent($cell);
+            }
+            $table->pushContent($row);
+        }
+        return $table;
+    }
 
-		foreach ($lines as $line){
-			if ($line[0] == "-") {
-				if (isset($row)) {
-					if (isset($cell)) {
-						if (isset($content)) {
-							$cell->pushContent(TransformText($content));
-							unset($content);
-						}
-						$row->pushContent($cell);
-						unset($cell);
-					}
-					$table->pushContent($row);
-				}	
-				$row = HTML::tr();
-				$attrs = $this->_parse_attr(substr($line,1));
-				foreach ($attrs as $key => $value) {
-					if (in_array ($key, array("id", "class", "title", "style",
-										"bgcolor", "align", "valign"))) {
-						$row->setAttr($key, $value);
-					}
-				}
-				continue;
-			}
-			if ($line[0] == "|" and isset($row)) {
-				if (isset($cell)) {
-					if (isset ($content)) {
-						$cell->pushContent(TransformText($content));
-						unset($content);
-					}
-					$row->pushContent($cell);
-				}
-				$cell = HTML::td();
-				$line = substr($line, 1);
-				if ($line[0] == "*" ) {
-					$attrs = $this->_parse_attr(substr($line,1));
-					foreach ($attrs as $key => $value) {
-						if (in_array ($key, array("id", "class", "title", "style",
-											"colspan", "rowspan", "width", "height",
-											"bgcolor", "align", "valign"))) {
-							$cell->setAttr($key, $value);
-						}
-					}
-					continue;
-				} 
-			} 
-			if (isset($row) and isset($cell)) {
-				$line = str_replace("?\>", "?>", $line);
-				$line = str_replace("\~", "~", $line);
-				$content .= $line . "\n";
-			}
-		}
-		if (isset($row)) {
-			if (isset($cell)) {
-				if (isset($content))
-					$cell->pushContent(TransformText($content));
-				$row->pushContent($cell);
-			}
-			$table->pushContent($row);
-		}
+    function _parse_attr($line) {
+        $attr_chunks = preg_split("/\s*,\s*/", strtolower($line));
+        $options = array();
+        foreach ($attr_chunks as $attr_pair) {
+            if (empty($attr_pair)) continue;
+            $key_val = preg_split("/\s*=\s*/", $attr_pair);
+            $options[trim($key_val[0])] = trim($key_val[1]);
+        }
+        return $options;
+    }
+}
 
-		return $table;
-	}
+// $Log: not supported by cvs2svn $
 
-	function _parse_attr($line) {
-
-		$attr_chunks = preg_split("/\s*,\s*/", strtolower($line));
-		foreach ($attr_chunks as $attr_pair) {
-			$key_val = preg_split("/\s*=\s*/", $attr_pair);
-			$options[trim($key_val[0])] = trim($key_val[1]);
-		}
-		return $options;
-	}
-};
-
+// For emacs users
+// Local Variables:
+// mode: php
+// tab-width: 8
+// c-basic-offset: 4
+// c-hanging-comment-ender-p: nil
+// indent-tabs-mode: nil
+// End:
 ?>
