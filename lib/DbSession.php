db56466e7a65d0e768320ff622228a89ee410f1b
rurban <rurban@96ab9672-09ca-45d6-a79d-3d69d39ca109>
2004-03-10 15:08:59 +0000

    DB_session adodb support

--- lib/DbSession.php
+++ lib/DbSession.php
@@ -1,4 +1,4 @@
-<?php rcs_id('$Id: DbSession.php,v 1.6 2004-02-28 21:14:08 rurban Exp $');
+<?php rcs_id('$Id: DbSession.php,v 1.7 2004-03-10 15:08:59 rurban Exp $');
 
 /**
  * Store sessions data in Pear DB / ADODB ....
@@ -257,14 +257,207 @@ extends DB_Session
 }
 
 
-// adodb-session wrapper: warning! adodb-session uses different db layout
-class DB_Session_ADODB_notyet
+// self-written adodb-sessions
+class DB_Session_ADODB
 extends DB_Session
 {
     var $_backend_type = "ADODB";
 
-    function DB_Session_ADODB_notyet ($dbh, $table) {
+    function DB_Session_ADODB ($dbh, $table) {
+
+        $this->_dbh = &$dbh;
+        $this->_table = $table;
+
+        ini_set('session.save_handler','user');
+        session_module_name('user'); // new style
+        session_set_save_handler(array(&$this, 'open'),
+                                 array(&$this, 'close'),
+                                 array(&$this, 'read'),
+                                 array(&$this, 'write'),
+                                 array(&$this, 'destroy'),
+                                 array(&$this, 'gc'));
+        return $this;
     }
+
+    function _connect() {
+        global $DBParams;
+        static $parsed = false;
+        $dbh = &$this->_dbh;
+        if (!$dbh) {
+            if (!$parsed) $parsed = parseDSN($DBParams['dsn']);
+            $this->_dbh = &ADONewConnection($parsed['phptype']); // Probably only MySql works just now
+            $conn = $this->_dbh->Connect($parsed['hostspec'],$parsed['username'], 
+                                         $parsed['password'], $parsed['database']);
+            $dbh = &$this->_dbh;                             
+        }
+        return $dbh;
+    }
+    
+    function query($sql) {
+        return $this->_dbh->Execute($sql);
+    }
+
+    function quote($string) {
+        return $this->_dbh->qstr($string);
+    }
+
+    function _disconnect() {
+        if (!$this->_dbh)
+            $this->_dbh->close();
+    }
+
+    /**
+     * Opens a session.
+     *
+     * Actually this function is a fake for session_set_save_handle.
+     * @param  string $save_path a path to stored files
+     * @param  string $session_name a name of the concrete file
+     * @return boolean true just a variable to notify PHP that everything 
+     * is good.
+     * @access private
+     */
+    function open ($save_path, $session_name) {
+        //$this->log("_open($save_path, $session_name)");
+        return true;
+    }
+
+    /**
+     * Closes a session.
+     *
+     * This function is called just after <i>write</i> call.
+     *
+     * @return boolean true just a variable to notify PHP that everything 
+     * is good.
+     * @access private
+     */
+    function close() {
+        //$this->log("_close()");
+        return true;
+    }
+
+    /**
+     * Reads the session data from DB.
+     *
+     * @param  string $id an id of current session
+     * @return string
+     * @access private
+     */
+    function read ($id) {
+        //$this->log("_read($id)");
+        $dbh = &$this->_connect();
+        $table = $this->_table;
+        $qid = $dbh->quote($id);
+        $res = '';
+        $rs = $dbh->Execute("SELECT sess_data FROM $table WHERE sess_id=$qid");
+        if (!$rs->EOF) {
+            $res = $rs->fields["sess_data"];
+        }
+        $this->_disconnect();
+        if (!empty($res) and preg_match('|^[a-zA-Z0-9/+=]+$|', $res))
+            $res = base64_decode($res);
+        return $res;
+    }
+  
+    /**
+     * Saves the session data into DB.
+     *
+     * Just  a  comment:       The  "write"  handler  is  not 
+     * executed until after the output stream is closed. Thus,
+     * output from debugging statements in the "write" handler
+     * will  never be seen in the browser. If debugging output
+     * is  necessary, it is suggested that the debug output be
+     * written to a file instead.
+     *
+     * @param  string $id
+     * @param  string $sess_data
+     * @return boolean true if data saved successfully  and false
+     * otherwise.
+     * @access private
+     */
+    function write ($id, $sess_data) {
+        
+        $dbh = &$this->_connect();
+        $table = $this->_table;
+        $qid = $dbh->quote($id);
+        $time = time();
+
+        // postgres can't handle binary data in a TEXT field.
+        if (isa($dbh, 'DB_pgsql'))
+            $sess_data = base64_encode($sess_data);
+        $qdata = $dbh->quote($sess_data);
+        $res = $dbh->query("UPDATE $table"
+                           . " SET sess_data=$qdata, sess_date=$time"
+                           . " WHERE sess_id=$qid");
+        if (!$dbh->hasAffectedRows)
+            $res = $dbh->query("INSERT INTO $table"
+                               . " (sess_id, sess_data, sess_date)"
+                               . " VALUES ($qid, $qdata, $time)");
+        $this->_disconnect();
+        return ! $res->EOF;
+    }
+
+    /**
+     * Destroys a session.
+     *
+     * Removes a session from the table.
+     *
+     * @param  string $id
+     * @return boolean true 
+     * @access private
+     */
+    function destroy ($id) {
+        $dbh = &$this->_connect();
+        $table = $this->_table;
+        $qid = $dbh->quote($id);
+
+        $dbh->query("DELETE FROM $table WHERE sess_id=$qid");
+
+        $this->_disconnect();
+        return true;     
+    }
+
+    /**
+     * Cleans out all expired sessions.
+     *
+     * @param  int $maxlifetime session's time to live.
+     * @return boolean true
+     * @access private
+     */
+    function gc ($maxlifetime) {
+        $dbh = &$this->_connect();
+        $table = $this->_table;
+        $threshold = time() - $maxlifetime;
+
+        $dbh->query("DELETE FROM $table WHERE sess_date < $threshold");
+
+        $this->_disconnect();
+        return true;
+    }
+
+    // WhoIsOnline support
+    function currentSessions() {
+        $sessions = array();
+        $dbh = &$this->_connect();
+        $table = $this->_table;
+        $rs = $this->query("SELECT sess_data,sess_date FROM $table ORDER BY sess_date DESC");
+        if ($rs->EOF) {
+            return $sessions;
+        }
+        while ($row = $rs->fetchRow()) {
+            $data = $row['sess_data'];
+            $date = $row['sess_date'];
+            if (preg_match('|^[a-zA-Z0-9/+=]+$|', $data))
+                $data = base64_decode($data);
+            // session_data contains the <variable name> + "|" + <packed string>
+            // we need just the wiki_user object (might be array as well)
+            $user = strstr($data,"wiki_user|");
+            $sessions[] = array('wiki_user' => substr($user,10), // from "O:" onwards
+                                'date' => $date);
+        }
+        $this->_disconnect();
+        return $sessions;
+    }
+
 }
 
 // Local Variables:
