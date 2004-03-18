db84ca9377a9d6081e90acde1f024d047f32fc76
rurban <rurban@96ab9672-09ca-45d6-a79d-3d69d39ca109>
2004-03-18 20:17:47 +0000

    The missing sqlite pear module, but it doesn't work yet good enough.

--- lib/WikiDB/backend/PearDB_sqlite.php
+++ lib/WikiDB/backend/PearDB_sqlite.php
@@ -1,9 +1,24 @@
 <?php // -*-php-*-
-// SQLite PearDB backend by Matthew Palmer
-// The SQLite DB will gain popularity with the current MySQL vs PHP license drama.
-rcs_id('$Id: PearDB_sqlite.php,v 1.1 2004-03-17 14:40:37 rurban Exp $');
+/** 
+ * SQLite PearDB backend by Matthew Palmer
+ * The SQLite DB will gain popularity with the current MySQL vs PHP license drama.
+ * It's in core since PHP-5.0, MySQL not anymore.
+ *
+ * Initial setup:
+ * sqlite -init /tmp/phpwiki-sqlite.db 
+ * sqlite /tmp/phpwiki-sqlite.db < schemas/sqlite.sql
+ */
+rcs_id('$Id: PearDB_sqlite.php,v 1.2 2004-03-18 20:17:47 rurban Exp $');
 
 require_once('lib/WikiDB/backend/PearDB.php');
+
+//TODO: create tables on virgin wiki
+/*
+    $db = &new DB_sqlite();
+    $db->connect($DBParams['dsn'], array('persistent'=> true) );
+    $result = $db->query("CREATE TABLE $table (comment varchar(50), 
+      datetime varchar(50));");
+*/
 
 class WikiDB_backend_sqlite
 extends WikiDB_backend_PearDB
@@ -51,6 +66,30 @@ extends WikiDB_backend_PearDB
             return unserialize($data);
         return unserialize(base64_decode($data));
     }
+
+    // same as DB::getSpecialQuery('tables')
+    /*
+    function sqlite_list_tables (&$dblink) {
+       $tables = array ();
+       $sql = "SELECT name FROM sqlite_master WHERE (type = 'table')";
+       if ($res = sqlite_query ($dblink, $sql)) {
+           while (sqlite_has_more($res)) {
+               $tables[] = sqlite_fetch_single($res);
+           }
+       }
+       return $tables;
+    }
+    */
+
+   function _table_exists (&$dblink, $table) {
+       $sql = "SELECT count(name) FROM sqlite_master WHERE ((type = 'table') and (name = '$table'))";
+       if ($res = sqlite_query ($dblink, $sql)) {
+           return sqlite_fetch_single($res) > 0;
+       } else {
+           return false; // or throw exception
+       }
+   }
+
 };
     
 // (c-file-style: "gnu")
