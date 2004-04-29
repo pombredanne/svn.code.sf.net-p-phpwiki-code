1dcc35f21651a69d005c10ce887eaf211176ebb8
rurban <rurban@96ab9672-09ca-45d6-a79d-3d69d39ca109>
2004-04-29 18:32:38 +0000

    Re-implement the classic phpwiki-1.2 feature of the  * 5 best incoming links: xx, xx, xx, ...  * 5 best outgoing links: xx, xx, xx, ...  * 5 most popular nearby: xx, xx, xx, ...

---
+++ trunk/lib/plugin/PopularNearby.php
@@ -0,0 +1,165 @@
+<?php // -*-php-*-
+rcs_id('$Id: PopularNearby.php,v 1.1 2004-04-29 18:32:38 rurban Exp $');
+/*
+ Copyright 2004 $ThePhpWikiProgrammingTeam
+
+ This file is part of PhpWiki.
+
+ PhpWiki is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ PhpWiki is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with PhpWiki; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+/** Re-implement the classic phpwiki-1.2 feature of the 
+ *  popular nearby pages, specific to the from/to links:
+ *    5 best incoming links: xx, xx, xx, ...
+ *    5 best outgoing links: xx, xx, xx, ...
+ *    5 most popular nearby: xx, xx, xx, ...
+ */
+/* Usage:
+
+* <?plugin PopularNearby mode=incoming ?>
+* <?plugin PopularNearby mode=outgoing ?>
+* <?plugin PopularNearby mode=nearby ?>
+
+*/
+ 
+
+require_once('lib/PageList.php');
+
+class WikiPlugin_PopularNearby
+extends WikiPlugin
+{
+    function getName () {
+        return _("PopularNearby");
+    }
+
+    function getDescription () {
+        return _("List the most popular pages nearby.");
+    }
+
+    function getVersion() {
+        return preg_replace("/[Revision: $]/", '',
+                            "\$Revision: 1.1 $");
+    }
+
+    function getDefaultArguments() {
+        return array('pagename' => '[pagename]',
+                     'mode'     => 'nearby', // or 'incoming' or 'outgoing'
+                     'exclude'  => '',
+                     'limit'    => 5, // limit <0 returns least popular pages
+                     'noheader' => 0,
+                    );
+    }
+    
+    function run($dbi, $argstr, &$request, $basepage) {
+    	$args = $this->getArgs($argstr, $request);
+        extract($args);
+        $header = '';
+        $page = $dbi->getPage($pagename);
+        switch ($mode) {
+        case 'incoming': // not the hits, but the number of links
+            if (! $noheader )
+                $header = sprintf(_("%d best incoming links: "),$limit); 
+            $links = $this->sortedLinks($page->getLinks("reversed"),"reversed",$limit);
+            break;
+        case 'outgoing': // not the hits, but the number of links
+            if (! $noheader )
+                $header = sprintf(_("%d best outgoing links: "),$limit); 
+            $links = $this->sortedLinks($page->getLinks(),false,$limit);
+            break;
+        case 'nearby':  // all linksfrom and linksto, sorted by hits 
+            if (! $noheader )
+                $header = sprintf(_("%d most popular nearby: "),$limit); 
+            $inlinks = $page->getLinks();
+            $outlinks = $page->getLinks('reversed');
+            $links = $this->sortedLinks(array_merge($inlinks->asArray(),
+                                                    $outlinks->asArray()),
+                                        false,$limit);
+            break;
+        }
+        // if (count($links) > $limit) array_splice($links, $limit);
+        $html = HTML($header);
+        for ($i=0; $i<count($links); $i++) {
+            $html->pushContent($links[$i]['format'],$i<count($links)-1?', ':'');
+        }
+        return $html;
+    }
+
+    /**
+     * Get and sort the links:
+     *   mode=nearby:   $pages Array
+     *   mode=incoming: $pages iter and $direction=true
+     *   mode=outgoing: $pages iter and $direction=false
+     *
+     * @access private
+     *
+     * @param $pages array of WikiDB_Page's or a Page_iterator
+     * @param $direction boolean: true if incoming links
+     * 
+     * @return Array of sorted links
+     */
+    function sortedLinks($pages, $direction=false, $limit=5) {
+    	$links = array();
+        if (is_array($pages)) {
+            foreach ($pages as $page) {
+                // just the number of hits
+                $hits = $page->get('hits');
+                if (!$hits) continue;
+                $links[] = array('hits' => $hits,
+                                 'pagename' => $page->_pagename,
+                                 'format' => HTML(WikiLink($page->_pagename),' (' . $hits . ')'));
+            }
+        } else {
+            while ($page = $pages->next()) {
+                // different score algorithm: 
+                //   the number of links to/from the page
+                $l = $page->getLinks(!$direction);
+                $score = $l->count();
+                if (!$score) continue;
+                $name = $page->_pagename;
+                $links[] = array('hits' => $score,
+                                 'pagename' => $name,
+                                 'format' => HTML(WikiLink($name),' (' . $score . ')'));
+            }
+            $pages->free();
+        }
+        if (count($links) > $limit)
+            array_splice($links, $limit);
+        return $this->sortByHits($links);
+    }
+
+    function sortByHits($links) {
+        if (!$links) return array();
+        usort($links,array('WikiPlugin_PopularNearby','cmp_hits'));
+        reset($links);
+        return $links;
+    }
+
+    function cmp_hits($a, $b) {
+        return ($a['hits'] < $b['hits']) ? 1 : ($a['hits'] == $b['hits']) ? 0 : -1;
+    }
+
+
+};
+
+// $Log: not supported by cvs2svn $
+
+// Local Variables:
+// mode: php
+// tab-width: 8
+// c-basic-offset: 4
+// c-hanging-comment-ender-p: nil
+// indent-tabs-mode: nil
+// End:
+?>
