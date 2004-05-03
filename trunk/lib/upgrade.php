4db8adf6bf8dcefd6c7de281d3538a9dd482ffda
rurban <rurban@96ab9672-09ca-45d6-a79d-3d69d39ca109>
2004-05-03 15:00:33 +0000

    added more database upgrading: session.sess_ip, page.id autp_increment

--- trunk/lib/upgrade.php
+++ trunk/lib/upgrade.php
@@ -1,5 +1,5 @@
 <?php //-*-php-*-
-rcs_id('$Id: upgrade.php,v 1.4 2004-05-02 21:26:38 rurban Exp $');
+rcs_id('$Id: upgrade.php,v 1.5 2004-05-03 15:00:33 rurban Exp $');
 
 /*
  Copyright 2004 $ThePhpWikiProgrammingTeam
@@ -47,6 +47,7 @@ require_once("lib/loadsave.php");
 
 // see loadsave.php for saving new pages.
 function CheckPgsrcUpdate(&$request) {
+    echo "<h3>",_("check for necessary pgsrc updates"),"</h3>\n";
     $dbi = $request->getDbh(); 
     $path = FindLocalizedFile(WIKI_PGSRC);
     $pgsrc = new fileSet($path);
@@ -56,7 +57,7 @@ function CheckPgsrcUpdate(&$request) {
         $pagename = urldecode($filename);
         $page = $dbi->getPage($pagename);
         if ($page->exists()) {
-            // check mtime
+            // check mtime: update automatically if pgsrc is newer
             $rev = $page->getCurrentRevision();
             $page_mtime = $rev->get('mtime');
             $data  = implode("", file($path."/".$filename));
@@ -92,16 +93,20 @@ function CheckPgsrcUpdate(&$request) {
 }
 
 /**
- * search table definition in appropriate schema and 
- * create it.
- * supported: mysql
+ * Search table definition in appropriate schema
+ * and create it.
+ * Supported: mysql
  */
-function installTable(&$dbh, $table) {
-    if (!in_array($DBParam['dbtype'],array('SQL','ADODB'))) return;
-    $backend = 'mysql'; // fixme
-    $schema = findFile("schemas/$backend.sql");
-    if (!$schema)
+function installTable(&$dbh, $table, $backend_type) {
+    global $DBParams;
+    if (!in_array($DBParams['dbtype'],array('SQL','ADODB'))) return;
+    echo _("install table")," ",$table," ... <br />\n";
+    $backend = &$dbh->_backend->_dbh;
+    $schema = findFile("schemas/${backend_type}.sql");
+    if (!$schema) {
+        echo "  ",_("FAILED"),": ",sprintf(_("no schema %s found"),"schemas/${backend_type}.sql")," ... <br />\n";
         return false;
+    }
     switch ($table) {
     case 'session': 
         break;
@@ -109,26 +114,90 @@ function installTable(&$dbh, $table) {
         break;
     case 'pref':
         break;
-    case 'members':
+    case 'member':
         break;
     }
+    echo "  ",_("FAILED"),": ",_("not yet implemented")," ... <br />\n";
 }
-/** 
+
+/**
  * currently update only session, user, pref and member
  * jeffs-hacks database api (around 1.3.2) later
  *   people should export/import their pages if using that old versions.
  */
 function CheckDatabaseUpdate($request) {
-    global $DBParam, $DBAuthParams;
-    if (!in_array($DBParam['dbtype'],array('SQL','ADODB'))) return;
+    global $DBParams, $DBAuthParams;
+    if (!in_array($DBParams['dbtype'],array('SQL','ADODB'))) return;
+    echo "<h3>",_("check for necessary database updates"),"</h3>\n";
     $dbh = &$request->_dbi;
     $backend = &$dbh->_backend->_dbh;
-    if ($DBParam['dbtype'] == 'SQL') {
+    if ($DBParams['dbtype'] == 'SQL') {
         $tables = $backend->getListOf('tables');
-    } elseif ($DBParam['dbtype'] == 'ADODB') {
+        $backend_type = $backend->phptype;
+    } elseif ($DBParams['dbtype'] == 'ADODB') {
         $tables = $backend->MetaTables();
+        $backend_type = $backend->databaseType;
     }
-    //...
+    $prefix = isset($DBParams['prefix']) ? $DBParams['prefix'] : '';
+    extract($dbh->_backend->_table_names);
+    foreach (explode(':','session:user:pref:member') as $table) {
+    	if (!in_array($table,$tables)) {
+    	    if ($prefix and !in_array($prefix.$table,$tables)) {
+    	        installTable(&$dbh, $prefix.$table, $backend_type);
+    	    } else {
+    	    	installTable(&$dbh, $table, $backend_type);
+    	    }
+    	}
+    }
+    // 1.3.8 added session.sess_ip
+    if (phpwiki_version() >= 1030.08 and USE_DB_SESSION and isset($request->_dbsession)) {
+  	echo _("check for new session.sess_ip column")," ...";
+  	$database = $DBParams['dbtype'] == 'ADODB' ? $backend->database : $backend->dsn['database'];
+        $session_tbl = $prefix . $DBParams['db_session_table'];
+  	$fields = mysql_list_fields($database,$session_tbl);
+  	$columns = mysql_num_fields($fields);
+        for ($i = 0; $i < $columns; $i++) {
+            $sess_fields[] = mysql_field_name($fields, $i);
+        }
+        mysql_free_result($fields);
+        if (!in_array("sess_ip",$sess_fields)) {
+            echo "<b>",_("ADDING"),"</b>"," ... ";		
+            mysql_query("ALTER TABLE $session_tbl ADD CHAR(15) NOT NULL")
+                or trigger_error("<br />\nSQL Error: ".mysql_error(),E_USER_WARNING);
+        } else {
+            echo _("OK");
+        }
+        echo "<br />\n";
+    }
+    // 1.3.10 mysql requires page.id auto_increment
+    // mysql, mysqli or mysqlt
+    if (phpwiki_version() >= 1030.10 and substr($backend_type,0,5) == 'mysql') {
+  	echo _("check for page.id auto_increment flag")," ...";
+  	$database = $DBParams['dbtype'] == 'ADODB' ? $backend->database : $backend->dsn['database'];
+  	$fields = mysql_list_fields($database,$page_tbl);
+  	$columns = mysql_num_fields($fields); 
+        for ($i = 0; $i < $columns; $i++) {
+            if (mysql_field_name($fields, $i) == 'id') {
+            	$flags = mysql_field_flags($fields, $i);
+            	if (!strstr($flags,"auto_increment")) {
+                    echo "<b>",_("ADDING"),"</b>"," ... ";		
+                    // MODIFY col_def valid since mysql 3.22.16,
+                    // older mysql's need CHANGE old_col col_def
+                    mysql_query("ALTER TABLE $page_tbl CHANGE id id INT NOT NULL AUTO_INCREMENT")
+                      or trigger_error("<br />\nSQL Error: ".mysql_error(),E_USER_WARNING);
+                    $fields = mysql_list_fields($database,$page_tbl);
+                    if (!strstr(mysql_field_flags($fields, $i),"auto_increment"))
+                        echo " <b>",_("FAILED"),"</b><br />\n";		
+                    else     
+                        echo _("OK"),"<br />\n";            		
+            	} else {
+                    echo _("OK"),"<br />\n";            		
+            	}
+            	break;
+            }
+        }
+        mysql_free_result($fields);
+    }
     return;
 }
 
@@ -163,12 +232,21 @@ function DoUpgrade($request) {
     StartLoadDump($request, _("Upgrading this PhpWiki"));
     CheckDatabaseUpdate($request);
     CheckPgsrcUpdate($request);
+    //CheckThemeUpdate($request);
     EndLoadDump($request);
 }
 
 
 /**
  $Log: not supported by cvs2svn $
+ Revision 1.4  2004/05/02 21:26:38  rurban
+ limit user session data (HomePageHandle and auth_dbi have to invalidated anyway)
+   because they will not survive db sessions, if too large.
+ extended action=upgrade
+ some WikiTranslation button work
+ revert WIKIAUTH_UNOBTAINABLE (need it for main.php)
+ some temp. session debug statements
+
  Revision 1.3  2004/04/29 22:33:30  rurban
  fixed sf.net bug #943366 (Kai Krakow)
    couldn't load localized url-undecoded pagenames
