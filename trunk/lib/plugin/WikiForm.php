d43a93b0b51df94c835b2103d0fe55c00f12fe9d
rurban <rurban@96ab9672-09ca-45d6-a79d-3d69d39ca109>
2004-06-21 17:06:38 +0000

    renamed constant

--- trunk/lib/plugin/WikiForm.php
+++ trunk/lib/plugin/WikiForm.php
@@ -1,5 +1,5 @@
 <?php // -*-php-*-
-rcs_id('$Id: WikiForm.php,v 1.13 2004-06-21 16:22:32 rurban Exp $');
+rcs_id('$Id: WikiForm.php,v 1.14 2004-06-21 17:06:38 rurban Exp $');
 /**
  Copyright 1999, 2000, 2001, 2002 $ThePhpWikiProgrammingTeam
 
@@ -34,7 +34,7 @@ extends WikiPlugin
 
     function getVersion() {
         return preg_replace("/[Revision: $]/", '',
-                            "\$Revision: 1.13 $");
+                            "\$Revision: 1.14 $");
     }
 
     function getDefaultArguments() {
@@ -76,7 +76,7 @@ extends WikiPlugin
         case 'dumpserial':
             $input['name'] = 'directory';
             if (!$default)
-                $input['value'] = DUMP_DIR;
+                $input['value'] = DEFAULT_DUMP_DIR;
             if (!$buttontext)
                 $buttontext = _("Dump Pages");
             $class = 'wikiadmin';
@@ -120,6 +120,16 @@ extends WikiPlugin
 };
 
 // $Log: not supported by cvs2svn $
+// Revision 1.13  2004/06/21 16:22:32  rurban
+// add DEFAULT_DUMP_DIR and HTML_DUMP_DIR constants, for easier cmdline dumps,
+// fixed dumping buttons locally (images/buttons/),
+// support pages arg for dumphtml,
+// optional directory arg for dumpserial + dumphtml,
+// fix a AllPages warning,
+// show dump warnings/errors on DEBUG,
+// don't warn just ignore on wikilens pagelist columns, if not loaded.
+// RateIt pagelist column is called "rating", not "ratingwidget" (Dan?)
+//
 // Revision 1.12  2004/04/18 01:11:52  rurban
 // more numeric pagename fixes.
 // fixed action=upload with merge conflict warnings.
