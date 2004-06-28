00db06f5add9754590e6d261c209c7c5b4785ff4
rurban <rurban@96ab9672-09ca-45d6-a79d-3d69d39ca109>
2004-06-28 14:17:38 +0000

    updated DSN parser from Pear. esp. for sqlite

--- trunk/lib/WikiDB/backend/ADODB.php
+++ trunk/lib/WikiDB/backend/ADODB.php
@@ -1,5 +1,5 @@
 <?php // -*-php-*-
-rcs_id('$Id: ADODB.php,v 1.33 2004-06-27 10:26:02 rurban Exp $');
+rcs_id('$Id: ADODB.php,v 1.34 2004-06-28 14:17:38 rurban Exp $');
 
 /*
  Copyright 2002,2004 $ThePhpWikiProgrammingTeam
@@ -968,31 +968,23 @@ extends WikiDB_backend_ADODB_generic_iter
     }
 }
 
-// Following function taken from adodb-pear.inc.php.  
+// Following function taken from Pear::DB (prev. from adodb-pear.inc.php).
 // Eventually, change index.php to provide the relevant information
 // directly?
     /**
-     * Parse a data source name
+     * Parse a data source name.
      *
-     * @param $dsn string Data Source Name to be parsed
-     *
-     * @return array an associative array with the following keys:
-     *
-     *  phptype: Database backend used in PHP (mysql, odbc etc.)
-     *  dbsyntax: Database used with regards to SQL syntax etc.
-     *  protocol: Communication protocol to use (tcp, unix etc.)
-     *  hostspec: Host specification (hostname[:port])
-     *  database: Database to use on the DBMS server
-     *  username: User name for login
-     *  password: Password for login
+     * Additional keys can be added by appending a URI query string to the
+     * end of the DSN.
      *
      * The format of the supplied DSN is in its fullest form:
-     *
-     *  phptype(dbsyntax)://username:password@protocol+hostspec/database
+     * <code>
+     *  phptype(dbsyntax)://username:password@protocol+hostspec/database?option=8&another=true
+     * </code>
      *
      * Most variations are allowed:
-     *
-     *  phptype://username:password@protocol+hostspec:110//usr/db_file.db
+     * <code>
+     *  phptype://username:password@protocol+hostspec:110//usr/db_file.db?mode=0644
      *  phptype://username:password@hostspec/database_name
      *  phptype://username:password@hostspec
      *  phptype://username@hostspec
@@ -1000,23 +992,42 @@ extends WikiDB_backend_ADODB_generic_iter
      *  phptype://hostspec
      *  phptype(dbsyntax)
      *  phptype
+     * </code>
+     *
+     * @param string $dsn Data Source Name to be parsed
+     *
+     * @return array an associative array with the following keys:
+     *  + phptype:  Database backend used in PHP (mysql, odbc etc.)
+     *  + dbsyntax: Database used with regards to SQL syntax etc.
+     *  + protocol: Communication protocol to use (tcp, unix etc.)
+     *  + hostspec: Host specification (hostname[:port])
+     *  + database: Database to use on the DBMS server
+     *  + username: User name for login
+     *  + password: Password for login
      *
      * @author Tomas V.V.Cox <cox@idecnet.com>
      */
-    function parseDSN($dsn) {
-        if (is_array($dsn)) {
-            return $dsn;
-        }
-
+    function parseDSN($dsn)
+    {
         $parsed = array(
             'phptype'  => false,
             'dbsyntax' => false,
+            'username' => false,
+            'password' => false,
             'protocol' => false,
             'hostspec' => false,
+            'port'     => false,
+            'socket'   => false,
             'database' => false,
-            'username' => false,
-            'password' => false
         );
+
+        if (is_array($dsn)) {
+            $dsn = array_merge($parsed, $dsn);
+            if (!$dsn['dbsyntax']) {
+                $dsn['dbsyntax'] = $dsn['phptype'];
+            }
+            return $dsn;
+        }
 
         // Find phptype and dbsyntax
         if (($pos = strpos($dsn, '://')) !== false) {
@@ -1024,65 +1035,102 @@ extends WikiDB_backend_ADODB_generic_iter
             $dsn = substr($dsn, $pos + 3);
         } else {
             $str = $dsn;
-            $dsn = NULL;
+            $dsn = null;
         }
 
         // Get phptype and dbsyntax
         // $str => phptype(dbsyntax)
         if (preg_match('|^(.+?)\((.*?)\)$|', $str, $arr)) {
-            $parsed['phptype'] = $arr[1];
-            $parsed['dbsyntax'] = (empty($arr[2])) ? $arr[1] : $arr[2];
+            $parsed['phptype']  = $arr[1];
+            $parsed['dbsyntax'] = !$arr[2] ? $arr[1] : $arr[2];
         } else {
-            $parsed['phptype'] = $str;
+            $parsed['phptype']  = $str;
             $parsed['dbsyntax'] = $str;
         }
 
-        if (empty($dsn)) {
+        if (!count($dsn)) {
             return $parsed;
         }
 
         // Get (if found): username and password
         // $dsn => username:password@protocol+hostspec/database
-        if (($at = strpos($dsn,'@')) !== false) {
+        if (($at = strrpos($dsn,'@')) !== false) {
             $str = substr($dsn, 0, $at);
             $dsn = substr($dsn, $at + 1);
             if (($pos = strpos($str, ':')) !== false) {
-                $parsed['username'] = urldecode(substr($str, 0, $pos));
-                $parsed['password'] = urldecode(substr($str, $pos + 1));
+                $parsed['username'] = rawurldecode(substr($str, 0, $pos));
+                $parsed['password'] = rawurldecode(substr($str, $pos + 1));
             } else {
-                $parsed['username'] = urldecode($str);
+                $parsed['username'] = rawurldecode($str);
             }
         }
 
         // Find protocol and hostspec
-        // $dsn => protocol+hostspec/database
-        if (($pos = strpos($dsn, '/')) !== false) {
-            $str = substr($dsn, 0, $pos);
-            $dsn = substr($dsn, $pos + 1);
+
+        // $dsn => proto(proto_opts)/database
+        if (preg_match('|^([^(]+)\((.*?)\)/?(.*?)$|', $dsn, $match)) {
+            $proto       = $match[1];
+            $proto_opts  = $match[2] ? $match[2] : false;
+            $dsn         = $match[3];
+
+        // $dsn => protocol+hostspec/database (old format)
         } else {
-            $str = $dsn;
-            $dsn = NULL;
+            if (strpos($dsn, '+') !== false) {
+                list($proto, $dsn) = explode('+', $dsn, 2);
+            }
+            if (strpos($dsn, '/') !== false) {
+                list($proto_opts, $dsn) = explode('/', $dsn, 2);
+            } else {
+                $proto_opts = $dsn;
+                $dsn = null;
+            }
         }
 
-        // Get protocol + hostspec
-        // $str => protocol+hostspec
-        if (($pos = strpos($str, '+')) !== false) {
-            $parsed['protocol'] = substr($str, 0, $pos);
-            $parsed['hostspec'] = urldecode(substr($str, $pos + 1));
-        } else {
-            $parsed['hostspec'] = urldecode($str);
+        // process the different protocol options
+        $parsed['protocol'] = (!empty($proto)) ? $proto : 'tcp';
+        $proto_opts = rawurldecode($proto_opts);
+        if ($parsed['protocol'] == 'tcp') {
+            if (strpos($proto_opts, ':') !== false) {
+                list($parsed['hostspec'], $parsed['port']) = explode(':', $proto_opts);
+            } else {
+                $parsed['hostspec'] = $proto_opts;
+            }
+        } elseif ($parsed['protocol'] == 'unix') {
+            $parsed['socket'] = $proto_opts;
         }
 
         // Get dabase if any
         // $dsn => database
-        if (!empty($dsn)) {
-            $parsed['database'] = $dsn;
+        if ($dsn) {
+            // /database
+            if (($pos = strpos($dsn, '?')) === false) {
+                $parsed['database'] = $dsn;
+            // /database?param1=value1&param2=value2
+            } else {
+                $parsed['database'] = substr($dsn, 0, $pos);
+                $dsn = substr($dsn, $pos + 1);
+                if (strpos($dsn, '&') !== false) {
+                    $opts = explode('&', $dsn);
+                } else { // database?param1=value1
+                    $opts = array($dsn);
+                }
+                foreach ($opts as $opt) {
+                    list($key, $value) = explode('=', $opt);
+                    if (!isset($parsed[$key])) {
+                        // don't allow params overwrite
+                        $parsed[$key] = rawurldecode($value);
+                    }
+                }
+            }
         }
 
         return $parsed;
     }
 
 // $Log: not supported by cvs2svn $
+// Revision 1.33  2004/06/27 10:26:02  rurban
+// oci8 patch by Philippe Vanhaesendonck + some ADODB notes+fixes
+//
 // Revision 1.32  2004/06/25 14:15:08  rurban
 // reduce memory footprint by caching only requested pagedate content (improving most page iterators)
 //
