99595edf9cbb82c4003794bc6ef138687d46a29d
rurban <rurban@96ab9672-09ca-45d6-a79d-3d69d39ca109>
2004-07-08 15:22:17 +0000

    added testImportOldMarkup

--- trunk/tests/unit/lib/SetupWiki.php
+++ trunk/tests/unit/lib/SetupWiki.php
@@ -7,20 +7,35 @@ require_once 'lib/loadsave.php';
 require_once 'PHPUnit.php';
 
 class SetupWiki extends PHPUnit_TestCase {
-    // constructor of the test suite
     function SetupWiki($name) {
        $this->PHPUnit_TestCase($name);
+    }
+
+    function testImportOldMarkup() {
+        global $request;
+        $dbi = $request->getDbh();
+        $pagename = 'OldMarkupTestPage';
+        $dbi->deletePage($pagename);
+        $this->assertFalse($dbi->isWikiPage($pagename));
+
+        $request->setArg('source', FindFile('pgsrc/'.$pagename));
+        $request->setArg('overwrite', 1);
+        LoadAny($request, $request->getArg('source'));
+        $this->assertTrue($dbi->isWikiPage($pagename));
     }
 
     function testSetupWiki() {
         global $request;
 
+        $dbi = $request->getDbh();
+        $dbi->deletePage('HomePage');
+        $this->assertFalse($dbi->isWikiPage('HomePage'));
+
         $request->setArg('source', FindFile('pgsrc'));
         $request->setArg('overwrite', 1);
         LoadAny($request, $request->getArg('source'));
         
-        $dbh = $request->getDbh();
-        $this->assertTrue($dbh->isWikiPage('HomePage'));
+        $this->assertTrue($dbi->isWikiPage('HomePage'));
     }
 }
 
