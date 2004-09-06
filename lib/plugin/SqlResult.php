66de0757a635472ad7e28b153a63506da7ff9681
rurban <rurban@96ab9672-09ca-45d6-a79d-3d69d39ca109>
2004-09-06 08:36:28 +0000

    support templates, with some vars

--- lib/plugin/SqlResult.php
+++ lib/plugin/SqlResult.php
@@ -1,5 +1,5 @@
 <?php // -*-php-*-
-rcs_id('$Id: SqlResult.php,v 1.2 2004-05-03 21:57:47 rurban Exp $');
+rcs_id('$Id: SqlResult.php,v 1.3 2004-09-06 08:36:28 rurban Exp $');
 /*
  Copyright 2004 $ThePhpWikiProgrammingTeam
  
@@ -19,15 +19,17 @@ rcs_id('$Id: SqlResult.php,v 1.2 2004-05-03 21:57:47 rurban Exp $');
  along with PhpWiki; if not, write to the Free Software
  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
+
 /**
- * This plugin displays result sets of arbitrary SQL select statements 
+ * This plugin displays results of arbitrary SQL select statements 
  * in table form.
- * The database definition, the dsn, must be defined in the local file 
+ * The database definition, the DSN, must be defined in the local file 
  * lib/plugin/SqlResult.ini
  *   A simple textfile with alias = dsn lines.
  *
- * TODO: Optional template file to format the result and handle some logic.
- *       Pass args to the SQL line (for paging)
+ * Optional template file to format the result and handle some logic.
+ * Template vars: %%where%%, %%sortby%%, %%limit%%
+ * TODO: paging
  *
  * Usage:
  *   <?plugin SqlResult alias=mysql
@@ -40,6 +42,16 @@ rcs_id('$Id: SqlResult.php,v 1.2 2004-05-03 21:57:47 rurban Exp $');
  *                   ORDER BY rating DESC 
  *                   LIMIT 5
  *   ?>
+  <?plugin SqlResult alias=imdb template=imdbmovies where||="Davies, Jeremy%"
+SELECT m.title, m.date, n.name, c.role
+  FROM movies as m, names as n, jobs as j, characters as c
+  WHERE n.name LIKE "%%where%%"
+  AND m.title_id = c.title_id
+  AND n.name_id = c.name_id
+  AND c.job_id = j.job_id
+  AND j.description = 'Actor'
+  ORDER BY m.date DESC
+?>
  *
  * @author: ReiniUrban
  */
@@ -59,13 +71,17 @@ extends WikiPlugin
 
     function getVersion() {
         return preg_replace("/[Revision: $]/", '',
-                            "\$Revision: 1.2 $");
+                            "\$Revision: 1.3 $");
     }
 
     function getDefaultArguments() {
-        return array('page'        => '[pagename]',
-                     'alias'       => false,
-                     'template'    => false,
+        return array(
+                     'alias'       => false, // DSN database specification
+                     'ordered'     => false, // if to display as <ol> list: single col only without template
+                     'template'    => false, // use a custom <theme>/template.tmpl
+                     'where'       => false, // custom filter for the query
+                     'sortby'      => false, // for paging, default none
+                     'limit'       => false, // for paging, default: only the first 50 
                     );
     }
 
@@ -88,23 +104,80 @@ extends WikiPlugin
         if (!$alias)
             return $this->error(_("No DSN alias for SqlResult.ini specified"));
 	$sql = $this->_sql;
-        //TODO: handle variables
+
+        // apply custom filters
+        if ($where and strstr($sql,"%%where%%"))
+            $sql = str_replace("%%where%%", $where, $sql);
+        if (strstr($sql,"%%limit%%")) // default: only the first 50 
+            $sql = str_replace("%%limit%%", $limit ? $limit : "0,50", $sql);
+        else {
+            if (strstr($sql,"LIMIT")) // default: only the first 50 
+                $sql = str_replace("%%limit%%", $limit ? $limit : "0,50", $sql);
+            else
+                $sql .= " LIMIT 0,50";
+        }
+        if (strstr($sql,"%%sortby%%")) {
+            if (!$sortby)
+                $sql = preg_replace("/ORDER BY .*%%sortby%%\s/m", "", $sql);
+            else
+                $sql = str_replace("%%sortby%%", $sortby, $sql);
+        }
 
         $inidsn = $this->getDsn($alias);
         if (!$inidsn)
             return $this->error(sprintf(_("No DSN for alias %s in SqlResult.ini found"),
                                         $alias));
-        $db = DB::connect($inidsn);
-        $all = $db->getAll($sql);
-
-        $html = HTML::table(array('class'=>'sqlresult'));
-        $i = 0;
-	foreach ($all as $row) {
-            $tr = HTML::tr(array('class'=> $i++ % 2 ? 'evenrow' : 'oddrow'));
-            foreach ($row as $col) {
-                $tr->pushContent(HTML::td($col));
+        // adodb or pear? adodb as default, since we distribute per default it. 
+        // for pear there may be overrides.
+        if ($DBParams['dbtype'] == 'SQL') {
+            $dbh = DB::connect($inidsn);
+            $all = $dbh->getAll($sql);
+        } else {
+            if ($DBParams['dbtype'] != 'ADODB') {
+                // require_once('lib/WikiDB/adodb/adodb-errorhandler.inc.php');
+                require_once('lib/WikiDB/adodb/adodb.inc.php');
             }
-            $html->pushContent($tr);
+            $parsed = parseDSN($inidsn);
+            $dbh = &ADONewConnection($parsed['phptype']); 
+            $conn = $dbh->Connect($parsed['hostspec'],$parsed['username'], 
+                                  $parsed['password'], $parsed['database']); 
+            $GLOBALS['ADODB_FETCH_MODE'] = ADODB_FETCH_ASSOC;
+            $dbh->SetFetchMode(ADODB_FETCH_ASSOC);
+
+            $all = $dbh->getAll($sql);
+
+            $GLOBALS['ADODB_FETCH_MODE'] = ADODB_FETCH_NUM;
+            $dbh->SetFetchMode(ADODB_FETCH_NUM);
+        }
+
+        // if ($limit) ; // TODO: fill paging vars (see PageList)
+
+        if ($template) {
+            $args = array('SqlResult' => $all,   // the resulting array of rows
+                          'ordered' => $ordered, // whether to display as <ul>/<dt> or <ol> 
+                          'where'   => $where,
+                          'sortby'  => $sortby,  // paging params (could also be taken from request...)
+                          'limit'   => $limit);
+            return Template($template, $args);
+        } else {
+            // if ($limit) ; // do paging via pagelink template
+            if ($ordered) {
+                $html = HTML::ol(array('class'=>'sqlresult'));
+                foreach ($all as $row) {
+                    $html->pushContent(HTML::li(array('class'=> $i++ % 2 ? 'evenrow' : 'oddrow'), $row[0]));
+                }
+            } else {
+                $html = HTML::table(array('class'=>'sqlresult'));
+                $i = 0;
+                foreach ($all as $row) {
+                    $tr = HTML::tr(array('class'=> $i++ % 2 ? 'evenrow' : 'oddrow'));
+                    foreach ($row as $col) {
+                        $tr->pushContent(HTML::td($col));
+                    }
+                    $html->pushContent($tr);
+                }
+            }
+            // if ($limit) ; // do paging via pagelink template
         }
         return $html;
     }
@@ -112,6 +185,12 @@ extends WikiPlugin
 };
 
 // $Log: not supported by cvs2svn $
+// Revision 1.2  2004/05/03 21:57:47  rurban
+// locale updates: we previously lost some words because of wrong strings in
+//   PhotoAlbum, german rewording.
+// fixed $_SESSION registering (lost session vars, esp. prefs)
+// fixed ending slash in listAvailableLanguages/Themes
+//
 // Revision 1.1  2004/05/03 20:44:58  rurban
 // fixed gettext strings
 // new SqlResult plugin
