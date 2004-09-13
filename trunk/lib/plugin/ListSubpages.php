7c2554b2d7e149a386fccb64095cd76b2317e599
rurban <rurban@96ab9672-09ca-45d6-a79d-3d69d39ca109>
2004-09-13 14:59:56 +0000

    info=count: number of backlinks for this subpage

--- trunk/lib/plugin/ListSubpages.php
+++ trunk/lib/plugin/ListSubpages.php
@@ -1,5 +1,5 @@
 <?php // -*-php-*-
-rcs_id('$Id: ListSubpages.php,v 1.4 2004-08-18 11:15:11 rurban Exp $');
+rcs_id('$Id: ListSubpages.php,v 1.5 2004-09-13 14:59:56 rurban Exp $');
 /*
  Copyright 2002 $ThePhpWikiProgrammingTeam
 
@@ -40,7 +40,7 @@ extends WikiPlugin
 
     function getVersion() {
         return preg_replace("/[Revision: $]/", '',
-                            "\$Revision: 1.4 $");
+                            "\$Revision: 1.5 $");
     }
 
     function getDefaultArguments() {
@@ -54,7 +54,7 @@ extends WikiPlugin
                      );
     }
     // info arg allows multiple columns
-    // info=mtime,hits,summary,version,author,locked,minor
+    // info=mtime,hits,summary,version,author,locked,minor,count
     // exclude arg allows multiple pagenames exclude=HomePage,RecentChanges
 
     function run($dbi, $argstr, &$request, $basepage) {
@@ -81,7 +81,12 @@ extends WikiPlugin
 
         $descrip = fmt("SubPages of %s:",
                        WikiLink($pagename, 'auto'));
-        $pagelist = new PageList($info, $exclude);
+        if ($info) {
+            $info = explode(",", $info);
+            if (in_array('count',$info))
+                $args['types']['count'] = new _PageList_Column_ListSubpages_count('count', _("#"), 'center');
+        }
+        $pagelist = new PageList($info, $exclude, $args);
         if (!$noheader)
             $pagelist->setCaption($descrip);
 
@@ -106,7 +111,19 @@ extends WikiPlugin
     }
 };
 
+// how many backlinks for this subpage
+class _PageList_Column_ListSubpages_count extends _PageList_Column {
+    function _getValue($page, &$revision_handle) {
+        $iter = $page->getBackLinks();
+        $count = $iter->count();
+        return $count;
+    }
+}
+
 // $Log: not supported by cvs2svn $
+// Revision 1.4  2004/08/18 11:15:11  rurban
+// added basepage argument. Default current
+//
 // Revision 1.3  2004/02/17 12:11:36  rurban
 // added missing 4th basepage arg at plugin->run() to almost all plugins. This caused no harm so far, because it was silently dropped on normal usage. However on plugin internal ->run invocations it failed. (InterWikiSearch, IncludeSiteMap, ...)
 //
