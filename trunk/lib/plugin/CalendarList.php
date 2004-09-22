4f01055d8ce9f89ffbe346a152daacf209657f9d
rurban <rurban@96ab9672-09ca-45d6-a79d-3d69d39ca109>
2004-09-22 13:36:45 +0000

    Support ranges, based on a simple patch by JoshWand   next_n_days, last_n_days, next_n   last_n not yet

--- trunk/lib/plugin/CalendarList.php
+++ trunk/lib/plugin/CalendarList.php
@@ -1,5 +1,5 @@
 <?php // -*-php-*-
-rcs_id('$Id: CalendarList.php,v 1.2 2004-02-17 12:11:36 rurban Exp $');
+rcs_id('$Id: CalendarList.php,v 1.3 2004-09-22 13:36:45 rurban Exp $');
 
 if (!defined('SECONDS_PER_DAY'))
 define('SECONDS_PER_DAY', 24 * 3600);
@@ -32,6 +32,12 @@ extends WikiPlugin
                      'year'         => '',
                      'month'        => '',
                      'month_offset' => 0,
+                     //support ranges, based on a simple patch by JoshWand
+                     'next_n_days'  => '',
+                     'last_n_days'  => '',
+                     // next or last n entries:
+                     'next_n'  => '',
+                     //'last_n'  => '', // not yet
 
                      'month_format' => '%B, %Y',
                      'wday_format'  => '%a',
@@ -78,12 +84,22 @@ extends WikiPlugin
             if (!($args[$param] = intval($args[$param])))
                 $args[$param]   = $dflt;
         }
-
-        $time = mktime(12, 0, 0,                               // hh, mm, ss,
-                       $args['month'] + $args['month_offset'], // month (1-12)
-                       1,                                      // mday (1-31)
-                       $args['year']);
-
+        if ($args['last_n_days']) {
+            $time = mktime(0, 0, 0,                            // hh, mm, ss,
+                           $args['month'] + $args['month_offset'], // month (1-12)
+                           $now['tm_mday'] - $args['last_n_days'],
+                           $args['year']);
+        } elseif ($args['next_n_days'] || $args['next_n']/* || $args['last_n']*/) {
+            $time = mktime(0, 0, 0,                            // hh, mm, ss,
+                           $args['month'] + $args['month_offset'], // month (1-12)
+                           $now['tm_mday'] ,                   // starting today
+                           $args['year']);
+        } else {
+            $time = mktime(12, 0, 0,                           // hh, mm, ss,
+                           $args['month'] + $args['month_offset'], // month (1-12)
+                           1,                                   // starting at monday
+                           $args['year']);
+        } 
         $t = localtime($time, 1);
 
         if ($now['tm_year'] == $t['tm_year'] && $now['tm_mon'] == $t['tm_mon'])
@@ -94,21 +110,35 @@ extends WikiPlugin
         $cal = HTML::dl();
 
         $done = false;
-
+        $n = 0; $max = (180 * SECONDS_PER_DAY) + $time;
         while (!$done) {
-            $cal->pushContent($this->__date($dbi, $time));
-
+            $success = $cal->pushContent($this->__date($dbi, $time));
             $time += SECONDS_PER_DAY;
-            $t     = localtime($time, 1);
-            $done  = $t['tm_mday'] == 1;
-        }
+            if ($time >= $max) return $cal;
 
+            $t     = localtime($time, 1);
+            if ($args['next_n_days']) {
+                if ($n == $args['next_n_days']) return $cal;
+                $n++;
+            } elseif ($args['next_n']) {
+                if ($n == $args['next_n']) return $cal;
+                if (!empty($success))
+                    $n++;
+            } elseif ($args['last_n_days']) {
+                $done = ($t['tm_mday'] == $now['tm_mday']);
+            } else {
+                $done = ($t['tm_mday'] == 1);
+            }
+        }
         return $cal;
     }
 };
 
 
 // $Log: not supported by cvs2svn $
+// Revision 1.2  2004/02/17 12:11:36  rurban
+// added missing 4th basepage arg at plugin->run() to almost all plugins. This caused no harm so far, because it was silently dropped on normal usage. However on plugin internal ->run invocations it failed. (InterWikiSearch, IncludeSiteMap, ...)
+//
 // Revision 1.1  2003/11/18 19:06:03  carstenklapp
 // New plugin to be used in conjunction with the Calendar plugin.
 // Upgraded to use SUBPAGE_SEPARATOR for subpages. SF patch tracker
