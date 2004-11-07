ecc539c89e540359e745a854d1320d1b15c11a4c
rurban <rurban@96ab9672-09ca-45d6-a79d-3d69d39ca109>
2004-11-07 16:04:54 +0000

    ACCESS_LOG_SQL schema

--- trunk/schemas/mysql-initialize.sql
+++ trunk/schemas/mysql-initialize.sql
@@ -1,4 +1,4 @@
--- $Id: mysql-initialize.sql,v 1.2 2004-09-06 08:31:27 rurban Exp $
+-- $Id: mysql-initialize.sql,v 1.3 2004-11-07 16:04:54 rurban Exp $
 
 CREATE TABLE page (
 	id              INT NOT NULL AUTO_INCREMENT,
@@ -92,3 +92,24 @@ CREATE TABLE rating (
         tstamp TIMESTAMP(14) NOT NULL,
         PRIMARY KEY (dimension, raterpage, rateepage)
 );
+
+-- only if you need fast log-analysis (spam prevention, recent referrers)
+-- see http://www.outoforder.cc/projects/apache/mod_log_sql/docs-2.0/#id2756178
+CREATE TABLE accesslog (
+        time_stamp    int unsigned,
+	remote_host   varchar(50),
+	remote_user   varchar(50),
+        request_method varchar(10),
+	request_line  varchar(255),
+	request_args  varchar(255),
+	request_file  varchar(255),
+	request_uri   varchar(255),
+	request_time  char(28),
+	status 	      smallint unsigned,
+	bytes_sent    smallint unsigned,
+        referer       varchar(255), 
+	agent         varchar(255),
+	request_duration float
+);
+CREATE INDEX log_time ON accesslog (time_stamp);
+CREATE INDEX log_host ON accesslog (remote_host);
