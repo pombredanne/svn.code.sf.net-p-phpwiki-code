5c5c63c2d7c961e1108f7807c1e8f907d142b59a
rurban <rurban@96ab9672-09ca-45d6-a79d-3d69d39ca109>
2004-12-15 17:47:32 +0000

    fix RecentChanges links fix footer layout

--- themes/blog/themeinfo.php
+++ themes/blog/themeinfo.php
@@ -1,5 +1,5 @@
 <?php
-rcs_id('$Id: themeinfo.php,v 1.1 2004-12-14 21:30:36 rurban Exp $');
+rcs_id('$Id: themeinfo.php,v 1.2 2004-12-15 17:47:32 rurban Exp $');
 
 /**
  * This file defines a blog theme for PhpWiki, 
@@ -51,6 +51,93 @@ class Theme_blog extends Theme_Sidebar {
             default:
                 return gettext(ucfirst($action));
         }
+    }
+
+    function getRecentChangesFormatter ($format) {
+        include_once($this->file('lib/RecentChanges.php'));
+        if (preg_match('/^rss|^sidebar/', $format))
+            return false;       // use default
+        if ($format == 'box')
+            return '_blog_RecentChanges_BoxFormatter';
+        return '_blog_RecentChanges_Formatter';
+    }
+/*
+    function getPageHistoryFormatter ($format) {
+        include_once($this->file('lib/RecentChanges.php'));
+        if (preg_match('/^rss|^sidebar/', $format))
+            return false;       // use default
+        return '_blog_PageHistory_Formatter';
+    }
+
+    function _get_name_and_rev ($page_or_rev) {
+        $version = false;
+
+        if (empty($page_or_rev)) {
+            global $request;
+            $pagename = $request->getArg("pagename");
+            $version = $request->getArg("version");
+        }
+        elseif (is_object($page_or_rev)) {
+            if (isa($page_or_rev, 'WikiDB_PageRevision')) {
+                $rev = $page_or_rev;
+                $page = $rev->getPage();
+                if (!$rev->isCurrent()) $version = $rev->getVersion();
+            }
+            else {
+                $page = $page_or_rev;
+            }
+            $pagename = $page->getName();
+        }
+        else {
+            $pagename = (string) $page_or_rev;
+        }
+        return compact('pagename', 'version');
+    }
+*/
+
+/*
+    function maybeSplitWikiWord($pagename) {
+    	if (strstr($pagename, "/Blog/")) {
+    	    return "/Blog/...";
+    	} else {
+    	    return parent::maybeSplitWikiWord($pagename);
+    	}
+    }
+*/
+    
+    /* TODO: use the blog summary as label instead of the pagename */
+    function linkExistingWikiWord($wikiword, $linktext = '', $version = false) {
+        global $request;
+        if ($version !== false and !$this->HTML_DUMP_SUFFIX)
+            $url = WikiURL($wikiword, array('version' => $version));
+        else
+            $url = WikiURL($wikiword);
+
+        // Extra steps for dumping page to an html file.
+        if ($this->HTML_DUMP_SUFFIX) {
+            $url = preg_replace('/^\./', '%2e', $url); // dot pages
+        }
+
+        $link = HTML::a(array('href' => $url));
+
+        if (isa($wikiword, 'WikiPageName'))
+             $default_text = $wikiword->shortName;
+         else
+             $default_text = $wikiword;
+         
+        if (!empty($linktext)) {
+            $link->pushContent($linktext);
+            $link->setAttr('class', 'named-wiki');
+            $link->setAttr('title', $this->maybeSplitWikiWord($default_text));
+        }
+        else {
+            //TODO: check if wikiblog
+            $link->pushContent($this->maybeSplitWikiWord($default_text));
+            $link->setAttr('class', 'wiki');
+        }
+        if ($request->getArg('frame'))
+            $link->setAttr('target', '_top');
+        return $link;
     }
 }
 
