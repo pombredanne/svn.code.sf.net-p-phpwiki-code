9c4ecbd1694edfa1d07a673665939e995ba58995
rurban <rurban@96ab9672-09ca-45d6-a79d-3d69d39ca109>
2004-12-17 12:31:57 +0000

    better logout, fake httpauth not yet

--- lib/WikiUser/HttpAuth.php
+++ lib/WikiUser/HttpAuth.php
@@ -1,5 +1,5 @@
 <?php //-*-php-*-
-rcs_id('$Id: HttpAuth.php,v 1.1 2004-11-01 10:43:58 rurban Exp $');
+rcs_id('$Id: HttpAuth.php,v 1.2 2004-12-17 12:31:57 rurban Exp $');
 /* Copyright (C) 2004 $ThePhpWikiProgrammingTeam
  */
 
@@ -30,6 +30,34 @@ extends _PassUser
             return $GLOBALS['ForbiddenUser'];
     }
 
+    // FIXME! This doesn't work yet!
+    // Allow httpauth by other method: Admin for now only
+    function _fake_auth($userid, $passwd) {
+    	return false;
+    	
+        header('WWW-Authenticate: Basic realm="'.WIKI_NAME.'"');
+        header("Authorization: Basic ".base64_encode($userid.":".$passwd));
+        if (!isset($_SERVER))
+            $_SERVER =& $GLOBALS['HTTP_SERVER_VARS'];
+        $GLOBALS['REMOTE_USER'] = $userid;
+        $_SERVER['PHP_AUTH_USER'] = $userid;
+        $_SERVER['PHP_AUTH_PW'] = $passwd;
+        //$GLOBALS['request']->setStatus(200);
+    }
+
+    function _logout() {
+        if (!isset($_SERVER))
+            $_SERVER =& $GLOBALS['HTTP_SERVER_VARS'];
+        header('WWW-Authenticate: Basic realm="'.WIKI_NAME.'"');
+        if (strstr(php_sapi_name(), 'apache'))
+            header('HTTP/1.0 401 Unauthorized'); 
+        else    
+            header("Status: 401 Access Denied"); //IIS and CGI need that
+        unset($GLOBALS['REMOTE_USER']);
+        unset($_SERVER['PHP_AUTH_USER']);
+        unset($_SERVER['PHP_AUTH_PW']);
+    }
+
     function _http_username() {
         if (!isset($_SERVER))
             $_SERVER =& $GLOBALS['HTTP_SERVER_VARS'];
@@ -41,17 +69,24 @@ extends _PassUser
 	    return $GLOBALS['HTTP_ENV_VARS']['REMOTE_USER'];
 	if (!empty($GLOBALS['REMOTE_USER']))
 	    return $GLOBALS['REMOTE_USER'];
+	//IIS:
+	if (!empty($_SERVER['HTTP_AUTHORIZATION'])) {
+            list($userid, $passwd) = explode(':', base64_decode(substr($_SERVER['HTTP_AUTHORIZATION'], 6)));
+            return $userid;
+	}    
 	return '';
     }
     
-    //force http auth authorization
+    // force http auth authorization
     function userExists() {
-        // todo: older php's
         $username = $this->_http_username();
         if (empty($username) or strtolower($username) != strtolower($this->_userid)) {
-            header('WWW-Authenticate: Basic realm="'.WIKI_NAME.'"');
-            header('HTTP/1.0 401 Unauthorized'); 
-            exit;
+            $this->_logout();
+            $user = $GLOBALS['ForbiddenUser'];
+            $user->_userid = $this->_userid =  "";
+            $this->_level = WIKIAUTH_FORBIDDEN;
+            return $user;
+            //exit;
         }
         $this->_userid = $username;
         // we should check if he is a member of admin, 
@@ -61,7 +96,8 @@ extends _PassUser
             $this->_level = WIKIAUTH_ADMIN;
         return $this;
     }
-        
+    
+    // ignore password for now, this is checked by apache.
     function checkPass($submitted_password) {
         return $this->userExists() 
             ? ($this->isAdmin() ? WIKIAUTH_ADMIN : WIKIAUTH_USER)
@@ -71,18 +107,22 @@ extends _PassUser
     function mayChangePass() {
         return false;
     }
-
+/*
     // hmm... either the server dialog or our own.
-    function PrintLoginForm (&$request, $args, $fail_message = false,
-                             $seperate_page = true) {
-        header('WWW-Authenticate: Basic realm="'.WIKI_NAME.'"');
-        header('HTTP/1.0 401 Unauthorized'); 
-        exit;
+    function _disabled_PrintLoginForm (&$request, $args, $fail_message=false, $seperate_page=true) {
+        $this->_logout();
+        return parent::PrintLoginForm ($request, $args, $fail_message, $seperate_page);
     }
-
+*/
 }
 
 // $Log: not supported by cvs2svn $
+// Revision 1.1  2004/11/01 10:43:58  rurban
+// seperate PassUser methods into seperate dir (memory usage)
+// fix WikiUser (old) overlarge data session
+// remove wikidb arg from various page class methods, use global ->_dbi instead
+// ...
+//
 
 // Local Variables:
 // mode: php
