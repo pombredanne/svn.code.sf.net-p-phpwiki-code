a54c342da4bdbe1935cf485a3995e745ede0ce80
nobody <nobody@96ab9672-09ca-45d6-a79d-3d69d39ca109>
2005-01-07 14:00:01 +0000

    This commit was manufactured by cvs2svn to create tag 'release-1_2_6'.

---
+++ admin/wiki_dumpHTML.php
@@ -0,0 +1,7 @@
+<!-- $Id: wiki_dumpHTML.php,v 1.1.2.1.2.2 2005-01-07 13:59:57 rurban Exp $ -->
+<?php
+
+   echo "Dump HTML called.<p>\n";
+   echo "Got: $dumpHTML $directory<br>\n";
+
+?>
--- schemas/sqlite-destroy.sql
+++
@@ -1,5 +0,0 @@
--- http://www.hezmatt.org/~mpalmer/sqlite-phpwiki/sqlite.sql
-
--- $Id: sqlite-destroy.sql,v 1.1 2004-07-22 16:49:20 dfrankow Exp $
-
--- Destroy not yet written ?
