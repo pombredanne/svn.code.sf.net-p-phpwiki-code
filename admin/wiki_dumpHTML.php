1ff56565e08c1e77287b1518e581f0246d9316ba
nobody <nobody@96ab9672-09ca-45d6-a79d-3d69d39ca109>
2005-01-07 14:21:11 +0000

    This commit was manufactured by cvs2svn to create tag 'release-1_2_7'.

---
+++ admin/wiki_dumpHTML.php
@@ -0,0 +1,7 @@
+<!-- $Id: wiki_dumpHTML.php,v 1.1.2.1.2.3 2005-01-07 14:02:27 rurban Exp $ -->
+<?php
+
+   echo "Dump HTML called.<p>\n";
+   echo "Got: $dumpHTML $directory<br>\n";
+
+?>
---
+++ admin/wiki_rebuilddbms.php
@@ -0,0 +1,6 @@
+<!-- $Id: wiki_rebuilddbms.php,v 1.1.2.1.2.3 2005-01-07 14:02:27 rurban Exp $ -->
+<?php
+
+   echo "Rebuild DBM files called.<p>\n";
+
+?>
