00f614b88951c9d9947a3ab84afd7731f2f4cf90
rurban <rurban@96ab9672-09ca-45d6-a79d-3d69d39ca109>
2005-04-10 10:43:25 +0000

    more mysql-4.1 quirks: add utf8 to the charset aliases table. (Bug #1180108)

--- lib/WikiDB/backend/ADODB_mysql.php
+++ lib/WikiDB/backend/ADODB_mysql.php
@@ -1,5 +1,5 @@
 <?php // -*-php-*-
-rcs_id('$Id: ADODB_mysql.php,v 1.14 2005-04-01 14:32:44 rurban Exp $');
+rcs_id('$Id: ADODB_mysql.php,v 1.15 2005-04-10 10:43:25 rurban Exp $');
 
 require_once('lib/WikiDB/backend/ADODB.php');
 
@@ -46,13 +46,13 @@ extends WikiDB_backend_ADODB
         // esp. needed for utf databases
         if ($this->_serverinfo['version'] > 401.0) {
             global $charset;
+            $aliases = array('iso-8859-1' => 'latin1',
+                             'utf-8'      => 'utf8');
             //http://dev.mysql.com/doc/mysql/en/charset-connection.html
-            if (strtolower($charset) == 'iso-8859-1') {
-                // mysql needs different names and doesn't resolve aliases
-                mysql_query("SET NAMES 'latin1'");
-                //mysql_query("SET CHARACTER SET latin1");
+            if (isset($aliases[strtolower($charset)])) {
+                // mysql needs special unusual names and doesn't resolve aliases
+                mysql_query("SET NAMES '". $aliases[$charset] . "'");
             } else {
-                //SET NAMES 'latin1' or 'utf8'
                 mysql_query("SET NAMES '$charset'");
             }
         }
