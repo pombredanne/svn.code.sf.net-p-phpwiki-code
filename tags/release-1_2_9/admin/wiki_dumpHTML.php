ff56a8957680d8319dba57ba6729e29d84285906
nobody <nobody@96ab9672-09ca-45d6-a79d-3d69d39ca109>
2005-07-23 11:37:16 +0000

    This commit was manufactured by cvs2svn to create tag 'release-1_2_9'.

---
+++ tags/release-1_2_9/admin/wiki_dumpHTML.php
@@ -0,0 +1,7 @@
+<!-- $Id: wiki_dumpHTML.php,v 1.1.2.1.2.4 2005-01-07 14:23:04 rurban Exp $ -->
+<?php
+
+   echo "Dump HTML called.<p>\n";
+   echo "Got: $dumpHTML $directory<br>\n";
+
+?>
---
+++ tags/release-1_2_9/lib/pageinfo.php
@@ -0,0 +1,76 @@
+<!-- $Id: pageinfo.php,v 1.5.4.4 2005-01-07 14:23:05 rurban Exp $ -->
+<!-- Display the internal structure of a page. Steve Wainstead, June 2000 -->
+<?php
+   if (get_magic_quotes_gpc()) {
+      $info = stripslashes($info);
+   }
+
+   $encname = htmlspecialchars($info);
+   $enter = gettext ("Enter a page name");
+   $go = gettext ("Go");
+   $html = "<form action=\"$ScriptUrl\" method=\"get\">\n" .
+	   "<input name=\"info\" value=\"$encname\" />" .
+	   " $enter\n" .
+	   "<input type=\"submit\" value=\"$go\" /><br />\n" .
+	   "<input type=\"checkbox\" name=\"showpagesource\"";
+
+   if (isset($showpagesource) && ($showpagesource == "on")) {
+      $html .= " checked";
+   }
+   $html .= " /> ";
+   $html .= gettext ("Show the page source and references");
+   $html .= "\n</form>\n";
+
+   // don't bother unless we were asked
+   if (! $info) {
+      GeneratePage('MESSAGE', $html, gettext("PageInfo"), 0);
+      exit;
+   }
+
+   function ViewpageProps($name, $pagestore)
+   {
+      global $dbi, $showpagesource, $datetimeformat, $FieldSeparator;
+
+      $pagehash = RetrievePage($dbi, $name, $pagestore);
+      if ($pagehash == -1) {
+         $table = sprintf (gettext ("Page name '%s' is not in the database"),
+		$name) . "\n";
+      }
+      else {
+	 $table = "<table border=\"1\" bgcolor=\"white\">\n";
+
+	 while (list($key, $val) = each($pagehash)) {
+	    if ($key > 0 || !$key) #key is an array index
+	       continue;
+            if ((gettype($val) == "array") && ($showpagesource == "on")) {
+               $val = implode($val, "$FieldSeparator#BR#$FieldSeparator\n");
+	       $val = htmlspecialchars($val);
+	       $val = str_replace("$FieldSeparator#BR#$FieldSeparator", "<br>", $val);
+            }
+	    elseif (($key == 'lastmodified') || ($key == 'created'))
+	       $val = date($datetimeformat, $val);
+	    elseif (gettype($val) != "array")
+	       $val = htmlspecialchars($val);
+
+            $table .= "<tr><td>$key</td><td>$val</td></tr>\n";
+	 }
+
+	 $table .= "</table>";
+      }
+      return $table;
+   }
+
+   $html .= "<p><b>";
+   $html .= gettext ("Current version");
+   $html .= "</b></p>";
+   // $dbi = OpenDataBase($WikiPageStore);   --- done by index.php
+   $html .= ViewPageProps($info, $WikiPageStore);
+
+   $html .= "<p><b>";
+   $html .= gettext ("Archived version");
+   $html .= "</b></p>";
+   // $dbi = OpenDataBase($ArchivePageStore);
+   $html .= ViewPageProps($info, $ArchivePageStore);
+
+   GeneratePage('MESSAGE', $html, gettext("PageInfo").": '$info'", 0);
+?>
