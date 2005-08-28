a183191cd030bd86966c468576c9f38e74edc171
rurban <rurban@96ab9672-09ca-45d6-a79d-3d69d39ca109>
2005-08-28 15:55:57 +0000

    russian locale by Pavel Zaichenko

---
+++ branches/release-1_2-branch/locale/ru/LC_MESSAGES/phpwiki.php
@@ -0,0 +1,172 @@
+<?php
+
+$locale["Set the administrator account and password first.\n"] =
+   "Сначала установите учетную запись и пароль администратора.\n";
+$locale["You entered an invalid login or password."] =
+   "Вы указали неверное имя или пароль.";
+$locale["You are about to remove '%s' permanently!"] =
+   "Вы собираетесь безвозвратно удалить '%s'!";
+$locale["Click %shere%s to remove the page now."] =
+   "Щелкните %sздесь%s для удаления страницы.";
+$locale["Otherwise press the \"Back\" button of your browser."] =
+   "Иначе нажмите кнопку Вашего браузера \"Назад\".";
+$locale["Function not yet implemented."] =
+   "Функция еще не реализована.";
+$locale["Remove page"] =
+   "Удалить страницу";
+$locale["Removed page '%s' succesfully."] =
+   "Страница '%s' успешно удалена.";
+$locale["Pages which link to %s"] =
+   "Страницы, которые ссылаются на %s";
+$locale["%d pages link to %s."] =
+   "%d страниц ссылаются на %s.";
+$locale["_wiki_name_regexp_"] =
+   "(?<![[:alnum:]])([[:upper:]][[:lower:]]+)+[[:upper:]]?([[:upper:]][[:lower:]]+)+(?![[:alnum:]])";
+$locale["templates/browse.html"] =
+   "locale/ru/templates/browse.html";
+$locale["templates/editpage.html"] =
+   "locale/ru/templates/editpage.html";
+$locale["templates/editlinks.html"] =
+   "locale/ru/templates/editlinks.html";
+$locale["templates/message.html"] =
+   "locale/ru/templates/message.html";
+$locale["./pgsrc"] =
+   "locale/ru/pgsrc";
+$locale["'%s': corrupt file"] =
+   "'%s': файл испорчен";
+$locale["%s: bad data<br>\n"] =
+   "%s: неверные данные<br>\n";
+$locale["%s: Bad filename in database<br>\n"] =
+   "%s: Неверное имя файла в базе данных<br>\n";
+$locale["WikiDiff::apply: line count mismatch: %s != %s"] =
+   "WikiDiff::apply: количество строк не совпадает: %s != %s";
+$locale["WikiDiff::_check: failed"] =
+   "WikiDiff::_check: сбой";
+$locale["WikiDiff::_check: edit sequence is non-optimal"] =
+   "WikiDiff::_check: последовательность изменений не оптимальна";
+$locale["WikiDiff Okay: LCS = %s"] =
+   "WikiDiff Okay: LCS = %s";
+$locale["Current page:"] =
+   "Текущая страница:";
+$locale["version %s"] =
+   "версия %s";
+$locale["last modified on %s"] =
+   "изменена %s";
+$locale["by %s"] =
+   "%s";
+$locale["None"] =
+   "None";
+$locale["Archived page:"] =
+   "Архивная страница:";
+$locale["Versions are identical"] =
+   "Версии идентичны";
+$locale["Diff of %s."] =
+   "Изменения %s.";
+$locale["FrontPage"] =
+   "ТитульнаяСтраница";
+$locale["Describe %s here."] =
+   "Опишите здесь %s.";
+$locale["Copy of %s"] =
+   "Копия %s";
+$locale["No page name passed into editpage!"] =
+   "Не указано имя страницы для редактирования!";
+$locale["This page has been locked by the administrator and cannot be edited."] =
+   "Эта страница была заблокирована администратором и не может быть изменена.";
+$locale["Sorry for the inconvenience."] =
+   "Извините за неудобство.";
+$locale["Problem while editing %s"] =
+   "Проблема при редактировании %s";
+$locale["Searching for \"%s\" ....."] =
+   "Поиск \"%s\" .....";
+$locale["(You entered an empty search string)"] =
+   "(Вы указали пустую строку для поиска)";
+$locale["%d matches found in %d pages."] =
+   "Найдено %d совпадений в %d страницах.";
+$locale["Full Text Search Results"] =
+   "Результаты полнотекстового поиска";
+$locale["Cannot establish connection to database, giving up."] =
+   "Не могу установить соединение с базой данных, отказ.";
+$locale["Error message: %s"] =
+   "Сообщение об ошибке: %s";
+$locale["Cannot open database %s, giving up."] =
+   "Не могу открыть базу данных %s, отказ.";
+$locale["Insert/update to table 'wiki' failed: %s"] =
+   "Сбой при выполнении вставки/обновления таблицы 'wiki': %s";
+$locale["Delete on %s failed: %s"] =
+   "Сбой при удалении из %s: %s";
+$locale["Insert into %s failed: %s"] =
+   "Сбой при вставке в %s: %s";
+$locale["Insert/update into table 'archive' failed: %s"] =
+   "Сбой при выполнении вставки/обновления таблицы 'archive': %s";
+$locale["MSSQL error: %s"] =
+   "Ошибка MSSQL: %s";
+$locale["Insert/Update failed: %s"] =
+   "Сбой при выполнении вставки/обновления: %s";
+$locale["Cannot delete '%s' from table '%s'"] =
+   "Не могу удалить '%s' из таблицы '%s'";
+$locale["MySQL error: %s"] =
+   "Ошибка MySQL: %s";
+$locale["Error writing page '%s'"] =
+   "Ошибка при записи страницы '%s'";
+$locale["Enter a page name"] =
+   "Введите имя страницы";
+$locale["Go"] =
+   "Выполнить";
+$locale["Show the page source and references"] =
+   "Показать текст страницы и ссылки";
+$locale["PageInfo"] =
+   "Инфо";
+$locale["Page name '%s' is not in the database"] =
+   "В базе данных нет страницы с именем '%s'";
+$locale["Current version"] =
+   "Текущая версия";
+$locale["Archived version"] =
+   "Архивная версия";
+$locale["RecentChanges"] =
+   "НедавниеИзменения";
+$locale["PhpWiki is unable to save your changes, because another user edited and saved the page while you were editing the page too. If saving proceeded now changes from the previous author would be lost."] =
+   "PhpWiki не может сохранить сделанные Вами изменения, так как за время редактирования страницы она была изменена и сохранена другим пользователем. Если продолжить сохранение, то изменения, сделанные другим пользователем, будут утеряны.";
+$locale["In order to recover from this situation follow these steps:"] =
+   "Для выхода из создавшейся ситуации выполните следующие действия:";
+$locale["Use your browser's <b>Back</b> button to go back to the edit page."] =
+   "Используйте кнопку Вашего браузера <b>Назад</b> для возврата к экрану редактирования страницы.";
+$locale["Copy your changes to the clipboard or to another temporary place (e.g. text editor)."] =
+   "Скопируйте Ваши изменения в буфер обмена или другое временное место (например, текстовый редактор).";
+$locale["<b>Reload</b> the page. You should now see the most current version of the page. Your changes are no longer there."] =
+   "<b>Обновите</b> страницу. Вы должны теперь видеть самую последнюю версию страницы. Эта версия страницы не содержит изменений, сделанных Вами.";
+$locale["Make changes to the file again. Paste your additions from the clipboard (or text editor)."] =
+   "Повторно внесите Ваши изменения в файл. Вставьте Ваши дополнения и буфера обмена (или текстового редактора).";
+$locale["Press <b>Save</b> again."] =
+   "Снова нажмите <b>Сохранить</b>.";
+$locale["Problem while updating %s"] =
+   "Проблема при обновлении %s";
+$locale["Thank you for editing %s."] =
+   "Благодарим за редактирование страницы %s.";
+$locale["Your careful attention to detail is much appreciated."] =
+   "Мы высоко ценим Ваше тщательное внимание к деталям.";
+$locale["%d pages match your query."] =
+   "%d страниц соответствую Вашему запросу.";
+$locale["Title Search Results"] =
+   "Результаты поиска по заголовкам";
+$locale["Inserting page %s, version %s from %s"] =
+   "Добавление станицы %s, версия %s из %s";
+$locale["WikiFatalError"] =
+   "WikiFatalError";
+$locale["_alnum_regexp_class_"] =
+   "[0-9A-Za-z]|А|Б|В|Г|Д|Е|Ё|Ж|З|И|Й|К|Л|М|Н|О|П|Р|С|Т|У|Ф|Х|Ц|Ч|Ш|Щ|Ь|Ы|Ъ|Э|Ю|Я|а|б|в|г|д|е|ё|ж|з|и|й|к|л|м|н|о|п|р|с|т|у|ф|х|ц|ч|ш|щ|ь|ы|ъ|э|ю|я";
+$locale["_alpha_regexp_class_"] =
+   "[A-Za-z]|А|Б|В|Г|Д|Е|Ё|Ж|З|И|Й|К|Л|М|Н|О|П|Р|С|Т|У|Ф|Х|Ц|Ч|Ш|Щ|Ь|Ы|Ъ|Э|Ю|Я|а|б|в|г|д|е|ё|ж|з|и|й|к|л|м|н|о|п|р|с|т|у|ф|х|ц|ч|ш|щ|ь|ы|ъ|э|ю|я";
+$locale["_upper_regexp_class_"] =
+   "[A-Z]|А|Б|В|Г|Д|Е|Ё|Ж|З|И|Й|К|Л|М|Н|О|П|Р|С|Т|У|Ф|Х|Ц|Ч|Ш|Щ|Ь|Ы|Ъ|Э|Ю|Я";
+$locale["_lower_regexp_class_"] =
+   "[a-z]|а|б|в|г|д|е|ё|ж|з|и|й|к|л|м|н|о|п|р|с|т|у|ф|х|ц|ч|ш|щ|ь|ы|ъ|э|ю|я";
+$locale["Search"] =
+   "Поиск";
+$locale["%d best incoming links:"] =
+   "%d лучших входящих ссылок:";
+$locale["%d best outgoing links:"] =
+   "%d лучших исходящих ссылок:";
+$locale["%d most popular nearby:"] =
+   "%d наиболее популярных соседних:";
+
+;?>
