10cc9ed688227c5154df634c3c0db1f00d923652
rurban <rurban@96ab9672-09ca-45d6-a79d-3d69d39ca109>
2005-09-14 06:06:43 +0000

    bump version to 1.3.11p1

--- trunk/lib/prepend.php
+++ trunk/lib/prepend.php
@@ -9,9 +9,10 @@ function rcs_id ($id) {
     if (defined('DEBUG') and DEBUG)
         $GLOBALS['RCS_IDS'] .= "$id\n"; 
 }
-rcs_id('$Id: prepend.php,v 1.42 2005-09-11 13:23:43 rurban Exp $');
+rcs_id('$Id: prepend.php,v 1.43 2005-09-14 06:06:43 rurban Exp $');
 
-define('PHPWIKI_VERSION', '1.3.11_20050911');
+// see lib/stdlib.php: phpwiki_version()
+define('PHPWIKI_VERSION', '1.3.11p1');
 
 /** 
  * Returns true if current php version is at mimimum a.b.c 
