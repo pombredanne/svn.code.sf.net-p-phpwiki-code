4313338dad9092300b365b94c3dbd7575759f42b
rurban <rurban@96ab9672-09ca-45d6-a79d-3d69d39ca109>
2007-01-07 18:46:15 +0000

    Wording: use imagepath

--- lib/plugin/TeX2png.php
+++ lib/plugin/TeX2png.php
@@ -1,5 +1,5 @@
 <?php // -*-php-*-
-rcs_id('$Id: TeX2png.php,v 1.3 2004-11-01 10:43:59 rurban Exp $');
+rcs_id('$Id: TeX2png.php,v 1.4 2007-01-07 18:46:15 rurban Exp $');
 /*
  Copyright 2004 Pierrick Meignen
 */
@@ -35,7 +35,7 @@ extends WikiPlugin
     }
     
     function getVersion() {
-        return preg_replace("/[Revision: $]/", '',"\$Revision: 1.3 $");
+        return preg_replace("/[Revision: $]/", '',"\$Revision: 1.4 $");
     }
     
     function getDefaultArguments() {
@@ -170,7 +170,7 @@ extends WikiPlugin
                 $this->createTexFile($texfile, $text);     
                 $this->createPngFile($this->imagepath, $imagename);
             } else {
-                $error_html = _("TeX directory not writable.");
+                $error_html = _("TeX imagepath not writable.");
                 trigger_error($error_html, E_USER_NOTICE);
             }
         }
@@ -221,6 +221,12 @@ extends WikiPlugin
 };
 
 // $Log: not supported by cvs2svn $
+// Revision 1.3  2004/11/01 10:43:59  rurban
+// seperate PassUser methods into seperate dir (memory usage)
+// fix WikiUser (old) overlarge data session
+// remove wikidb arg from various page class methods, use global ->_dbi instead
+// ...
+//
 
 // For emacs users
 // Local Variables:
