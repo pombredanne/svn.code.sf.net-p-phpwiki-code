5ca6e18f3b5495d428b1ff4458b2d298f4e997cd
rurban <rurban@96ab9672-09ca-45d6-a79d-3d69d39ca109>
2007-01-21 13:20:08 +0000

    Fix hits display

--- lib/plugin/SearchHighlight.php
+++ lib/plugin/SearchHighlight.php
@@ -1,5 +1,5 @@
 <?php // -*-php-*-
-rcs_id('$Id: SearchHighlight.php,v 1.2 2007-01-20 15:53:51 rurban Exp $');
+rcs_id('$Id: SearchHighlight.php,v 1.3 2007-01-21 13:20:08 rurban Exp $');
 /*
 Copyright 2004,2007 $ThePhpWikiProgrammingTeam
 
@@ -45,13 +45,13 @@ extends WikiPlugin
 
     function getVersion() {
         return preg_replace("/[Revision: $]/", '',
-                            "\$Revision: 1.2 $");
+                            "\$Revision: 1.3 $");
     }
 
     function getDefaultArguments() {
         // s, engine and engine_url are picked from the request
         return array('noheader' => false,    //don't print the header
-                     'hits'     => false,    //print the list of found terms additionally
+                     'hits'     => false,    //print the list of lines with lines terms additionally
                      's'        => false,
                      'case_exact' => false,  //not yet supported
                      'regex'    => false,    //not yet supported
@@ -88,27 +88,31 @@ extends WikiPlugin
     function showhits($page, $hilight_re) {
         $current = $page->getCurrentRevision();
         $matches = preg_grep("/$hilight_re/i", $current->getContent());
-        $html = array();
+        $html = HTML::dl();
         foreach ($matches as $line) {
             $line = $this->highlight_line($line, $hilight_re);
-            $html[] = HTML::dd(HTML::small(array('class' => 'search-context'),
-                                           $line));
+            $html->pushContent(HTML::dd(array('class' => 'search-context'),
+                                        HTML::small($line)));
         }
         return $html;
     }
 
     function highlight_line ($line, $hilight_re) {
+        $html = HTML();
         while (preg_match("/^(.*?)($hilight_re)/i", $line, $m)) {
             $line = substr($line, strlen($m[0]));
-            $html[] = $m[1];    // prematch
-            $html[] = HTML::strong(array('class' => 'search-term'), $m[2]); // match
+            // prematch + match
+            $html->pushContent($m[1], HTML::strong(array('class' => 'search-term'), $m[2])); 
         }
-        $html[] = $line;        // postmatch
+        $html->pushContent($line);       // postmatch
         return $html;
     }
 };
 
 // $Log: not supported by cvs2svn $
+// Revision 1.2  2007/01/20 15:53:51  rurban
+// Rewrite of SearchHighlight: through ActionPage and InlineParser
+//
 // Revision 1.1  2004/09/26 14:58:36  rurban
 // naive SearchHighLight implementation
 //
