908d4b665c136d172ef986efda9e1a1909354ab8
rurban <rurban@96ab9672-09ca-45d6-a79d-3d69d39ca109>
2007-05-24 18:39:17 +0000

    improved WantedPages

--- trunk/lib/WikiDB/backend/dumb/WantedPagesIter.php
+++ trunk/lib/WikiDB/backend/dumb/WantedPagesIter.php
@@ -1,5 +1,5 @@
 <?php // -*-php-*-
-rcs_id('$Id: WantedPagesIter.php,v 1.2 2007-01-04 16:45:49 rurban Exp $');
+rcs_id('$Id: WantedPagesIter.php,v 1.3 2007-05-24 18:39:17 rurban Exp $');
 
 //require_once('lib/WikiDB/backend.php');
 
@@ -20,22 +20,41 @@ extends WikiDB_backend_iterator
             $this->exclude = $exclude ? PageList::explodePageList($exclude) : array();
         else 
             $this->exclude = $exclude;
+        $this->sortby = $sortby; // ignored
+        if ($limit) { // extract from,count from limit
+            list($this->from, $this->limit) = $backend->limit($limit);
+        } else { 
+            $this->limit = 0;
+            $this->from = 0;
+        }
+        $this->pos = 0;
+        $this->pagelinks = array();
     }
 
     function next() {
         while ($page = $this->_allpages->next()) {
+            while ($this->pagelinks) { // deferred return
+            	return array_pop($this->pagelinks);
+            }
+    	    $this->pagelinks = array();
+            if ($this->limit and $this->pos > $this->limit) break;
             $pagename = $page['pagename'];
             $links = $this->_backend->get_links($pagename, false);
             while ($link = $links->next()) {
+            	if ($this->limit and $this->pos > $this->limit) break;
                 if ($this->exclude and in_array($link['pagename'], $this->exclude)) continue;
-                // better membership for a pageiterator???
+                // better membership for a pageiterator?
                 if (! in_array($link['pagename'], $this->_allpages_array)) {
-                    $links->free();
+                    if ($this->from and $this->pos < $this->from) continue;
+                    // collect all links per page and return them deferred
                     $link['wantedfrom'] = $pagename;
-                    return $link;
+                    $this->pagelinks[] = $link;
+                    $this->pos++;
                 }
             }
             $links->free();
+            unset($links);
+            if ($this->pagelinks) return array_pop($this->pagelinks);
         }
         return false;
     }
@@ -43,6 +62,8 @@ extends WikiDB_backend_iterator
     function free() {
         unset($this->_allpages_array);
         $this->_allpages->free();
+        unset($this->_allpages);
+        unset($this->_backend);
     }
 }
 
