9643a33e34f9263a7894f9d21d64ce5ca6d6310c
rurban <rurban@96ab9672-09ca-45d6-a79d-3d69d39ca109>
2007-08-10 22:02:58 +0000

    Don't escape passwords, only utf8 encode. escape all other names and attributes. support new option LDAP_SEARCH_FILTER

--- trunk/lib/WikiUser/LDAP.php
+++ trunk/lib/WikiUser/LDAP.php
@@ -1,5 +1,5 @@
 <?php //-*-php-*-
-rcs_id('$Id: LDAP.php,v 1.9 2007-06-13 12:48:14 rurban Exp $');
+rcs_id('$Id: LDAP.php,v 1.10 2007-08-10 22:02:58 rurban Exp $');
 /* Copyright (C) 2004,2007 $ThePhpWikiProgrammingTeam
  * This file is part of PhpWiki. Terms and Conditions see LICENSE. (GPL2)
  */
@@ -12,6 +12,10 @@ extends _PassUser
  * Preferences are handled in _PassUser
  */
 {
+    /**
+     * ->_init()
+     * connect and bind to the LDAP host
+     */
     function _init() {
         if ($this->_ldap = ldap_connect(LDAP_AUTH_HOST)) { // must be a valid LDAP server!
             global $LDAP_SET_OPTION;
@@ -42,7 +46,10 @@ extends _PassUser
             return false;
         }
     }
-    
+
+    /**    
+     * free and close the bound ressources
+     */
     function _free() {
         if (isset($this->_sr)   and is_resource($this->_sr))   ldap_free_result($this->_sr);
         if (isset($this->_ldap) and is_resource($this->_ldap)) ldap_close($this->_ldap);
@@ -50,6 +57,65 @@ extends _PassUser
         unset($this->_ldap);
     }
 
+    /**
+     * LDAP names allow all chars but "*", "(", ")", "\", "NUL". 
+     * " should be quoted as \"
+     * Quoting is done by \xx (two-digit hexcode). "*" <=> "\2a"
+     * Non-ascii chars must be converted to utf-8.
+     * Password should NOT be escaped, just converted to utf-8.
+     *
+     * @see http://www.faqs.org/rfcs/rfc4514.html LDAP String Representation of Distinguished Names
+     */
+    function _stringEscape($name) {
+	$name = strtr(utf8_encode($name), 
+		      array("*" => "\\2a",
+			    "?" => "\\3f",
+			    "(" => "\\28",
+			    ")" => "\\29",
+			    "\\" => "\\5c",
+			    '"'  => '\"',
+			    "\0" => "\\00"));
+	return $name;
+    }
+
+    /**
+     * LDAP names may contain every utf-8 character. However we restrict them a bit for convenience.
+     * @see _stringEscape()
+     */
+    function isValidName ($userid = false) {
+        if (!$userid) $userid = $this->_userid;
+	// We are more restrictive here, but must allow explitly utf-8
+        return preg_match("/^[\-\w_\.@ ]+$/u", $userid) and strlen($userid) < 64;
+    }
+
+    /**
+     * Construct the configured search filter and properly escape the userid.
+     * Apply LDAP_SEARCH_FIELD and optionally LDAP_SEARCH_FILTER.
+     *
+     * @param string $userid username, unquoted in the current charset.
+     * @access private
+     * @return string The 3rd argument to ldap_search()
+     * @see http://www.faqs.org/rfcs/rfc4514.html LDAP String Representation of Distinguished Names
+     */
+    function _searchparam($userid) {
+	$euserid = $this->_stringEscape($userid);
+	// Need to set the right root search information. See config/config.ini
+	if (LDAP_SEARCH_FILTER) {
+	    $st_search = str_replace("\$userid", $euserid, LDAP_SEARCH_FILTER);
+	} else {
+	    $st_search = LDAP_SEARCH_FIELD
+		? LDAP_SEARCH_FIELD."=$euserid"
+		: "uid=$euserid";
+	}
+	return $st_search;
+    }
+
+    /**
+     * Passwords must not be escaped, but sent as "stringprep"'ed utf-8. 
+     *
+     * @see http://www.faqs.org/rfcs/rfc4514.html LDAP String Representation of Distinguished Names
+     * @see http://www.faqs.org/rfcs/rfc3454.html stringprep
+     */
     function checkPass($submitted_password) {
 
         $this->_authmethod = 'LDAP';
@@ -63,23 +129,22 @@ extends _PassUser
             $this->_free();
             return WIKIAUTH_FORBIDDEN;
         }
-        // A LDAP speciality: empty passwords are valid with ldap_bind!!!
+        // A LDAP speciality: Empty passwords are always true for ldap_bind !!! 
+	// So we have to disallow this regardless of PASSWORD_LENGTH_MINIMUM = 0
         if (strlen($submitted_password) == 0) {
             trigger_error(_("Empty password not allowed for LDAP"), E_USER_WARNING);
             $this->_free();
-            return WIKIAUTH_FORBIDDEN;
+	    return $this->_tryNextPass($submitted_password);
+	    //return WIKIAUTH_FORBIDDEN;
         }
-        if (strstr($userid,'*')) {
+        /*if (strstr($userid,'*')) { // should be safely escaped now
             trigger_error(fmt("Invalid username '%s' for LDAP Auth", $userid), 
                           E_USER_WARNING);
             return WIKIAUTH_FORBIDDEN;
-        }
+	}*/
 
         if ($ldap = $this->_init()) {
-            // Need to set the right root search information. See config/config.ini
-            $st_search = LDAP_SEARCH_FIELD
-                ? LDAP_SEARCH_FIELD."=$userid"
-                : "uid=$userid";
+	    $st_search = $this->_searchparam($userid);
             if (!$this->_sr = ldap_search($ldap, LDAP_BASE_DN, $st_search)) {
 		trigger_error(_("Could not search in LDAP"), E_USER_WARNING);
  		$this->_free();
@@ -96,65 +161,56 @@ extends _PassUser
             // Of course it would be better to narrow down the BASE_DN
             for ($i = 0; $i < $info["count"]; $i++) {
                 $dn = $info[$i]["dn"];
-                // The password is still plain text.
-		// LDAP allows all chars but *, (, ), \, NUL
-		// Quoting is done by \xx (two-digit hexcode). * <=> \2a
-		// Handling '?' is unspecified
-		$password = strtr($submitted_password, 
-				array("*" => "\\2a",
-				      "?" => "\\3f",
-				      "(" => "\\28",
-				      ")" => "\\29",
-				      "\\" => "\\5c",
-				      "\0" => "\\00"));
+                // The password must be converted to utf-8, but unescaped.
                 // On wrong password the ldap server will return: 
                 // "Unable to bind to server: Server is unwilling to perform"
                 // The @ catches this error message.
-                if ($r = @ldap_bind($ldap, $dn, $password)) {
+		// If CHARSET=utf-8 the form should have already converted it to utf-8.
+                if ($r = @ldap_bind($ldap, $dn, $submitted_password)) {
                     // ldap_bind will return TRUE if everything matches
-		    // Get the mail from ldap
+		    // Optionally get the mail from LDAP
 		    if (!empty($info[$i]["mail"][0])) {
 			$this->_prefs->_prefs['email']->default_value = $info[$i]["mail"][0];
 		    }
             	    $this->_free();
                     $this->_level = WIKIAUTH_USER;
                     return $this->_level;
-                }
+                } else {
+		    // Try again, this time explicitly
+		    if ($r = @ldap_bind($ldap, $dn, utf8_encode($submitted_password))) {
+			if (!empty($info[$i]["mail"][0])) {
+			    $this->_prefs->_prefs['email']->default_value = $info[$i]["mail"][0];
+			}
+			$this->_free();
+			$this->_level = WIKIAUTH_USER;
+			return $this->_level;
+		    }
+		}
             }
 	    if (DEBUG)
 		trigger_error(_("Wrong password: ") . 
-			      str_repeat("*",strlen($submitted_password)), 
+			      str_repeat("*", strlen($submitted_password)), 
 			      E_USER_WARNING);
             $this->_free();
         } else {
             $this->_free();
-	    trigger_error(_("Could not connect to LDAP"), E_USER_WARNING);
+	    trigger_error(fmt("Could not connect to LDAP host %s", LDAP_AUTH_HOST), E_USER_WARNING);
 	}
 
         return $this->_tryNextPass($submitted_password);
     }
 
 
-    function isValidName ($userid = false) {
-        if (!$userid) $userid = $this->_userid;
-	// LDAP allows all chars but *, (, ), \, NUL
-	// Quoting is done by \xx (two-digit hexcode). * <=> \2a
-	// We are more restrictive here, but must allow explitly utf-8
-        return preg_match("/^[\-\w_\.@ ]+$/u", $userid) and strlen($userid) < 64;
-    }
-
     function userExists() {
         $userid = $this->_userid;
-        if (strstr($userid, '*')) {
+        /*if (strstr($userid, '*')) {
             trigger_error(fmt("Invalid username '%s' for LDAP Auth", $userid),
                           E_USER_WARNING);
             return false;
-        }
+	}*/
         if ($ldap = $this->_init()) {
             // Need to set the right root search information. see ../index.php
-            $st_search = LDAP_SEARCH_FIELD
-                ? LDAP_SEARCH_FIELD."=$userid"
-                : "uid=$userid";
+	    $st_search = $this->_searchparam($userid);
             if (!$this->_sr = ldap_search($ldap, LDAP_BASE_DN, $st_search)) {
  		$this->_free();
         	return $this->_tryNextUser();
@@ -178,6 +234,9 @@ extends _PassUser
 }
 
 // $Log: not supported by cvs2svn $
+// Revision 1.9  2007/06/13 12:48:14  rurban
+// fix wrong fix from 1.3.13p1
+//
 // Revision 1.8  2007/06/07 16:31:33  rurban
 // Important! Fixes bug #1732882 ldap_bind with empty password
 // Adds diagnostics on other ldap failures
