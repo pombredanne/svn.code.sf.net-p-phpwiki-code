9a9d71b5ba5f5987226e1539602decba624c6c33
vargenau <vargenau@96ab9672-09ca-45d6-a79d-3d69d39ca109>
2008-06-20 18:07:17 +0000

    Fix plugin-list syntax

--- trunk/lib/plugin/IncludePages.php
+++ trunk/lib/plugin/IncludePages.php
@@ -1,5 +1,5 @@
 <?php // -*-php-*-
-rcs_id('$Id: IncludePages.php,v 1.3 2007-07-14 12:04:32 rurban Exp $');
+rcs_id('$Id: IncludePages.php,v 1.4 2008-06-20 18:07:17 vargenau Exp $');
 /*
  Copyright 2004 $ThePhpWikiProgrammingTeam
 
@@ -22,7 +22,7 @@ rcs_id('$Id: IncludePages.php,v 1.3 2007-07-14 12:04:32 rurban Exp $');
 
 /**
  * IncludePages: Include a list of multiple pages, based on IncludePage.
- * usage:   <?plugin IncludePages pages=<?plugin-list BackLinks ?> ?>
+ * usage:   <?plugin IncludePages pages=<!plugin-list BackLinks !> ?>
  * author:  ReiniUrban
  * 
  * Todo: follow RedirectTo
@@ -43,7 +43,7 @@ extends WikiPlugin_IncludePage
 
     function getVersion() {
         return preg_replace("/[Revision: $]/", '',
-                            "\$Revision: 1.3 $");
+                            "\$Revision: 1.4 $");
     }
 
     function getDefaultArguments() {
@@ -84,6 +84,9 @@ extends WikiPlugin_IncludePage
 };
 
 // $Log: not supported by cvs2svn $
+// Revision 1.3  2007/07/14 12:04:32  rurban
+// todo comment
+//
 // Revision 1.2  2005/09/30 18:41:39  uckelman
 // Fixed more passes-by-reference.
 //
