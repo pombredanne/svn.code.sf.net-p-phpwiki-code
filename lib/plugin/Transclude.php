23b4aa7a4c2ec4b728286c416faeedbb71c8dcbf
vargenau <vargenau@96ab9672-09ca-45d6-a79d-3d69d39ca109>
2008-10-20 16:47:13 +0000

    Implemented 'quiet' argument in Transclude plugin, like in IncludePage plugin

--- lib/plugin/Transclude.php
+++ lib/plugin/Transclude.php
@@ -62,7 +62,8 @@ extends WikiPlugin
 
     function getDefaultArguments() {
         return array( 'src'     => false, // the src url to include
-                      'height'  => 450 // height of the iframe
+                      'height'  => 450, // height of the iframe
+                      'quiet'   => false // if set, frame appears as normal content
                     );
     }
 
@@ -114,9 +115,13 @@ extends WikiPlugin
         $iframe = new HtmlElement('ilayer', array('src' => $src), $iframe);
         */
 
-        return HTML(HTML::p(array('class' => 'transclusion-title'),
-                            fmt("Transcluded from %s", LinkURL($src))),
-                    $this->_js(), $iframe);
+        if ($quiet) {
+            return HTML($this->_js(), $iframe);
+        } else {
+            return HTML(HTML::p(array('class' => 'transclusion-title'),
+                                fmt("Transcluded from %s", LinkURL($src))),
+                        $this->_js(), $iframe);
+        }
     }
 
     /**
@@ -156,46 +161,6 @@ extends WikiPlugin
           ');
     }
 };
-
-// $Log: not supported by cvs2svn $
-// Revision 1.9  2004/06/14 11:31:39  rurban
-// renamed global $Theme to $WikiTheme (gforge nameclash)
-// inherit PageList default options from PageList
-//   default sortby=pagename
-// use options in PageList_Selectable (limit, sortby, ...)
-// added action revert, with button at action=diff
-// added option regex to WikiAdminSearchReplace
-//
-// Revision 1.8  2004/02/17 12:11:36  rurban
-// added missing 4th basepage arg at plugin->run() to almost all plugins. This caused no harm so far, because it was silently dropped on normal usage. However on plugin internal ->run invocations it failed. (InterWikiSearch, IncludeSiteMap, ...)
-//
-// Revision 1.7  2003/02/27 22:47:27  dairiki
-// New functions in HtmlElement:
-//
-// JavaScript($js)
-//    Helper for generating javascript.
-//
-// IfJavaScript($if_content, $else_content)
-//    Helper for generating
-//       <script>document.write('...')</script><noscript>...</noscript>
-//    constructs.
-//
-// Revision 1.6  2003/02/25 05:45:34  carstenklapp
-// Added "See: " in front of url, so for browsers that do not support
-// <iframe> at least there is an indication to the user that this
-// plugin is actually doing something while at the same time without
-// being (subjectively) too disruptive to page content.
-//
-// Revision 1.5  2003/02/24 14:34:44  carstenklapp
-// Added iframe title (bobby.org accessibility guidelines).
-// Simplified output for non-iframe and non-visual browsers (as suggested
-// by http://www.uwosh.edu/programs/accessibility/tutorial.html).
-//
-// Revision 1.4  2003/01/18 22:08:01  carstenklapp
-// Code cleanup:
-// Reformatting & tabs to spaces;
-// Added copyleft, getVersion, getDescription, rcs_id.
-//
 
 // (c-file-style: "gnu")
 // Local Variables:
