1bb17bd4ca441b669b17ba2b671a7f5e3963a9f4
vargenau <vargenau@96ab9672-09ca-45d6-a79d-3d69d39ca109>
2009-06-15 12:59:13 +0000

    Add fieldset and legend

--- lib/plugin/WatchPage.php
+++ lib/plugin/WatchPage.php
@@ -74,13 +74,13 @@ extends WikiPlugin
 
     function showNotify(&$request, $messages, $page, $pagelist, $verified) {
 	$isNecessary = ! $this->contains($pagelist, $page);
-        $form = HTML::form
-	    (array('action' => $request->getPostURL(),
-		   'method' => 'post'),
+        $form = HTML::fieldset(HTML::legend("Watch Page"), 
+                               HTML::form(array('action' => $request->getPostURL(),
+		                                'method' => 'post'),
 	     HiddenInputs(array('verify' => 1)),
 	     HiddenInputs($request->getArgs(),false,array('verify')),
 	     $messages,
-	     HTML::p(_("Your current watchlist: "), $this->showWatchList($pagelist)));
+	     HTML::p(_("Your current watchlist: "), $this->showWatchList($pagelist))));
 	if ($isNecessary) {
 	    $form->pushContent(HTML::p(_("New watchlist: "), 
 	                               $this->showWatchList($this->addpagelist($page, $pagelist))),
