02e61924b6f797a25e71307634d2cc86ce9dc6ad
vargenau <vargenau@96ab9672-09ca-45d6-a79d-3d69d39ca109>
2009-09-24 09:01:07 +0000

    New plugin to add video in wiki pages

---
+++ trunk/lib/plugin/Video.php
@@ -0,0 +1,118 @@
+<?php // -*-php-*-
+rcs_id('$Id$');
+/*
+ * Copyright 2009 Roger Guignard and Marc-Etienne Vargenau, Alcatel-Lucent
+ *
+ * This file is part of PhpWiki.
+ *
+ * PhpWiki is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * PhpWiki is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PhpWiki; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+/*
+ * Standard Alcatel-Lucent disclaimer for contributing to open source
+ *
+ * "The VideoPlugin ("Contribution") has not been tested and/or
+ * validated for release as or in products, combinations with products or
+ * other commercial use. Any use of the Contribution is entirely made at
+ * the user's own responsibility and the user can not rely on any features,
+ * functionalities or performances Alcatel-Lucent has attributed to the
+ * Contribution.
+ *
+ * THE CONTRIBUTION BY ALCATEL-LUCENT IS PROVIDED AS IS, WITHOUT WARRANTY
+ * OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ * WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, COMPLIANCE,
+ * NON-INTERFERENCE AND/OR INTERWORKING WITH THE SOFTWARE TO WHICH THE
+ * CONTRIBUTION HAS BEEN MADE, TITLE AND NON-INFRINGEMENT. IN NO EVENT SHALL
+ * ALCATEL-LUCENT BE LIABLE FOR ANY DAMAGES OR OTHER LIABLITY, WHETHER IN
+ * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * CONTRIBUTION OR THE USE OR OTHER DEALINGS IN THE CONTRIBUTION, WHETHER
+ * TOGETHER WITH THE SOFTWARE TO WHICH THE CONTRIBUTION RELATES OR ON A STAND
+ * ALONE BASIS."
+ */
+
+class WikiPlugin_Video
+extends WikiPlugin
+{
+    function getName() {
+        return _("Video");
+    }
+
+    function getDescription() {
+        return _("Display video in Flash");
+    }
+
+    function getVersion() {
+        return preg_replace("/[Revision: $]/", '',
+                            "\$Revision$");
+    }
+
+    function getDefaultArguments() {
+        return array('width'  => 460,
+                     'height' => 320,
+                     'url' => '',
+                     'file' => '',
+                     'autoplay' => 'false'
+                     );
+    }
+
+    function run($dbi, $argstr, &$request, $basepage) {
+
+        global $WikiTheme;
+        $args = $this->getArgs($argstr, $request);
+        extract($args);
+
+        $html = HTML();
+
+        $object = HTML::object(array('data' => SERVER_URL . $WikiTheme->_findData('flowplayer-3.1.3.swf'),
+                                     'type' => "application/x-shockwave-flash",
+                                     'width' => $width,
+                                     'height' => $height));
+        
+        $param = HTML::param(array('name' => "allowfullscreen",
+                                   'value' => "true"));
+        $object->pushContent($param);
+        
+        $param = HTML::param(array('name' => "allowscriptaccess",
+                                   'value' => "false"));
+        $object->pushContent($param);
+        
+        if (! $url && ! $file) {
+            return $this->error(_("Both 'url' or 'file' parameters missing."));
+        }
+        elseif ($url && $file) {
+            return $this->error(_("Choose only one of 'url' or 'page' parameters."));
+        }
+        elseif ($file) {
+            $url = SERVER_URL . getUploadDataPath() . '/' . $file;
+        }
+        $value = "config={'clip':{'url':'" . $url . "','autoPlay':" . $autoplay . "}}";
+        $param = HTML::param(array('name' => "flashvars",
+                                   'value' => $value));
+        $object->pushContent($param);
+
+        $html->pushContent($object);
+
+        return $html;
+    }
+};
+
+// Local Variables:
+// mode: php
+// tab-width: 4
+// c-basic-offset: 4
+// c-hanging-comment-ender-p: nil
+// indent-tabs-mode: nil
+// End:
+?>
