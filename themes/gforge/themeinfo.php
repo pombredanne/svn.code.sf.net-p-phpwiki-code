09cd58b19c90d33059465bd1b34f349f74de1ade
vargenau <vargenau@96ab9672-09ca-45d6-a79d-3d69d39ca109>
2009-10-19 12:57:39 +0000

    removed global code setters, switched to $WikiTheme->load() in main

--- themes/gforge/themeinfo.php
+++ themes/gforge/themeinfo.php
@@ -6,12 +6,12 @@ require_once('lib/WikiTheme.php');
 class WikiTheme_gforge extends WikiTheme {
 
     function header() {
-        global $HTML, $group_id, $group_public_name, $request, $project, $Language;
+        global $HTML, $group_id, $group_public_name, $request, $project;
 
         $pagename = $request->getArg('pagename');
 
         $submenu = Template('navbar');
-        
+
         //group is private
         if (!$project->isPublic()) {
             //if it's a private group, you must be a member of that group
@@ -40,86 +40,74 @@ class WikiTheme_gforge extends WikiTheme {
         	if (!$external_user) {
 	        	$page = $request->getPage();
 	        	if ($page->get('external')) {
-	    			$external_msg = 'This page is external.';
+	    			$external_msg = _("This page is external.");
 	    		}
-	    		echo $HTML->warning_msg($Language->getText('project_admin','external_project') .
+	    		echo $HTML->warning_msg(_("This project is shared with third-party users (non Alcatel-Lucent users).") .
 	    								(isset($external_msg) ? ' ' . $external_msg : ''));
 			}
         }
     }
-    
+
     function footer() {
         global $HTML;
-        
+
         $HTML->footer(array());
-        
+
+    }
+
+    function load() {
+
+        $this->initGlobals();
+
+        /**
+         * The Signature image is shown after saving an edited page. If this
+         * is set to false then the "Thank you for editing..." screen will
+         * be omitted.
+         */
+
+        $this->addImageAlias('signature', WIKI_NAME . "Signature.png");
+        // Uncomment this next line to disable the signature.
+        $this->addImageAlias('signature', false);
+
+        /*
+         * Link icons.
+         */
+        $this->setLinkIcon('http');
+        $this->setLinkIcon('https');
+        $this->setLinkIcon('ftp');
+        $this->setLinkIcon('mailto');
+
+        $this->setButtonSeparator("");
+
+        /**
+         * WikiWords can automatically be split by inserting spaces between
+         * the words. The default is to leave WordsSmashedTogetherLikeSo.
+         */
+        $this->setAutosplitWikiWords(false);
+
+        /**
+         * Layout improvement with dangling links for mostly closed wiki's:
+         * If false, only users with edit permissions will be presented the
+         * special wikiunknown class with "?" and Tooltip.
+         * If true (default), any user will see the ?, but will be presented
+         * the PrintLoginForm on a click.
+         */
+        $this->setAnonEditUnknownLinks(false);
+
+        /*
+         * You may adjust the formats used for formatting dates and times
+         * below.  (These examples give the default formats.)
+         * Formats are given as format strings to PHP strftime() function See
+         * http://www.php.net/manual/en/function.strftime.php for details.
+         * Do not include the server's zone (%Z), times are converted to the
+         * user's time zone.
+         */
+        $this->setDateFormat("%d %B %Y");
+        $this->setTimeFormat("%H:%M");
     }
 }
 
 $WikiTheme = new WikiTheme_gforge('gforge');
-// CSS file defines fonts, colors and background images for this style.
-
-/**
- * The logo image appears on every page and links to the HomePage.
- */
-$WikiTheme->addImageAlias('logo', WIKI_NAME . 'Logo.png');
-
-/**
- * The Signature image is shown after saving an edited page. If this
- * is set to false then the "Thank you for editing..." screen will
- * be omitted.
- */
-
-$WikiTheme->addImageAlias('signature', WIKI_NAME . "Signature.png");
-// Uncomment this next line to disable the signature.
-$WikiTheme->addImageAlias('signature', false);
-
-/*
- * Link icons.
- */
-// $WikiTheme->setLinkIconAttr('after');
-$WikiTheme->setLinkIcon('http');
-$WikiTheme->setLinkIcon('https');
-$WikiTheme->setLinkIcon('ftp');
-$WikiTheme->setLinkIcon('mailto');
-//$WikiTheme->setLinkIcon('interwiki');
-//$WikiTheme->setLinkIcon('wikiuser');
-//$WikiTheme->setLinkIcon('*', 'url');
-
-$WikiTheme->setButtonSeparator("\n | ");
-
-/**
- * WikiWords can automatically be split by inserting spaces between
- * the words. The default is to leave WordsSmashedTogetherLikeSo.
- */
-$WikiTheme->setAutosplitWikiWords(false);
-
-/**
- * Layout improvement with dangling links for mostly closed wiki's:
- * If false, only users with edit permissions will be presented the 
- * special wikiunknown class with "?" and Tooltip.
- * If true (default), any user will see the ?, but will be presented 
- * the PrintLoginForm on a click.
- */
-$WikiTheme->setAnonEditUnknownLinks(false);
-
-/*
- * You may adjust the formats used for formatting dates and times
- * below.  (These examples give the default formats.)
- * Formats are given as format strings to PHP strftime() function See
- * http://www.php.net/manual/en/function.strftime.php for details.
- * Do not include the server's zone (%Z), times are converted to the
- * user's time zone.
- */
-$WikiTheme->setDateFormat("%d %B %Y");
-$WikiTheme->setTimeFormat("%H:%M");
-
-/*
- * To suppress times in the "Last edited on" messages, give a
- * give a second argument of false:
- */
-//$WikiTheme->setDateFormat("%B %d, %Y", false); 
-
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 // (c-file-style: "gnu")
@@ -129,5 +117,5 @@ $WikiTheme->setTimeFormat("%H:%M");
 // c-basic-offset: 4
 // c-hanging-comment-ender-p: nil
 // indent-tabs-mode: nil
-// End:   
+// End:
 ?>
