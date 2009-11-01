c0cca06634d809f6b9f392b5ceee209f0a83b5b9
vargenau <vargenau@96ab9672-09ca-45d6-a79d-3d69d39ca109>
2009-11-01 17:08:51 +0000

    Normalize header

--- lib/plugin/CacheTest.php
+++ lib/plugin/CacheTest.php
@@ -1,5 +1,25 @@
 <?php // -*-php-*-
 rcs_id('$Id$');
+/*
+ * Copyright (C) 2002 Johannes Große
+ * 
+ * This file is part of PhpWiki.
+ *
+ * PhpWiki is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * PhpWiki is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PhpWiki; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
 // +---------------------------------------------------------------------+
 // | CacheTest.php                                                       |
 // +---------------------------------------------------------------------+
@@ -7,7 +27,6 @@ rcs_id('$Id$');
 // | text to image conversion.                                           |
 // | This is a usage example of WikiPluginCached.                        |
 // |                                                                     |
-// | Copyright (C) 2002 Johannes Große (Johannes Gro&szlig;e)            |
 // | You may copy this code freely under the conditions of the GPL       |
 // +---------------------------------------------------------------------+
 
