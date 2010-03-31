ea05a3f98c66a7aafec68fa54e9377d99a7a5ffe
vargenau <vargenau@96ab9672-09ca-45d6-a79d-3d69d39ca109>
2010-03-31 11:59:04 +0000

    define('PHPWIKI_VERSION', '1.4.0RC-20100431');

--- trunk/lib/prepend.php
+++ trunk/lib/prepend.php
@@ -12,7 +12,7 @@ function rcs_id ($id) {
 rcs_id('$Id$');
 
 // see lib/stdlib.php: phpwiki_version()
-define('PHPWIKI_VERSION', '1.3.14-20090604');
+define('PHPWIKI_VERSION', '1.4.0RC-20100431');
 
 /** 
  * Returns true if current php version is at mimimum a.b.c 
