ec73c96030aba3e7f5cacaf438def1990c030156
vargenau <vargenau@96ab9672-09ca-45d6-a79d-3d69d39ca109>
2012-11-20 09:06:44 +0000

    Update to PEAR Cache-1.5.6

--- trunk/lib/pear/Cache/Error.php
+++ trunk/lib/pear/Cache/Error.php
@@ -14,34 +14,39 @@
 // +----------------------------------------------------------------------+
 // | Authors: Ulf Wendel <ulf.wendel@phpdoc.de>                           |
 // +----------------------------------------------------------------------+
+//
+// $Id: Error.php 174777 2004-12-15 09:09:33Z dufuz $
 
 require_once 'PEAR.php';
 
 /**
 * Cache Error class
-*
+* 
 * @package Cache
 */
-class Cache_Error extends PEAR_Error {
+class Cache_Error extends PEAR_Error
+{
+
 
   /**
   * Prefix of all error messages.
-  *
+  * 
   * @var  string
   */
   var $error_message_prefix = 'Cache-Error: ';
-
+  
   /**
   * Creates an cache error object.
-  *
+  * 
   * @param  string  error message
   * @param  string  file where the error occured
   * @param  string  linenumber where the error occured
   */
   function Cache_Error($msg, $file = __FILE__, $line = __LINE__) {
-
+    
     $this->PEAR_Error(sprintf("%s [%s on line %d].", $msg, $file, $line));
-
+    
   } // end func Cache_Error
-
+  
 } // end class Cache_Error
+?>
