370c5c27d98ecb3f8251dffa49321da7e04be4cb
vargenau <vargenau@96ab9672-09ca-45d6-a79d-3d69d39ca109>
2012-12-05 19:01:32 +0000

    (defined('FUSIONFORGE') and FUSIONFORGE)

--- lib/loadsave.php
+++ lib/loadsave.php
@@ -1527,7 +1527,7 @@ function SetupWiki(&$request)
     // Ensure that all mandatory pages are loaded
     $finder = new FileFinder;
 
-    if (!FUSIONFORGE) {
+    if (!(defined('FUSIONFORGE') and FUSIONFORGE)) {
         $mandatory = explode(':', 'SandBox:Template/Category:Template/Talk:SpecialPages:CategoryCategory:CategoryActionPage:Help/OldTextFormattingRules:Help/TextFormattingRules:PhpWikiAdministration');
     } elseif (WIKI_NAME == "help") {
         $mandatory = explode(':', 'SandBox:Template/Category:Template/Talk:SpecialPages:CategoryCategory:CategoryActionPage:Help/TextFormattingRules:PhpWikiAdministration');
