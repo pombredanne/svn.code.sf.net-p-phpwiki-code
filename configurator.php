ba51ec7507c7c8ab2ca1f35fe61fb8689cc2564e
vargenau <vargenau@96ab9672-09ca-45d6-a79d-3d69d39ca109>
2013-04-02 10:14:58 +0000

    utf-8 --> UTF-8

--- configurator.php
+++ configurator.php
@@ -165,7 +165,7 @@ echo '<', '?xml version="1.0" encoding="utf-8"?', ">\n";
         "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
     <title>Configuration tool for PhpWiki <?php echo $config_file ?></title>
     <style type="text/css" media="screen">
         <!--
