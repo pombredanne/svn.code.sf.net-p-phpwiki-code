7ea37de8569c85afa1afaceb3c2fd1104e3688c6
vargenau <vargenau@96ab9672-09ca-45d6-a79d-3d69d39ca109>
2013-04-02 15:49:52 +0000

    Use HTML 5 DOCTYPE

--- trunk/passencrypt.php
+++ trunk/passencrypt.php
@@ -1,9 +1,7 @@
-<?php echo "<?xml version=\"1.0\" encoding=\"iso-8859-1\"?>\n"; ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html>
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1"/>
+    <meta charset="UTF-8" />
     <title>Password Encryption Tool</title>
     <!--
     Copyright 1999, 2000, 2001, 2002 $ThePhpWikiProgrammingTeam
