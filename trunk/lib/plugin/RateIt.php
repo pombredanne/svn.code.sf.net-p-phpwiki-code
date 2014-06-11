9be3eb64322fd819e395624143179db40e96651d
vargenau <vargenau@96ab9672-09ca-45d6-a79d-3d69d39ca109>
2014-06-11 16:21:02 +0000

    Update PHP Doc

--- trunk/lib/plugin/RateIt.php
+++ trunk/lib/plugin/RateIt.php
@@ -309,13 +309,13 @@ var msg_rating_deleted = '" . _("Rating deleted!") . "';
      *
      * This needs to be put in the <body> section of the page.
      *
-     * @param $pagename    Name of the page to rate
-     * @param $version     Version of the page to rate (may be "" for current)
-     * @param $imgPrefix   Prefix of the names of the images that display the rating
+     * @param string $pagename    Name of the page to rate
+     * @param int $version     Version of the page to rate (may be "" for current)
+     * @param string $imgPrefix   Prefix of the names of the images that display the rating
      *                    You can have two widgets for the same page displayed at
      *                    once iff the imgPrefix-s are different.
-     * @param $dimension   Id of the dimension to rate
-     * @param $small       Makes a smaller ratings widget if non-false
+     * @param int $dimension   Id of the dimension to rate
+     * @param bool $small       Makes a smaller ratings widget if non-false
      *
      * Limitations: Currently this can only print the current users ratings.
      *              And only the widget, but no value (for buddies) also.
