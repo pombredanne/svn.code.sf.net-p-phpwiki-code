ee4dc8f505bc1beca6549eff6cac66ce555c0347
vargenau <vargenau@96ab9672-09ca-45d6-a79d-3d69d39ca109>
2014-07-25 13:13:46 +0000

    Create branch release-1.4.0 from r7695

---
+++ branches/release-1.4.0/schemas/sqlite-destroy.sql
@@ -0,0 +1,14 @@
+-- http://www.hezmatt.org/~mpalmer/sqlite-phpwiki/sqlite.sql
+
+-- $Id$
+
+DROP TABLE page;
+DROP TABLE version;
+DROP TABLE recent;
+DROP TABLE nonempty;
+DROP TABLE link;
+DROP TABLE session;
+DROP TABLE pref;
+DROP TABLE member;
+DROP TABLE rating;
+DROP TABLE accesslog;
