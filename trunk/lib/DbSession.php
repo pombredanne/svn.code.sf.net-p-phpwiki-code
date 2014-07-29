5af8c25868895303619e83ed4a29156bb14824c2
vargenau <vargenau@96ab9672-09ca-45d6-a79d-3d69d39ca109>
2014-07-29 09:00:38 +0000

    Create tag release-1.5.0

---
+++ trunk/lib/DbSession.php
@@ -0,0 +1,68 @@
+<?php
+
+/**
+ * Store sessions data in Pear DB / ADODB / dba / PDO, ....
+ *
+ * History
+ *
+ * Originally by Stanislav Shramko <stanis@movingmail.com>
+ * Minor rewrite by Reini Urban <rurban@x-ray.at> for Phpwiki.
+ * Quasi-major rewrite/decruft/fix by Jeff Dairiki <dairiki@dairiki.org>.
+ * ADODB, dba and PDO classes by Reini Urban.
+ *
+ * Warning: Enable USE_SAFE_DBSESSION if you get INSERT duplicate id warnings.
+ */
+class DbSession
+{
+    /**
+     * @param mixed $dbh
+     * DB handle, or WikiDB object (from which the DB handle will
+     * be extracted.
+     *
+     * @param string $table
+     * @return bool|DbSession
+     * Name of SQL table containing session data.
+     */
+    function __construct(&$dbh, $table = 'session')
+    {
+        // Check for existing DbSession handler
+        $db_type = $dbh->getParam('dbtype');
+        if (isa($dbh, 'WikiDB')) {
+            @include_once("lib/DbSession/" . $db_type . ".php");
+
+            $class = "DbSession_" . $db_type;
+            if (class_exists($class)) {
+                // dba has no ->_dbh, so this is used for the session link
+                $this->_backend = new $class($dbh->_backend->_dbh, $table);
+                return;
+            }
+        }
+        //Fixme: E_USER_WARNING ignored!
+        trigger_error(sprintf(_("Your WikiDB DB backend “%s” cannot be used for DbSession.") . " " .
+                _("Set USE_DB_SESSION to false."),
+            $db_type), E_USER_WARNING);
+    }
+
+    function currentSessions()
+    {
+        return $this->_backend->currentSessions();
+    }
+
+    function query($sql)
+    {
+        return $this->_backend->query($sql);
+    }
+
+    function quote($string)
+    {
+        return $string;
+    }
+}
+
+// Local Variables:
+// mode: php
+// tab-width: 8
+// c-basic-offset: 4
+// c-hanging-comment-ender-p: nil
+// indent-tabs-mode: nil
+// End:
