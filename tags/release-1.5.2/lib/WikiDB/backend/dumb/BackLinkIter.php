1cb1163b8289f23909ebceefd97d63ce4f2b28a6
vargenau <vargenau@96ab9672-09ca-45d6-a79d-3d69d39ca109>
2014-10-10 14:41:06 +0000

    Create tag release-1.5.2

---
+++ tags/release-1.5.2/lib/WikiDB/backend/dumb/BackLinkIter.php
@@ -0,0 +1,47 @@
+<?php
+
+require_once 'lib/WikiDB/backend.php';
+
+/**
+ * This backlink iterator will work with any WikiDB_backend
+ * which has a working get_links(,'links_from') method.
+ *
+ * This is mostly here for testing, 'cause it's slow,slow,slow.
+ */
+class WikiDB_backend_dumb_BackLinkIter
+    extends WikiDB_backend_iterator
+{
+    function __construct(&$backend, &$all_pages, $pagename)
+    {
+        $this->_pages = $all_pages;
+        $this->_backend = &$backend;
+        $this->_target = $pagename;
+    }
+
+    function next()
+    {
+        while ($page = $this->_pages->next()) {
+            $pagename = $page['pagename'];
+            $links = $this->_backend->get_links($pagename, false);
+            while ($link = $links->next()) {
+                if ($link['pagename'] == $this->_target) {
+                    $links->free();
+                    return $page;
+                }
+            }
+        }
+    }
+
+    function free()
+    {
+        $this->_pages->free();
+    }
+}
+
+// Local Variables:
+// mode: php
+// tab-width: 8
+// c-basic-offset: 4
+// c-hanging-comment-ender-p: nil
+// indent-tabs-mode: nil
+// End:
