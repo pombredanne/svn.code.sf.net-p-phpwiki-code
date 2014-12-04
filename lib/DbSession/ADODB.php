2570c2962a42369e4f274dca7da859cf42ace88b
vargenau <vargenau@96ab9672-09ca-45d6-a79d-3d69d39ca109>
2014-12-04 16:12:50 +0000

    Harmonize lib/DbSession/*.php

--- lib/DbSession/ADODB.php
+++ lib/DbSession/ADODB.php
@@ -64,6 +64,7 @@ class DbSession_ADODB
         return $this->_dbh->Execute($sql);
     }
 
+    // adds surrounding quotes
     function quote($string)
     {
         return $this->_dbh->qstr($string);
@@ -124,11 +125,12 @@ class DbSession_ADODB
         if (!empty($res) and preg_match('|^[a-zA-Z0-9/+=]+$|', $res))
             $res = base64_decode($res);
         if (strlen($res) > 4000) {
-            trigger_error("Overlarge session data! " . strlen($res) .
-                " gt. 4000", E_USER_WARNING);
+            // trigger_error("Overlarge session data! ".strlen($res). " gt. 4000", E_USER_WARNING);
             $res = preg_replace('/s:6:"_cache";O:12:"WikiDB_cache".+}$/', "", $res);
             $res = preg_replace('/s:12:"_cached_html";s:.+",s:4:"hits"/', 's:4:"hits"', $res);
-            if (strlen($res) > 4000) $res = '';
+            if (strlen($res) > 4000) {
+                $res = '';
+            }
         }
         return $res;
     }
@@ -232,7 +234,7 @@ class DbSession_ADODB
         return true;
     }
 
-    // WhoIsOnline support.
+    // WhoIsOnline support
     // TODO: ip-accesstime dynamic blocking API
     function currentSessions()
     {
