62e001d297afb0e48524ef4876e1f9a72a58e512
vargenau <vargenau@96ab9672-09ca-45d6-a79d-3d69d39ca109>
2015-02-05 10:19:04 +0000

    Include top-level files in translations

--- locale/update-makefile.sh
+++ locale/update-makefile.sh
@@ -37,7 +37,7 @@ po_files () {
 # find ../lib fails on cygwin
 pot_file_deps () {
     test -f .exclude || ( echo lib/pear/ > .exclude; echo lib/WikiDB/adodb/ > .exclude )
-    (cd ..; find lib themes \( -type d -regex '\(^lib/pear\)\|\(^lib/WikiDB/adodb\)' \) -prune -o \( -type f -a -name \*.php -o -name \*.tmpl \)) |
+    (cd ..; find . \( -type d -regex '\(^lib/pear\)\|\(^lib/WikiDB/adodb\)' \) -prune -o \( -type f -a -name \*.php -o -name \*.tmpl \)) |
         egrep -v '(^lib/pear)|(^lib/WikiDB/adodb)' |
         grep -v -f .exclude |
 	sed 's|^|${POT_FILE}: ../|;' |
