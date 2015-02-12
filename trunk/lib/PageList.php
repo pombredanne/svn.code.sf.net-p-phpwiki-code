0ae43f51cbd7230beab58c69b51876e4d3f6dcd2
vargenau <vargenau@96ab9672-09ca-45d6-a79d-3d69d39ca109>
2015-02-12 09:56:50 +0000

    Remove &middot

--- trunk/lib/PageList.php
+++ trunk/lib/PageList.php
@@ -1835,7 +1835,6 @@ class PageList
 
     // comma=1
     // Condense list without a href links: "Page1, Page2, ..."
-    // Alternative $seperator = HTML::raw(' &middot; ')
     // FIXME: only unique list entries, esp. with nopage
     private function generateCommaListAsString()
     {
