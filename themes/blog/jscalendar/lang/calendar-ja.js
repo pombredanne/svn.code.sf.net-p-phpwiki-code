52f2954ccb75f49911f7708e8049015d7674ebd1
vargenau <vargenau@96ab9672-09ca-45d6-a79d-3d69d39ca109>
2015-04-16 15:24:15 +0000

    jscalendar: add Calendar._FD

--- themes/blog/jscalendar/lang/calendar-ja.js
+++ themes/blog/jscalendar/lang/calendar-ja.js
@@ -18,6 +18,12 @@ Calendar._DN = new Array
  "金",
  "土",
  "日");
+
+// First day of the week. "0" means display Sunday first, "1" means display
+// Monday first, etc.
+Calendar._FD = 0;
+
+// full month names
 Calendar._MN = new Array
 ("1月",
  "2月",
@@ -34,6 +40,24 @@ Calendar._MN = new Array
 
 // tooltips
 Calendar._TT = {};
+Calendar._TT["INFO"] = "About the calendar";
+
+Calendar._TT["ABOUT"] =
+"DHTML Date/Time Selector\n" +
+"(c) dynarch.com 2002-2005 / Author: Mihai Bazon\n" + // don't translate this ;-)
+"For latest version visit: http://www.dynarch.com/projects/calendar/\n" +
+"Distributed under GNU LGPL.  See http://gnu.org/licenses/lgpl.html for details." +
+"\n\n" +
+"Date selection:\n" +
+"- Use the «, » buttons to select year\n" +
+"- Use the ‹, › buttons to select month\n" +
+"- Hold mouse button on any of the above buttons for faster selection.";
+Calendar._TT["ABOUT_TIME"] = "\n\n" +
+"Time selection:\n" +
+"- Click on any of the time parts to increase it\n" +
+"- or Shift-click to decrease it\n" +
+"- or click and drag for faster selection.";
+
 Calendar._TT["PREV_YEAR"] = "前年";
 Calendar._TT["PREV_MONTH"] = "前月";
 Calendar._TT["GO_TODAY"] = "今日";
@@ -42,13 +66,23 @@ Calendar._TT["NEXT_YEAR"] = "翌年";
 Calendar._TT["SEL_DATE"] = "日付選択";
 Calendar._TT["DRAG_TO_MOVE"] = "ウィンドウの移動";
 Calendar._TT["PART_TODAY"] = " (今日)";
-Calendar._TT["MON_FIRST"] = "月曜日を先頭に";
-Calendar._TT["SUN_FIRST"] = "日曜日を先頭に";
+
+// the following is to inform that "%s" is to be the first day of week
+// %s will be replaced with the day name.
+Calendar._TT["DAY_FIRST"] = "Display %s first";
+
+// This may be locale-dependent.  It specifies the week-end days, as an array
+// of comma-separated numbers.  The numbers are from 0 to 6: 0 means Sunday, 1
+// means Monday, etc.
+Calendar._TT["WEEKEND"] = "0,6";
+
 Calendar._TT["CLOSE"] = "閉じる";
 Calendar._TT["TODAY"] = "今日";
+Calendar._TT["TIME_PART"] = "(Shift-)Click or drag to change value";
 
 // date formats
 Calendar._TT["DEF_DATE_FORMAT"] = "y-mm-dd";
 Calendar._TT["TT_DATE_FORMAT"] = "%m月 %d日 (%a)";
 
 Calendar._TT["WK"] = "週";
+Calendar._TT["TIME"] = "Time:";
