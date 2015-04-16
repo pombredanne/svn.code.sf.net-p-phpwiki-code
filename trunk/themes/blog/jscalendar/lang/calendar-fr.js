0beb50f4e32e2744dc0d8891260d03cad575a7ce
vargenau <vargenau@96ab9672-09ca-45d6-a79d-3d69d39ca109>
2015-04-16 14:53:54 +0000

    Improve French translation

--- trunk/themes/blog/jscalendar/lang/calendar-fr.js
+++ trunk/themes/blog/jscalendar/lang/calendar-fr.js
@@ -1,14 +1,12 @@
 // ** I18N
 
-// Calendar EN language
-// Author: Mihai Bazon, <mihai_bazon@yahoo.com>
+// Calendar FR language
+// Author: David Duret, <pilgrim@mala-template.net>
 // Distributed under the same terms as the calendar itself.
 
 // For translators: please use UTF-8 if possible.  We strongly believe that
 // Unicode is the answer to a real internationalized world.  Also please
 // include your contact information in the header, as can be seen above.
-
-// Translator: David Duret, <pilgrim@mala-template.net> from previous french version
 
 // full day names
 Calendar._DN = new Array
@@ -44,6 +42,10 @@ Calendar._SDN = new Array
  "Sam",
  "Dim");
 
+// First day of the week. "0" means display Sunday first, "1" means display
+// Monday first, etc.
+Calendar._FD = 0;
+
 // full month names
 Calendar._MN = new Array
 ("Janvier",
@@ -68,7 +70,7 @@ Calendar._SMN = new Array
  "Mai",
  "Juin",
  "Juil",
- "Aout",
+ "Août",
  "Sep",
  "Oct",
  "Nov",
@@ -80,19 +82,19 @@ Calendar._TT["INFO"] = "A propos du calendrier";
 
 Calendar._TT["ABOUT"] =
 "DHTML Date/Heure Selecteur\n" +
-"(c) dynarch.com 2002-2005 / Author: Mihai Bazon\n" + // don't translate this this ;-)
+"(c) dynarch.com 2002-2005 / Author: Mihai Bazon\n" + // don't translate this ;-)
 "Pour la dernière version visitez : http://www.dynarch.com/projects/calendar/\n" +
 "Distribué par GNU LGPL.  Voir http://gnu.org/licenses/lgpl.html pour les détails." +
 "\n\n" +
 "Selection de la date :\n" +
 "- Utiliser les boutons «, » pour sélectionner l\'année\n" +
 "- Utiliser les boutons ‹, › pour selectionner les mois\n" +
-"- Garder la souris sur n'importe quel bouton pour une sélection plus rapide";
+"- Garder la souris sur n'importe quel bouton pour une sélection plus rapide.";
 Calendar._TT["ABOUT_TIME"] = "\n\n" +
 "Selection de l\'heure :\n" +
 "- Cliquer sur heures ou minutes pour incrémenter\n" +
 "- ou Maj-clic pour décrémenter\n" +
-"- ou clic et glisser-déplacer pour une sélection plus rapide";
+"- ou clic et glisser-déplacer pour une sélection plus rapide.";
 
 Calendar._TT["PREV_YEAR"] = "Année préc. (maintenir pour menu)";
 Calendar._TT["PREV_MONTH"] = "Mois préc. (maintenir pour menu)";
