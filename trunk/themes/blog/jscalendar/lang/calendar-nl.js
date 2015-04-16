9c2002436d9a73a856f18d856d7626e93f4d20bd
vargenau <vargenau@96ab9672-09ca-45d6-a79d-3d69d39ca109>
2015-04-16 15:03:01 +0000

    jscalendar; add Calendar._FD

--- trunk/themes/blog/jscalendar/lang/calendar-nl.js
+++ trunk/themes/blog/jscalendar/lang/calendar-nl.js
@@ -1,4 +1,14 @@
 // ** I18N
+
+// Calendar NL language
+// Author: Mihai Bazon, <mihai_bazon@yahoo.com>
+// Distributed under the same terms as the calendar itself.
+
+// For translators: please use UTF-8 if possible.  We strongly believe that
+// Unicode is the answer to a real internationalized world.  Also please
+// include your contact information in the header, as can be seen above.
+
+// full day names
 Calendar._DN = new Array
 ("Zondag",
  "Maandag",
@@ -10,7 +20,11 @@ Calendar._DN = new Array
  "Zondag");
 
 Calendar._SDN_len = 2;
+// First day of the week. "0" means display Sunday first, "1" means display
+// Monday first, etc.
+Calendar._FD = 0;
 
+// full month names
 Calendar._MN = new Array
 ("Januari",
  "Februari",
@@ -31,7 +45,7 @@ Calendar._TT["INFO"] = "Info";
 
 Calendar._TT["ABOUT"] =
 "DHTML Datum/Tijd Selector\n" +
-"(c) dynarch.com 2002-2005 / Author: Mihai Bazon\n" +
+"(c) dynarch.com 2002-2005 / Author: Mihai Bazon\n" + // don't translate this ;-)
 "Ga voor de meest recente versie naar: http://www.dynarch.com/projects/calendar/\n" +
 "Verspreid onder de GNU LGPL. Zie http://gnu.org/licenses/lgpl.html voor details." +
 "\n\n" +
@@ -45,7 +59,6 @@ Calendar._TT["ABOUT_TIME"] = "\n\n" +
 "- of Shift-klik om het te verlagen\n" +
 "- of klik en sleep voor een snellere selectie.";
 
-//Calendar._TT["TOGGLE"] = "Selecteer de eerste week-dag";
 Calendar._TT["PREV_YEAR"] = "Vorig jaar (ingedrukt voor menu)";
 Calendar._TT["PREV_MONTH"] = "Vorige maand (ingedrukt voor menu)";
 Calendar._TT["GO_TODAY"] = "Ga naar Vandaag";
@@ -54,11 +67,14 @@ Calendar._TT["NEXT_YEAR"] = "Volgend jaar (ingedrukt voor menu)";
 Calendar._TT["SEL_DATE"] = "Selecteer datum";
 Calendar._TT["DRAG_TO_MOVE"] = "Klik en sleep om te verplaatsen";
 Calendar._TT["PART_TODAY"] = " (vandaag)";
-//Calendar._TT["MON_FIRST"] = "Toon Maandag eerst";
-//Calendar._TT["SUN_FIRST"] = "Toon Zondag eerst";
 
+// the following is to inform that "%s" is to be the first day of week
+// %s will be replaced with the day name.
 Calendar._TT["DAY_FIRST"] = "Toon %s eerst";
 
+// This may be locale-dependent.  It specifies the week-end days, as an array
+// of comma-separated numbers.  The numbers are from 0 to 6: 0 means Sunday, 1
+// means Monday, etc.
 Calendar._TT["WEEKEND"] = "0,6";
 
 Calendar._TT["CLOSE"] = "Sluiten";
