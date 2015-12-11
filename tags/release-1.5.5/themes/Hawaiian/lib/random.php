fb9970df74befb8ea412d880f2bc2a02cf34c961
vargenau <vargenau@96ab9672-09ca-45d6-a79d-3d69d39ca109>
2015-12-11 10:00:51 +0000

    Create tag release-1.5.5

---
+++ tags/release-1.5.5/themes/Hawaiian/lib/random.php
@@ -0,0 +1,32 @@
+<?php
+class randomImage
+{
+    /**
+     * Usage:
+     *
+     * $imgSet = new randomImage($WikiTheme->file("images/pictures"));
+     * $imgFile = "pictures/" . $imgSet->filename;
+     */
+    function randomImage($dirname)
+    {
+
+        $this->filename = ""; // Pick up your filename here.
+
+        $_imageSet = new imageSet($dirname);
+        $this->imageList = $_imageSet->getFiles();
+        unset($_imageSet);
+
+        if (empty($this->imageList)) {
+            trigger_error(sprintf(_("%s is empty."), $dirname),
+                E_USER_NOTICE);
+        } else {
+            $dummy = $this->pickRandom();
+        }
+    }
+
+    function pickRandom()
+    {
+        $this->filename = $this->imageList[array_rand($this->imageList)];
+        return $this->filename;
+    }
+}
