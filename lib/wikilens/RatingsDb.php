91cac2bea4ead507052040a72646fb1aceea3917
vargenau <vargenau@96ab9672-09ca-45d6-a79d-3d69d39ca109>
2016-10-06 17:07:22 +0000

    Add standard file header

--- lib/wikilens/RatingsDb.php
+++ lib/wikilens/RatingsDb.php
@@ -1,4 +1,27 @@
 <?php
+/**
+ * Copyright © 2004,2007 Reini Urban
+ * Copyright © 2010 $ThePhpWikiProgrammingTeam
+ *
+ * This file is part of PhpWiki.
+ *
+ * PhpWiki is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * PhpWiki is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with PhpWiki; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * SPDX-License-Identifier: GPL-2.0+
+ *
+ */
 
 /*
  * @author:  Dan Frankowski (wikilens group manager), Reini Urban (as plugin)
@@ -9,6 +32,7 @@
  * - finish mysuggest.c (external engine with data from mysql)
  * - add the various show modes (esp. TopN queries in PHP)
  */
+
 /*
  CREATE TABLE rating (
         dimension INT(4) NOT NULL,
