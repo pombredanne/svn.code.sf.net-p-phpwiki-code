b1309fd70c50c23ef2bb16cb5e0494dba847d308
vargenau <vargenau@96ab9672-09ca-45d6-a79d-3d69d39ca109>
2016-10-07 13:27:15 +0000

    Add standard file header

--- lib/WikiDB/backend/dumb/WantedPagesIter.php
+++ lib/WikiDB/backend/dumb/WantedPagesIter.php
@@ -1,10 +1,34 @@
 <?php
 /**
+ * Copyright © 2004,2007 Reini Urban
+ *
+ * This file is part of PhpWiki.
+ *
+ * PhpWiki is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * PhpWiki is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with PhpWiki; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * SPDX-License-Identifier: GPL-2.0+
+ *
+ */
+
+/**
  * This iterator will work with any WikiDB_backend
  * which has a working get_links(,'links_from') method.
  *
  * This is mostly here for testing, 'cause it's slow,slow,slow.
  */
+
 class WikiDB_backend_dumb_WantedPagesIter
     extends WikiDB_backend_iterator
 {
