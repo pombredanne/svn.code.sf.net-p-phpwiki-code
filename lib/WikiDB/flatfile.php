85c14ab42eb61431f884ca3b94d989b022059365
vargenau <vargenau@96ab9672-09ca-45d6-a79d-3d69d39ca109>
2016-10-07 16:33:46 +0000

    Add SPDX-License-Identifier

--- lib/WikiDB/flatfile.php
+++ lib/WikiDB/flatfile.php
@@ -1,7 +1,6 @@
 <?php
-
 /**
- * Copyright 1999, 2005 $ThePhpWikiProgrammingTeam
+ * Copyright © 1999, 2005 $ThePhpWikiProgrammingTeam
  *
  * This file is part of PhpWiki.
  *
@@ -18,6 +17,9 @@
  * You should have received a copy of the GNU General Public License along
  * with PhpWiki; if not, write to the Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * SPDX-License-Identifier: GPL-2.0+
+ *
  */
 
 require_once 'lib/WikiDB.php';
@@ -28,6 +30,7 @@ require_once 'lib/WikiDB/backend/flatfile.php';
  * flatfile has readable (mimified) page_data files, the rest is the
  * same as in the file backend (serialized arrays).
  */
+
 class WikiDB_flatfile extends WikiDB
 {
     function __construct($dbparams)
