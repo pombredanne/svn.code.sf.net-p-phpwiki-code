afb1cbb679bb3a4d5b5ff5e9d34aa721db2aa2bc
vargenau <vargenau@96ab9672-09ca-45d6-a79d-3d69d39ca109>
2016-10-07 12:33:31 +0000

    Add standard file header

--- themes/blog/lib/RecentChanges.php
+++ themes/blog/lib/RecentChanges.php
@@ -1,10 +1,32 @@
 <?php
+/**
+ * Copyright © 2004-2005 Reini Urban
+ *
+ * This file is part of PhpWiki.
+ *
+ * PhpWiki is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * PhpWiki is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with PhpWiki; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * SPDX-License-Identifier: GPL-2.0+
+ *
+ */
+
 /*
  * Extensions/modifications to the stock RecentChanges (and PageHistory) format.
  */
 
 require_once 'lib/plugin/RecentChanges.php';
-//require_once('lib/plugin/PageHistory.php');
 
 class _blog_RecentChanges_BoxFormatter
     extends _RecentChanges_BoxFormatter
