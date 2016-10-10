0df40e535f969a343dd928813049cba41ff076ba
vargenau <vargenau@96ab9672-09ca-45d6-a79d-3d69d39ca109>
2016-10-10 13:05:42 +0000

    No space at EOL

--- passencrypt.php
+++ passencrypt.php
@@ -21,9 +21,9 @@
     You should have received a copy of the GNU General Public License along
     with PhpWiki; if not, write to the Free Software Foundation, Inc.,
     51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-  
+
     SPDX-License-Identifier: GPL-2.0+
-  
+
     -->
 </head>
 <body>
