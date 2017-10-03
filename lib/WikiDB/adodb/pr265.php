b0ceadc8e2286a5e2a0db4390860b471cccec930
vargenau <vargenau@96ab9672-09ca-45d6-a79d-3d69d39ca109>
2017-10-03 16:33:54 +0000

    Update Adodb to version 5.20.9

--- lib/WikiDB/adodb/pr265.php
+++
@@ -1,9 +0,0 @@
-<?php
-include('adodb.inc.php');
-include('adodb-xmlschema03.inc.php');
-
-$db = ADONewConnection('mysqli');
-$db->connect('localhost', 'root', 'C0yote71', 'apecove');
-$xml = new adoSchema($db);
-print $xml->extractSchema();
-
