738ad48977bb65ca6dde9d777687418110bbed1c
vargenau <vargenau@96ab9672-09ca-45d6-a79d-3d69d39ca109>
2020-12-04 16:59:21 +0000

    replace "GPL-2.0+" by "GPL-2.0-or-later" (deprecated since SPDX 2.0

--- trunk/lib/plugin/RawHtml.php
+++ trunk/lib/plugin/RawHtml.php
@@ -18,7 +18,7 @@
  * with PhpWiki; if not, write to the Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  *
- * SPDX-License-Identifier: GPL-2.0+
+ * SPDX-License-Identifier: GPL-2.0-or-later
  *
  */
 
