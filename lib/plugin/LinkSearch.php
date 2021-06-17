53e7df7ab3531aae679e3c2c686a934ce8f0fca5
vargenau <vargenau@96ab9672-09ca-45d6-a79d-3d69d39ca109>
2021-06-17 13:29:29 +0000

    method get in lower case

--- lib/plugin/LinkSearch.php
+++ lib/plugin/LinkSearch.php
@@ -113,7 +113,7 @@ function dirsign_switch() {
         $submit = Button('submit:search', _("LinkSearch"), false);
         $instructions = _("Search in pages for links with the matching name.");
         $form = HTML::form(array('action' => $action,
-                'method' => 'GET',
+                'method' => 'get',
                 'accept-charset' => 'UTF-8'),
             $dirsign_switch,
             $hiddenfield,
