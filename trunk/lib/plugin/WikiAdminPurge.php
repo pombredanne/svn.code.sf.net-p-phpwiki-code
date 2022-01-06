cba319ca452862c30151786f4ec4d0fd96768eb3
vargenau <vargenau@96ab9672-09ca-45d6-a79d-3d69d39ca109>
2022-01-06 09:18:03 +0000

    lib/plugin/WikiAdminPurge.php: do not display checkbox when confirming

--- trunk/lib/plugin/WikiAdminPurge.php
+++ trunk/lib/plugin/WikiAdminPurge.php
@@ -108,9 +108,10 @@ class WikiPlugin_WikiAdminPurge
         $this->preSelectS($args, $request);
 
         $p = $request->getArg('p');
-        if (!$p) $p = $this->_list;
+        if (!$p) {
+            $p = $this->_list;
+        }
         $post_args = $request->getArg('admin_purge');
-
         $next_action = 'select';
         $pages = array();
         if ($p && $request->isPost() &&
@@ -142,21 +143,27 @@ class WikiPlugin_WikiAdminPurge
         }
         if ($next_action == 'select') {
             // List all pages to select from.
-            $pages = $this->collectPages($pages, $dbi, $args['sortby'], $args['limit'], $args['exclude']);
+            $pages = $this->collectPages($pages, $dbi, $args['sortby'],
+                                         $args['limit'], $args['exclude']);
         }
 
         $header = HTML::fieldset();
-        $pagelist = new PageList_Selectable($args['info'], $args['exclude'],
-            array('types' =>
-            array('purge'
-            => new PageList_Column_purge('purge', _("Purge")))));
-        $pagelist->addPageList($pages);
         if ($next_action == 'verify') {
+            $pagelist = new PageList_Unselectable($args['info'], $args['exclude'],
+                array('types' =>
+                array('purge'
+                => new PageList_Column_purge('purge', _("Purge")))));
+            $pagelist->addPageList($pages);
             $button_label = _("Yes");
             $header->pushContent(HTML::legend(_("Confirm purge")));
             $header->pushContent(HTML::p(HTML::strong(
                     _("Are you sure you want to permanently purge the following pages?"))));
         } else {
+            $pagelist = new PageList_Selectable($args['info'], $args['exclude'],
+                array('types' =>
+                array('purge'
+                => new PageList_Column_purge('purge', _("Purge")))));
+            $pagelist->addPageList($pages);
             $button_label = _("Permanently purge selected pages");
             $header->pushContent(HTML::legend(_("Select the pages to purge")));
         }
